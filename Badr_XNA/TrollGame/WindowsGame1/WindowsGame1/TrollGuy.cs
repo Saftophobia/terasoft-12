b4e07b889bf76a4f79c6e5e2575b75eb68a59c2e
q3ahmd@gmail.com <q3ahmd@gmail.com@113cb9fd-de2b-231c-2074-2ec382a84f02>
2012-04-13 11:54:44 +0000

    My XNA Game, this is just a simple troll guy moving, but i made it OO and not all in 1 file to see what variables need to be passed from the XNA class to the other classes.

---
+++ Badr_XNA/TrollGame/WindowsGame1/WindowsGame1/TrollGuy.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
+namespace WindowsGame1
+{
+    class TrollGuy
+    {
+        Texture2D texture;
+        Vector2 pos1;
+        Vector2 pos2;
+        float speed1;
+        public TrollGuy(ContentManager Content)
+        {
+            speed1 = 3.0f;
+            pos1 = Vector2.Zero;
+            pos2 = Vector2.Zero;
+            texture = Content.Load<Texture2D>(@"Images/epic_trollface");
+        }
+        public void update(GameWindow Window)
+        {
+            pos1.X += speed1;
+            if (pos1.X > Window.ClientBounds.Width - texture.Width ||
+            pos1.X < 0)
+                speed1 *= -1;
+        }
+
+        public void draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Begin();
+            spriteBatch.Draw(texture,
+            pos1,
+            null,
+            Color.White,
+            0,
+            Vector2.Zero,
+            1,
+            SpriteEffects.None,
+            0);
+            spriteBatch.End();
+        }
+    }
+}
