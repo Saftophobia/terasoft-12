ae805141f5dd5767df55a6dab237d2a83cf097c3
michel.nader.mansour@gmail.com <michel.nader.mansour@gmail.com@113cb9fd-de2b-231c-2074-2ec382a84f02>
2012-04-14 17:15:34 +0000

    Added Michel_XNA folder containing the project I've made

---
+++ branches/XNA HelloWorld!/Michel_XNA/HelloWorld!/HelloWorld!/HelloWorld!/MyWizard.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace HelloWorld_
+{
+    class MyWizard
+    {
+        public Vector2 Position;
+        public Vector2 Velocity;
+        public Texture2D Texture;
+
+        public MyWizard(Vector2 Position, Vector2 Velocity, Texture2D Texture)
+        {
+            this.Position = Position;
+            this.Velocity = Velocity;
+            this.Texture = Texture;
+        }
+
+        public void Update()
+        {
+            this.Position += this.Velocity;
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Begin();
+            spriteBatch.Draw(Texture,new Rectangle((int)Position.X,(int)Position.Y,Texture.Width,Texture.Height),Color.White);
+            spriteBatch.End();
+        }
+    }
+}
