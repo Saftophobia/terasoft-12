f5b4476b60f9224c988dc9183cd2777daadf06e6
bishoybassem@gmail.com <bishoybassem@gmail.com@113cb9fd-de2b-231c-2074-2ec382a84f02>
2012-04-18 21:37:55 +0000

    Added camera class to represent all camera types

---
+++ trunk/ terasoft-12/Mechanect/Mechanect/Mechanect/Cameras/Camera.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Mechanect.Cameras
+{
+    public abstract class Camera
+    {
+
+        public Matrix view { get; set; }
+        public Matrix projection { get; set; }
+
+        protected GraphicsDevice GraphicsDevice { get; set; }
+
+        public Camera(GraphicsDevice graphicsDevice)
+        {
+            this.GraphicsDevice = graphicsDevice;
+            this.projection = Matrix.CreatePerspectiveFieldOfView(MathHelper.ToRadians(45), graphicsDevice.Viewport.AspectRatio, 0.1f, 1000000.0f);
+        }
+
+        public abstract void Update();
+
+    }
+}
