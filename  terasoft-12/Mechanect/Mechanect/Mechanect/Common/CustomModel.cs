7f2bb52306c2ce462ae872e530f3d8dc716f93a2
bishoybassem@gmail.com <bishoybassem@gmail.com@113cb9fd-de2b-231c-2074-2ec382a84f02>
2012-04-18 21:43:05 +0000

    Added CustomModel class to represent any 3d model

---
+++  terasoft-12/Mechanect/Mechanect/Mechanect/Common/CustomModel.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Mechanect.Common
+{
+    public class CustomModel
+    {
+
+        public Vector3 position { get; set; }
+        public Vector3 rotation { get; set; }
+        public Vector3 scale { get; set; }
+
+        private Model model;
+        private Matrix[] modelTransforms;
+
+        private GraphicsDevice graphicsDevice;
+
+        public CustomModel(Model model, Vector3 Position, Vector3 Rotation, Vector3 Scale, GraphicsDevice graphicsDevice)
+        {
+            this.position = Position;
+            this.rotation = Rotation;
+            this.scale = Scale;
+            this.graphicsDevice = graphicsDevice;
+            this.model = model;
+            modelTransforms = new Matrix[model.Bones.Count];
+            model.CopyAbsoluteBoneTransformsTo(modelTransforms);
+        }
+
+        public void Draw(Matrix view, Matrix projection)
+        {
+            Matrix world = Matrix.CreateScale(scale) * Matrix.CreateFromYawPitchRoll(rotation.Y, rotation.X, rotation.Z) * Matrix.CreateTranslation(position);
+            foreach (ModelMesh mesh in model.Meshes)
+            {
+                Matrix localWorld = modelTransforms[mesh.ParentBone.Index] * world;
+                foreach (ModelMeshPart part in mesh.MeshParts)
+                {
+                    BasicEffect e = (BasicEffect)part.Effect;
+                    e.World = localWorld;
+                    e.View = view;
+                    e.Projection = projection;
+                    e.EnableDefaultLighting();
+                }
+                mesh.Draw();
+            }
+        }
+    }
+}
