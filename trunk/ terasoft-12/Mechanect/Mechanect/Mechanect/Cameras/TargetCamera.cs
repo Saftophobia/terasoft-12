9177ffe632d83e17cd91d068cf40742090beaae8
bishoybassem@gmail.com <bishoybassem@gmail.com@113cb9fd-de2b-231c-2074-2ec382a84f02>
2012-04-18 21:39:34 +0000

    Added TargetCamera class

---
+++ trunk/ terasoft-12/Mechanect/Mechanect/Mechanect/Cameras/TargetCamera.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Mechanect.Cameras
+{
+    public class TargetCamera : Camera
+    {
+
+        private Vector3 position;
+        private Vector3 target;
+
+        public TargetCamera(Vector3 position, Vector3 target, GraphicsDevice graphicsDevice)
+            : base(graphicsDevice)
+        {
+            this.position = position;
+            this.target = target;
+        }
+
+        public override void Update()
+        {
+            this.view = Matrix.CreateLookAt(position, target, Vector3.Up);
+        }
+
+    }
+
+}
