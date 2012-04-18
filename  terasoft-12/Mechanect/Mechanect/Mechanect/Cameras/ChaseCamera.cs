8ffed3d36e8694f23b01d09953ba2dc85322937b
bishoybassem@gmail.com <bishoybassem@gmail.com@113cb9fd-de2b-231c-2074-2ec382a84f02>
2012-04-18 21:38:58 +0000

    Added ChaseCamera class

---
+++  terasoft-12/Mechanect/Mechanect/Mechanect/Cameras/ChaseCamera.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Mechanect.Cameras
+{
+    public class ChaseCamera : Camera
+    {
+        private Vector3 position;
+        private Vector3 target;
+
+        private Vector3 followTargetPosition;
+
+        private Vector3 positionOffset;
+        private Vector3 targetOffset;
+
+        private Vector3 relativeCameraRotation { get; set; }
+
+        public ChaseCamera(Vector3 positionOffset, Vector3 targetOffset, Vector3 relativeCameraRotation, GraphicsDevice graphicsDevice)
+            : base(graphicsDevice)
+        {
+            this.positionOffset = positionOffset;
+            this.targetOffset = targetOffset;
+            this.relativeCameraRotation = relativeCameraRotation;
+        }
+
+        public void Move(Vector3 newFollowTargetPosition)
+        {
+            this.followTargetPosition = newFollowTargetPosition;
+        }
+
+        public void Rotate(Vector3 rotationChange)
+        {
+            this.relativeCameraRotation += rotationChange;
+        }
+
+        public override void Update()
+        {
+            Matrix rotation = Matrix.CreateFromYawPitchRoll(relativeCameraRotation.Y, relativeCameraRotation.X, relativeCameraRotation.Z);
+            Vector3 desiredPosition = followTargetPosition + Vector3.Transform(positionOffset, rotation);
+            position = Vector3.Lerp(position, desiredPosition, .15f);
+            target = followTargetPosition + Vector3.Transform(targetOffset, rotation);
+            view = Matrix.CreateLookAt(position, target, Vector3.Transform(Vector3.Up, rotation));
+        }
+    }
+}
