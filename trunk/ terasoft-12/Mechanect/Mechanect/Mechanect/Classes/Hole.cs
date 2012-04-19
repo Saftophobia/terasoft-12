0873c7ebc64bec4292a2564635d75c8dac654ef8
q3ahmd@gmail.com <q3ahmd@gmail.com@113cb9fd-de2b-231c-2074-2ec382a84f02>
2012-04-19 15:55:33 +0000

    added radius and position to the class, with their accessors

--- trunk/ terasoft-12/Mechanect/Mechanect/Mechanect/Classes/Hole.cs
+++ trunk/ terasoft-12/Mechanect/Mechanect/Mechanect/Classes/Hole.cs
@@ -2,10 +2,35 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.Xna.Framework;
 
 namespace Mechanect.Classes
 {
     class Hole
     {
+        private int radius;
+        public int Radius
+        {
+            get
+            {
+                return radius;
+            }
+            set
+            {
+                radius = value;
+            }
+        }
+        private Vector2 position;
+        public Vector2 Position
+        {
+            get
+            {
+                return position;
+            }
+            set
+            {
+                position = value;
+            }
+        }
     }
 }
