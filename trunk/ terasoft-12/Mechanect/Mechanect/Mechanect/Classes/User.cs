55b3be8d57094e1ce611cd860f867ba10bcc5501
q3ahmd@gmail.com <q3ahmd@gmail.com@113cb9fd-de2b-231c-2074-2ec382a84f02>
2012-04-19 16:02:41 +0000

    added assumedLegMass and shootingPosition, with their accessrors and added a using statement for Xna framework

--- trunk/ terasoft-12/Mechanect/Mechanect/Mechanect/Classes/User.cs
+++ trunk/ terasoft-12/Mechanect/Mechanect/Mechanect/Classes/User.cs
@@ -2,10 +2,35 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.Xna.Framework;
 
 namespace Mechanect.Classes
 {
     class User
     {
+        private double assumedLegMass;
+        public double AssumedLegMass
+        {
+            get
+            {
+                return assumedLegMass;
+            }
+            set
+            {
+                assumedLegMass = value;
+            }
+        }
+        private Vector2 shootingPosition;
+        public Vector2 ShootingPosition
+        {
+            get
+            {
+                return shootingPosition;
+            }
+            set
+            {
+                shootingPosition = value;
+            }
+        }
     }
 }
