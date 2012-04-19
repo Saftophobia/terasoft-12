c6450032eadf90abd17497580018529a46dc5888
hollaxyman@gmail.com <hollaxyman@gmail.com@113cb9fd-de2b-231c-2074-2ec382a84f02>
2012-04-19 15:26:23 +0000

    Added the instance variables,Constructor and 3 methods to get Location,width,height.

--- trunk/ terasoft-12/Mechanect/Mechanect/Mechanect/Classes/Prey.cs
+++ trunk/ terasoft-12/Mechanect/Mechanect/Mechanect/Classes/Prey.cs
@@ -1,11 +1,59 @@
-﻿using System;
+ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Windows;
 
 namespace Mechanect.Classes
 {
     class Prey
     {
+        Point location;
+        int length;
+        int width;
+
+
+        public Prey(Point location, int width, int length)
+        {
+            this.location = location;
+            this.length = length;
+            this.width = width;
+        }
+        /// <summary>
+        /// returns the location of the prey.
+        /// </summary>
+        /// <remarks>
+        /// <para>AUTHOR: Tamer Nabil </para
+        /// </remarks>
+        /// <returns></returns>
+        public Point getLocation()
+        {
+            return location;
+        }
+
+        /// <summary>
+        /// return the Height of the Prey
+        /// </summary>
+        /// <remarks>
+        /// <para>AUTHOR: Tamer Nabil </para
+        /// </remarks>
+        /// <returns></returns>
+        public int getHeight()
+        {
+            return length;
+        }
+        /// <summary>
+        /// return the Width of the Prey
+        /// </summary>
+        /// <remarks>
+        /// <para>AUTHOR: Tamer Nabil </para
+        /// </remarks>
+        /// <returns></returns>
+        public int getWidth()
+        {
+            return width;
+        }
+    
+    
     }
 }
