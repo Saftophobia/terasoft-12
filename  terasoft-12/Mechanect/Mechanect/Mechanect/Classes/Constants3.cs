a58a4451f81ad895a5640fa788c3c7f859fe4b97
q3ahmd@gmail.com <q3ahmd@gmail.com@113cb9fd-de2b-231c-2074-2ec382a84f02>
2012-04-19 14:50:49 +0000

    moved         public static const float MIN_VELOCITYY = 1.0f;         public static const float MIN_VELOCITYX = 1.0f;         public static const float MAX_VELOCITYY = 1.0f;         public static const float MAX_VELOCITYX = 1.0f;         public static const float MAX_HOLEPOSX = 1.0f;         public static const float MAX_HOLEPOSY = 1.0f; from Constants1.cs to Constants3.cs because Constants3.cs is the one where experiment 3 Constants should be

---  terasoft-12/Mechanect/Mechanect/Mechanect/Classes/Constants3.cs
+++  terasoft-12/Mechanect/Mechanect/Mechanect/Classes/Constants3.cs
@@ -7,5 +7,11 @@ namespace Mechanect.Classes
 {
     class Constants3
     {
+	    public static const float MIN_VELOCITYY = 1.0f;
+        public static const float MIN_VELOCITYX = 1.0f;
+        public static const float MAX_VELOCITYY = 1.0f;
+        public static const float MAX_VELOCITYX = 1.0f;
+        public static const float MAX_HOLEPOSX = 1.0f;
+        public static const float MAX_HOLEPOSY = 1.0f;
     }
 }
