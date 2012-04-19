75b810ab1a0a6e87c93cd46ecbe5f2ca3eddeb6d
q3ahmd@gmail.com <q3ahmd@gmail.com@113cb9fd-de2b-231c-2074-2ec382a84f02>
2012-04-19 22:48:18 +0000

    transformed my method completely from 2D to 3D and fixed small errors in checking in the difference between shooting position and hole position and also made the checking condition of friction more reliable

--- trunk/ terasoft-12/Mechanect/Mechanect/Mechanect/Classes/Environment3.cs
+++ trunk/ terasoft-12/Mechanect/Mechanect/Mechanect/Classes/Environment3.cs
@@ -31,80 +31,80 @@ namespace Mechanect.Classes
 
 
 
-        //public int IsSolvable()
-        //{
+        public int IsSolvable()
+        {
 
-        //    if (ball.Radius <= 0)
-        //        return Constants3.NEGATIVE_BRADIUS;
-        //    if (ball.Mass <= 0)
-        //        return Constants3.NEGATIVE_BMASS;
-        //    if (hole.Radius <= 0)
-        //        return Constants3.NEGATIVE_HRADIUS;
-        //    if (user.AssumedLegMass <= 0)
-        //        return Constants3.NEGATIVE_LMASS;
-        //    //hole position not before the leg position
-        //    if (hole.Position.Y - user.ShootingPosition.Y <= 0)
-        //        return Constants3.NEGATIVE_HPOSY;
-        //    if (friction < 0)
-        //        return Constants3.NEGATIVE_FRICTION;
-        //    if (ball.Radius > hole.Radius)
-        //        return Constants3.NEGATIVE_RDIFFERENCE;
+            if (ball.Radius <= 0)
+                return Constants3.NEGATIVE_BRADIUS;
+            if (ball.Mass <= 0)
+                return Constants3.NEGATIVE_BMASS;
+            if (hole.Radius <= 0)
+                return Constants3.NEGATIVE_HRADIUS;
+            if (user.AssumedLegMass <= 0)
+                return Constants3.NEGATIVE_LMASS;
+            //hole position not before the leg position
+            if (hole.Position.Z - user.ShootingPosition.Z > 0)
+                return Constants3.NEGATIVE_HPOSY;
+            if (friction <= 0)
+                return Constants3.NEGATIVE_FRICTION;
+            if (ball.Radius > hole.Radius)
+                return Constants3.NEGATIVE_RDIFFERENCE;
 
-        //    /*
-        //     * Enviornment e;
-        //     * e.getFinalPosition(e.getVelocityAfterCollision(new Vector2(Constants3.MAX_VELOCITYX, Constants3.MAX_VELOCITYY,
-        //     * ballMass,AssumedLegMass,wind));//implement with hasScored();
-        //    */
+            /*
+             * Enviornment e;
+             * e.getFinalPosition(e.getVelocityAfterCollision(new Vector2(Constants3.MAX_VELOCITYX, Constants3.MAX_VELOCITYY,
+             * ballMass,AssumedLegMass,wind));//implement with hasScored();
+            */
 
-        //    Vector3 finalPos = Vector3.Zero;//to be changed with the position returned from getFinalPostion();
-        //    if (Vector3.Subtract(finalPos, user.ShootingPosition).LengthSquared() > Vector3.Subtract(hole.Position, user.ShootingPosition).LengthSquared())
-        //        return Constants3.HOLE_OUT_OF_FAR_RANGE;
-        //    /*
-        //     * Enviornment e;
-        //     * e.getFinalPosition(e.getVelocityAfterCollision(new Vector2(Constants3.MIN_VELOCITYX, Constants3.MIN_VELOCITYY,
-        //     * ballMass, AssumedLegMass, wind, friction));
-        //    */
+            Vector3 finalPos = Vector3.Zero;//to be changed with the position returned from getFinalPostion();
+            if (Vector3.Subtract(finalPos, user.ShootingPosition).LengthSquared() > Vector3.Subtract(hole.Position, user.ShootingPosition).LengthSquared())
+                return Constants3.HOLE_OUT_OF_FAR_RANGE;
+            /*
+             * Enviornment e;
+             * e.getFinalPosition(e.getVelocityAfterCollision(new Vector2(Constants3.MIN_VELOCITYX, Constants3.MIN_VELOCITYY,
+             * ballMass, AssumedLegMass, wind, friction));
+            */
 
-        //    if (Vector3.Subtract(finalPos, user.ShootingPosition).LengthSquared() > Vector3.Subtract(hole.Position, user.ShootingPosition).LengthSquared()) //length squared used for better performance than length
-        //        return Constants3.HOLE_OUT_OF_NEAR_RANGE;
+            if (Vector3.Subtract(finalPos, user.ShootingPosition).LengthSquared() > Vector3.Subtract(hole.Position, user.ShootingPosition).LengthSquared()) //length squared used for better performance than length
+                return Constants3.HOLE_OUT_OF_NEAR_RANGE;
 
-        //    return Constants3.SOLVABLE_EXPERIMENT;//means solvable
-        //}
+            return Constants3.SOLVABLE_EXPERIMENT;//means solvable
+        }
 
 
-        //public void GenerateSolvable()
-        //{
-        //    int x = Constants3.SOLVABLE_EXPERIMENT;
-        //    do
-        //    {
-        //        x = IsSolvable();
-        //        switch (x)
-        //        {
-        //            case Constants3.HOLE_OUT_OF_NEAR_RANGE: if (hole.Position.X == Constants3.MAX_HOLEPOSX && hole.Position.Z != Constants3.MAX_HOLEPOSY)
-        //                    hole.Position = (Vector3.Add(hole.Position, new Vector3(0, 0, -1)));
-        //                else if (hole.Position.Z == Constants3.MAX_HOLEPOSX && hole.Position.X != Constants3.MAX_HOLEPOSY)
-        //                    hole.Position = (Vector3.Add(hole.Position, new Vector3(1, 0, 0)));
-        //                else friction++; break;
-        //            case Constants3.HOLE_OUT_OF_FAR_RANGE: if (hole.Position.X == Constants3.MAX_HOLEPOSX && hole.Position.Z != Constants3.MAX_HOLEPOSY)
-        //                    hole.Position = (Vector2.Subtract(hole.Position, new Vector3(0, 0,-1)));
-        //                else if (hole.Position.Y == Constants3.MAX_HOLEPOSX && hole.Position.X != Constants3.MAX_HOLEPOSY)
-        //                    hole.Position = (Vector2.Subtract(hole.Position, new Vector3(1, 0, 0)));
-        //                else if (wind != 0)
-        //                    wind--;
-        //                else if (friction != 1)
-        //                    friction--;
-        //                else if (friction <= 1)
-        //                    friction -= 0.1f;
-        //                break;
-        //            case Constants3.NEGATIVE_RDIFFERENCE: int tmp = ball.Radius; ball.Radius = (hole.Radius); hole.Radius = (tmp); break;
-        //            case Constants3.NEGATIVE_LMASS: user.AssumedLegMass *= 1; break;
-        //            case Constants3.NEGATIVE_BMASS: ball.Mass *= -1; break;
-        //            case Constants3.NEGATIVE_BRADIUS: ball.Radius *= -1; break;
-        //            case Constants3.NEGATIVE_HRADIUS: hole.Radius *= -1; break;
-        //            case Constants3.NEGATIVE_FRICTION: friction *= -1; break;
-        //        }
-        //    } while ((x = IsSolvable()) != Constants3.SOLVABLE_EXPERIMENT);
-        //}
+        public void GenerateSolvable()
+        {
+            int x = Constants3.SOLVABLE_EXPERIMENT;
+            do
+            {
+                x = IsSolvable();
+                switch (x)
+                {
+                    case Constants3.HOLE_OUT_OF_NEAR_RANGE: if (hole.Position.X == Constants3.MAX_HOLEPOSX && hole.Position.Z != Constants3.MAX_HOLEPOSZ)
+                            hole.Position = (Vector3.Add(hole.Position, new Vector3(0, 0, -1)));
+                        else if (hole.Position.Z == Constants3.MAX_HOLEPOSX && hole.Position.X != Constants3.MAX_HOLEPOSZ)
+                            hole.Position = (Vector3.Add(hole.Position, new Vector3(1, 0, 0)));
+                        else friction++; break;
+                    case Constants3.HOLE_OUT_OF_FAR_RANGE: if (hole.Position.X == Constants3.MAX_HOLEPOSX && hole.Position.Z != Constants3.MAX_HOLEPOSZ)
+                            hole.Position = (Vector3.Subtract(hole.Position, new Vector3(0, 0, -1)));
+                        else if (hole.Position.Y == Constants3.MAX_HOLEPOSX && hole.Position.X != Constants3.MAX_HOLEPOSZ)
+                            hole.Position = (Vector3.Subtract(hole.Position, new Vector3(1, 0, 0)));
+                        else if (wind != 0)
+                            wind--;
+                        else if (friction != 1)
+                            friction--;
+                        else if (friction <= 1)
+                            friction -= 0.1f;
+                        break;
+                    case Constants3.NEGATIVE_RDIFFERENCE: int tmp = ball.Radius; ball.Radius = (hole.Radius); hole.Radius = (tmp); break;
+                    case Constants3.NEGATIVE_LMASS: user.AssumedLegMass *= 1; break;
+                    case Constants3.NEGATIVE_BMASS: ball.Mass *= -1; break;
+                    case Constants3.NEGATIVE_BRADIUS: ball.Radius *= -1; break;
+                    case Constants3.NEGATIVE_HRADIUS: hole.Radius *= -1; break;
+                    case Constants3.NEGATIVE_FRICTION: friction *= -1; break;
+                }
+            } while ((x = IsSolvable()) != Constants3.SOLVABLE_EXPERIMENT);
+        }
 
 
         
