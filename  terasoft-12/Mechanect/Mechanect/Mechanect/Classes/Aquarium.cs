c9c0a1146c426110eb013ba82d757d168cceea05
hollaxyman@gmail.com <hollaxyman@gmail.com@113cb9fd-de2b-231c-2074-2ec382a84f02>
2012-04-19 15:29:59 +0000

    Added the instance variables,Constructor and 3 methods to get Location,width,height.

---  terasoft-12/Mechanect/Mechanect/Mechanect/Classes/Aquarium.cs
+++  terasoft-12/Mechanect/Mechanect/Mechanect/Classes/Aquarium.cs
@@ -1,11 +1,57 @@
-﻿using System;
+ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Windows;
 
 namespace Mechanect.Classes
 {
     class Aquarium
     {
+    
+        Point location;
+        int length;
+        int width;
+
+
+        public Aquarium(Point location, int width, int length)
+        {
+            this.location = location;
+            this.length = length;
+            this.width = width;
+        }
+        /// <summary>
+        /// returns the location of the Aquarium.
+        /// </summary>
+        /// <remarks>
+        /// <para>AUTHOR: Tamer Nabil </para
+        /// </remarks>
+        /// <returns></returns>
+        public Point getLocation()
+        {
+            return location;
+        }
+        /// <summary>
+        /// returns the height of the Aquarium.
+        /// </summary>
+        /// <remarks>
+        /// <para>AUTHOR: Tamer Nabil </para
+        /// </remarks>
+        /// <returns></returns>
+        public int getHeight()
+        {
+            return length;
+        }
+        /// <summary>
+        /// returns the width of the Aquarium.
+        /// </summary>
+        /// <remarks>
+        /// <para>AUTHOR: Tamer Nabil </para
+        /// </remarks>
+        /// <returns></returns>
+        public int getWidth()
+        {
+            return width;
+        }
     }
 }
