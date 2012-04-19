a50983ae054cfdaeef0b89ef02f5f9d9a32873d1
hollaxyman@gmail.com <hollaxyman@gmail.com@113cb9fd-de2b-231c-2074-2ec382a84f02>
2012-04-19 15:23:11 +0000

    Added the instance variables,Constructor and method to return the location of predator.

---  terasoft-12/Mechanect/Mechanect/Mechanect/Classes/Predator.cs
+++  terasoft-12/Mechanect/Mechanect/Mechanect/Classes/Predator.cs
@@ -1,11 +1,36 @@
-﻿using System;
+ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Windows;
 
 namespace Mechanect.Classes
 {
     class Predator
     {
+    
+        Point location;
+        int size;
+
+        public Predator(Point location)
+        {
+            this.location = location;
+
+        }
+
+        /// <summary>
+        /// returns the location of the predator.
+        /// </summary>
+        /// <remarks>
+        /// <para>AUTHOR: Tamer Nabil </para
+        /// </remarks>
+        /// <returns></returns>
+       
+
+        public Point getLocation()
+        {
+            return location;
+        }
+    
     }
 }
