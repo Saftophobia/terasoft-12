19631eec8e4e63b3026b5510aaacb0208922e6c3
hollaxyman@gmail.com <hollaxyman@gmail.com@113cb9fd-de2b-231c-2074-2ec382a84f02>
2012-04-19 17:47:52 +0000

    Added generate2Dpoint(int x,int y) method in Class Tools2

---  terasoft-12/Mechanect/Mechanect/Mechanect/Classes/Tools2.cs
+++  terasoft-12/Mechanect/Mechanect/Mechanect/Classes/Tools2.cs
@@ -1,4 +1,4 @@
-﻿using System;
+ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,5 +7,25 @@ namespace Mechanect.Classes
 {
     class Tools2
     {
+    
+       /// <summary>
+        /// Generate 2D point randomly through given range.
+        /// </summary>
+        /// <remarks>
+        /// <para>AUTHOR: Tamer Nabil </para>
+        /// </remarks>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <returns></returns>
+        public Point generate2Dpoint(int x,int y)
+        {
+            Random rand = new Random();
+           int xpoint = rand.Next(x,y);
+           int ypoint = rand.Next(x,y);
+            Point point = new Point(xpoint,ypoint);
+
+            return point;
+
+        }
     }
 }
