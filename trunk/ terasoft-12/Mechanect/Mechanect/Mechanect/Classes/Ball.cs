0e9ac9ea33743387160f591b88c10da18f753621
mk.cena92@gmail.com <mk.cena92@gmail.com@113cb9fd-de2b-231c-2074-2ec382a84f02>
2012-04-19 16:41:33 +0000

    Cena- adding the method that generates a random mass for the ball within a given range

--- trunk/ terasoft-12/Mechanect/Mechanect/Mechanect/Classes/Ball.cs
+++ trunk/ terasoft-12/Mechanect/Mechanect/Mechanect/Classes/Ball.cs
@@ -30,5 +30,25 @@ namespace Mechanect.Classes
             }
         }
 
+        public Ball(float minMass, float maxMass)
+        {
+            mass = generateBallMass(minMass,maxMass);
+        }
+
+        /// <summary>
+        /// generates a random mass for the ball within a certain range
+        /// </summary>
+        /// <param name="min">minimum mass of the ball</param>
+        /// <param name="max">maximum mass of the ball</param>
+        /// <returns></returns>
+        public float generateBallMass(float min, float max)
+        {
+            Random random = new Random();
+            float generatedMass = ((float)(random.NextDouble() * (max - min))) + min;
+            return generatedMass;
+        }
+
+
+
     }
 }
