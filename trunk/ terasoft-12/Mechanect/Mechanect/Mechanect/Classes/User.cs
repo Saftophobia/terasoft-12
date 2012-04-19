e80f60a5c9c453eccc2d6a7ff897f8b0c60d42fa
mk.cena92@gmail.com <mk.cena92@gmail.com@113cb9fd-de2b-231c-2074-2ec382a84f02>
2012-04-19 18:42:10 +0000

    Cena - Adding instance variables and initializing them in the constructor

--- trunk/ terasoft-12/Mechanect/Mechanect/Mechanect/Classes/User.cs
+++ trunk/ terasoft-12/Mechanect/Mechanect/Mechanect/Classes/User.cs
@@ -11,6 +11,255 @@ namespace Mechanect.Classes
     class User
     {
 
+        private Joint trackedJoint;
+        public Joint TrackedJoint  //  the user's tracked joint
+        {
+            get
+            {
+                return trackedJoint;
+            }
+            set
+            {
+                trackedJoint = value;
+            }
+        }
+         
+        private bool rightLeg;  // is the user using his right leg or left
+        public bool RightLeg
+        {
+            get
+            {
+                return rightLeg;
+            }
+            set
+            {
+                rightLeg = value;
+            }
+        }
+
+        private bool movedForward; // has the user moved his leg forward
+        public bool MovedForward
+        {
+            get
+            {
+                return movedForward;
+            }
+            set
+            {
+                movedForward = value;
+            }
+        }
+
+        //x position
+        private double initialRightLegPositionX; //meters // stores the initial position of the right leg
+        public double InitialRightLegPositionX
+        {
+            get
+            {
+                return initialRightLegPositionX;
+            }
+            set
+            {
+                initialRightLegPositionX = value;
+            }
+        }
+        
+        private double currentRightLegPositionX; //meters // stores the current position of the right leg 
+        public double CurrentRightLegPositionX
+        {
+            get
+            {
+                return CurrentRightLegPositionX;
+            }
+            set
+            {
+                currentRightLegPositionX = value;
+            }
+        }
+
+        //z position
+        private double initialRightLegPositionZ; //meters
+        public double InitialRightLegPositionZ
+        {
+            get
+            {
+                return initialRightLegPositionZ;
+            }
+            set
+            {
+                initialRightLegPositionZ = value;
+            }
+        }
+
+
+        private double currentRightLegPositionZ; //meters
+        public double CurrentRightLegPositionZ
+        {
+            get
+            {
+                return CurrentRightLegPositionZ;
+            }
+            set
+            {
+                currentRightLegPositionZ = value;
+            }
+        }
+
+        // store 
+
+        private double storeX1;
+        public double StoreX1
+        {
+            get
+            {
+                return storeX1;
+            }
+            set
+            {
+                storeX1 = value;
+
+            }
+        }
+        private double storeX2;
+        public double StoreX2
+        {
+            get
+            {
+                return storeX2;
+            }
+            set
+            {
+                storeX2 = value;
+
+            }
+        }
+        private double storeZ1;
+        public double StoreZ1
+        {
+            get
+            {
+                return storeZ1;
+            }
+            set
+            {
+                storeZ1 = value;
+
+            }
+        }
+        public double storeZ2;
+        public double StoreZ2
+        {
+            get
+            {
+                return storeZ2;
+            }
+            set
+            {
+                storeZ2 = value;
+
+            }
+        }
+
+
+        //left leg
+
+        //x position
+        private double initialLeftLegPositionX; //meters // stores the initial position of the left leg
+        public double InitialLeftLegPositionX
+        {
+            get
+            {
+                return initialLeftLegPositionX;
+            }
+            set
+            {
+                initialLeftLegPositionX = value;
+            }
+        }
+
+        private double currentLeftLegPositionX; //meters // stores the current position of the left leg 
+        public double CurrentLeftLegPositionX
+        {
+            get
+            {
+                return CurrentLeftLegPositionX;
+            }
+            set
+            {
+                currentLeftLegPositionX = value;
+            }
+        }
+
+        //z position
+        private double initialLeftLegPositionZ; //meters
+        public double InitialLeftLegPositionZ
+        {
+            get
+            {
+                return initialLeftLegPositionZ;
+            }
+            set
+            {
+                initialLeftLegPositionZ = value;
+            }
+        }
+
+
+        private double currentLeftLegPositionZ; //meters
+        public double CurrentLeftLegPositionZ
+        {
+            get
+            {
+                return CurrentLeftLegPositionZ;
+            }
+            set
+            {
+                currentLeftLegPositionZ = value;
+            }
+        }
+
+
+
+        private double velocity;
+        public double Velocity // the velocity of the users leg
+        {
+            get
+            {
+                return velocity;
+            }
+            set
+            {
+                velocity = value;
+            }
+        }
+
+        private bool trying; // is the user in the trying mode or shooting
+        public bool Trying
+        {
+            get
+            {
+                return trying;
+            }
+            set
+            {
+                trying = value;
+            }
+        }
+
+        private double angle; // angle of the leg
+        public double Angle
+        {
+            get
+            {
+                return angle;
+            }
+            set
+            {
+                angle = value;
+            }
+        }
+
+
         private Skeleton user;
         public Skeleton User
         {
@@ -22,6 +271,11 @@ namespace Mechanect.Classes
             {
                 user = value;
             }
+        }
+
+        public void setSkeleton()
+        {
+            user = kinect.requestSkeleton();
         }
 
         private MKinect kinect;
@@ -68,6 +322,28 @@ namespace Mechanect.Classes
             kinect = new MKinect();
             user = kinect.requestSkeleton();
             this.assumedLegMass = assumedLegMass;
+
+            initialLeftLegPositionX = 0;
+            currentLeftLegPositionX = 0;
+            initialLeftLegPositionZ = 0;
+            currentLeftLegPositionZ = 0;
+
+            initialRightLegPositionX = 0;
+            currentRightLegPositionX = 0;
+            initialRightLegPositionZ = 0;
+            currentRightLegPositionZ = 0;
+
+            storeX1 = 0;
+            storeX2 = 0;
+            storeZ1 = 0;
+            storeZ2 = 0;
+            
+            velocity = 0;
+            angle = 0;
+        
+            movedForward = false;
+            trying = true;
+         
 
         }
     }
