4e3e71b30df57f3effec3deeef87a795a553a7e4
bishoybassem@gmail.com <bishoybassem@gmail.com@113cb9fd-de2b-231c-2074-2ec382a84f02>
2012-04-20 19:24:58 +0000

    Added AnimationFrame and ModelFramedAnimation classes

---
+++  terasoft-12/Mechanect/Mechanect/Mechanect/Common/AnimationFrame.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace Mechanect.Common
+{
+    public class AnimationFrame
+    {
+        public Vector3 Position;
+        public Vector3 Rotation;
+        public TimeSpan Time;
+
+        public AnimationFrame(Vector3 Position, Vector3 Rotation, TimeSpan Time)
+        {
+            this.Position = Position;
+            this.Rotation = Rotation;
+            this.Time = Time;
+        }
+    }
+}
