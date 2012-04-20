f4b82b97c90e7b9e885dfe64c00bf7b16af94f56
q3ahmd@gmail.com <q3ahmd@gmail.com@113cb9fd-de2b-231c-2074-2ec382a84f02>
2012-04-20 09:39:23 +0000

    just some naming conventions, changed all "All uppercase" variable names to normal names as constants in C# are not written in "All uppercase" convention

---  terasoft-12/Mechanect/Mechanect/Mechanect/Classes/Constants3.cs
+++  terasoft-12/Mechanect/Mechanect/Mechanect/Classes/Constants3.cs
@@ -7,24 +7,25 @@ namespace Mechanect.Classes
 {
     static class Constants3
     {
-        public const float MIN_VELOCITYY = 1.0f;
-        public const float MIN_VELOCITYX = 1.0f;
-        public const float MAX_VELOCITYY = 1.0f;
-        public const float MAX_VELOCITYX = 1.0f;
-        public const float MAX_HOLEPOSX = 1.0f;
-        public const float MAX_HOLEPOSZ = 1.0f;
-        public const int NEGATIVE_BRADIUS = 1;
-        public const int NEGATIVE_BMASS = 2;
-        public const int NEGATIVE_HRADIUS = 3;
-        public const int NEGATIVE_LMASS = 4;
-        public const int NEGATIVE_HPOSZ = 6;
-        public const int NEGATIVE_FRICTION = 7;
-        public const int NEGATIVE_RDIFFERENCE = 8;
+        public const float minVelocityY = 1.0f;
+        public const float minVelocityX = 1.0f;
+        public const float maxVelocityY = 1.0f;
+        public const float maxVelocityX = 1.0f;
+        public const float maxHolePosX = 1.0f;
+        public const float maxHolePosZ = 1.0f;
+        public const int negativeBRradius = 1;
+        public const int negativeBMass = 2;
+        public const int negativeHRadius = 3;
+        public const int negativeLMass = 4;
+        public const int negativeHPosZ = 6;
+        public const int negativeFriction = 7;
+        public const int negativeRDifference = 8;
         //Difference between the radii of the ball and hole
-        public const int HOLE_OUT_OF_FAR_RANGE = 9;
-        public const int HOLE_OUT_OF_NEAR_RANGE = 10;
-        public const int SOLVABLE_EXPERIMENT = 0;
+        public const int holeOutOfFarRange = 9;
+        public const int holeOutOfNearRange = 10;
+        public const int solvableExperiment = 0;
         public const float NORMAL_LEG_MASS = 0.01f;
+        //CHANGE NORMAL_LEG_MASS TO THE NAMING CONVENTIONS USED ABOVE!!
         public const double unitTime = 0.0333333333334; //seconds
         public const float legMovementTolerance = 0.09f; //meters
     }
