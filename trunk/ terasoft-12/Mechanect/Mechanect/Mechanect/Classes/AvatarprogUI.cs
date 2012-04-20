efb8e90d0740e1d47609e2fce5f43ccd67df0fea
asafty2003@gmail.com <asafty2003@gmail.com@113cb9fd-de2b-231c-2074-2ec382a84f02>
2012-04-20 15:48:09 +0000

    update issue 7
    Status: In Progress
    
    Added AvatarprogUI.cs class, it handles 2 Users, tracks their motion and represent it on screen. its a helper class, need to get called to the main Class and some extra lines ( these lines are //)

---
+++ trunk/ terasoft-12/Mechanect/Mechanect/Mechanect/Classes/AvatarprogUI.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Kinect;
+using Mechanect.Common;
+
+namespace Mechanect.Classes
+{
+    class AvatarprogUI
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        MKinect kinect;
+        Vector2 vectorp1;
+        Vector2 vectorp2;
+        Texture2D phototexture;
+        Skeleton p1;
+        Skeleton p2;
+        public AvatarprogUI()
+        {
+            //kinect = new MKinect();
+            //base.Initialize();
+            // spriteBatch = new SpriteBatch(GraphicsDevice);
+            //phototexture = Content.Load<Texture2D>("ball");
+            vectorp1 = new Vector2(700, 25);
+            vectorp2 = new Vector2(700, 200);
+        }
+
+        protected void Update()
+        {
+            //if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
+            //  this.Exit();
+            p1 = kinect.requestSkeleton();
+            p2 = kinect.requestSkeleton();
+            if (p1 != null)
+            {
+                vectorp1.X = p1.Position.Z * 200;
+            }
+            if (p2 != null)
+            {
+                vectorp2.X = p2.Position.Z * 200;
+            }
+            // base.Update(gameTime);
+        }
+
+        protected void Draw(GameTime gameTime)
+        {
+            //GraphicsDevice.Clear(Color.CornflowerBlue);
+            spriteBatch.Begin();
+            spriteBatch.Draw(phototexture, vectorp1, new Rectangle(0, 0, 100, 100), Color.White);
+            spriteBatch.Draw(phototexture, vectorp2, new Rectangle(0, 0, 100, 100), Color.White);
+            spriteBatch.End();
+            //base.Draw(gameTime);
+        }
+    }
+}
