b19a3cb08b98547703999169825fc72c6b5181cd
asafty2003@gmail.com <asafty2003@gmail.com@113cb9fd-de2b-231c-2074-2ec382a84f02>
2012-04-20 21:04:54 +0000

    update issue 13
    Status: In Progress
    
    Added a modified algorithm, a function to use the constraints on the generated list, and a function to generated a list of random time slices

--- trunk/ terasoft-12/Mechanect/Mechanect/Mechanect/Classes/Tools1.cs
+++ trunk/ terasoft-12/Mechanect/Mechanect/Mechanect/Classes/Tools1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Security.Cryptography;
 
 namespace Mechanect.Classes
 {
@@ -36,5 +37,86 @@ namespace Mechanect.Classes
             }
 
         }
+
+        
+
+        /// <summary>
+        /// modified Fisher–Yates shuffle algorithm "http://en.wikipedia.org/wiki/Fisher-Yates_shuffle"
+        /// It shuffles the content of the List in random way
+        /// </summary>
+        /// <typeparam name="T">Type of the Data in the array</typeparam>
+        /// <param name="list">provide a list to get shuffled</param>
+        /// <remarks>
+        ///<para>AUTHOR: Safty </para>
+        ///<para>DATE WRITTEN: 20/4/12 </para>
+        ///<para>DATE MODIFIED: 20/4/12 </para>
+        ///</remarks>
+        public static void RNGshuffle<T>(IList<T> list)
+        {
+            RNGCryptoServiceProvider provider = new RNGCryptoServiceProvider();
+            int n = list.Count;
+            while (n > 1)
+            {
+                byte[] box = new byte[1];
+                do provider.GetBytes(box);
+                while (!(box[0] < n * (Byte.MaxValue / n)));
+                int k = (box[0] % n);
+                n--;
+                T value = list[k];
+                list[k] = list[n];
+                list[n] = value;
+            }
+            return;
+        }
+
+        /// <summary>
+        /// applies constraints to the generated list
+        /// </summary>
+        /// <typeparam name="T">Type of the Data in the array</typeparam>
+        /// <param name="list">provide a list to get shuffled according to some constraints</param>
+        /// <remarks>
+        ///<para>AUTHOR: Safty </para>
+        ///<para>DATE WRITTEN: 20/4/12 </para>
+        ///<para>DATE MODIFIED: 20/4/12 </para>
+        ///</remarks>
+         public static void commandshuffler<T>(IList<T> list)
+        {
+            RNGshuffle<T>(list);
+            if (list[0].ToString() == "decreasingAcceleration" || list[0].ToString() == "constantVelocity" || list[0].ToString() == "constantDisplacement")
+                commandshuffler<T>(list);
+            if (list[1].ToString() == "decreasingAcceleration" || list[1].ToString() == "constantAcceleration"  || list[1].ToString() == "increasingAcceleration")
+                commandshuffler<T>(list);
+            if (list[1].ToString() == "constantDisplacement")
+            {
+                 if (list[2].ToString() == "decreasingAcceleration" || list[2].ToString() == "constantVelocity" || list[2].ToString() == "constantDisplacement")
+                commandshuffler<T>(list);
+            }
+            return;
+            }
+
+        /// <summary>
+        /// create a list with generated random seconds (3-6)
+        /// </summary>
+        /// <param name="size">desired size of the list to be created</param>
+        /// <remarks>
+        ///<para>AUTHOR: Safty </para>
+        ///<para>DATE WRITTEN: 20/4/12 </para>
+        ///<para>DATE MODIFIED: 20/4/12 </para>
+        ///</remarks>
+         private List<int> generaterandomnumbers(int size){
+                Random rand = new Random();
+                List<int> result = new List<int>();
+                HashSet<int> check = new HashSet<int>();
+                for (Int32 i = 0; i < size; i++) {
+                    int curValue = rand.Next(3,6);
+                    
+                    result.Add(curValue);
+                    }
+                return result;
+            }
+
+           
+        }
+
     }
-}
+
