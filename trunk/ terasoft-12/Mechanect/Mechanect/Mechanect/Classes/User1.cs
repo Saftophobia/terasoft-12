6ebf133f7eade2e841e09f893fbc07c42b03b755
mk.cena92@gmail.com <mk.cena92@gmail.com@113cb9fd-de2b-231c-2074-2ec382a84f02>
2012-04-22 20:40:05 +0000

    Cena - Moving the common variables and methods to class user and the uncommon ones are distributed over the subclasses  User1.cs,User2.cs,User3

---
+++ trunk/ terasoft-12/Mechanect/Mechanect/Mechanect/Classes/User1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Mechanect.Classes
+{
+    class User1:User
+    {
+        /// <summary>
+        /// this is the index of the array of currentCommands in the class Game which defines what is the command active
+        /// </summary>
+        private int activeCommand;
+        public int ActiveCommand
+        {
+            get
+            {
+                return activeCommand;
+            }
+            set
+            {
+                activeCommand = value;
+            }
+        }
+        private List<float> positions;
+        public List<float> Positions
+        {
+            get
+            {
+                return positions;
+            }
+            set
+            {
+                positions = value;
+            }
+        }
+        private bool disqualified;
+        public bool Disqualified
+        {
+            get
+            {
+                return disqualified;
+            }
+            set
+            {
+                disqualified = value;
+            }
+        }
+        private int disqualificationTime;
+        public int DisqualificationTime
+        {
+            get
+            {
+                return disqualificationTime;
+            }
+            set
+            {
+                disqualificationTime = value;
+            }
+        }
+        
+
+    }
+}
