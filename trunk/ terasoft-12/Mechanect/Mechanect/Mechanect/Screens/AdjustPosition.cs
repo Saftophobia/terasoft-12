cd519b5f13d6791a08dec85d248ae9c0a9ee4761
3abdelazim@gmail.com <3abdelazim@gmail.com@113cb9fd-de2b-231c-2074-2ec382a84f02>
2012-04-22 09:56:33 +0000

    Added new screen "AdjustPosition.cs"

---
+++ trunk/ terasoft-12/Mechanect/Mechanect/Mechanect/Screens/AdjustPosition.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using Mechanect.Classes;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using Microsoft.Kinect;
+
+namespace Mechanect.Screens
+{
+    class AdjustPosition : Mechanect.Common.GameScreen
+    {
+
+        Skeleton skeleton;
+        int minDepth;
+        int maxDepth;
+        float minAngle;
+        float maxAngle;
+
+        int depth;
+        float angle;
+
+        public int getDepth()
+        {
+            return 0;
+        }
+
+        public float getAngle()
+        {
+            return 0;
+        }
+
+        public AdjustPosition(Skeleton skeleton, int minDepth, int maxDepth, float minAngle, float maxAngle)
+        {
+            this.skeleton = skeleton;
+            this.minDepth = minDepth;
+            this.maxDepth = maxDepth;
+            this.minAngle = minAngle;
+            this.maxAngle = maxAngle;
+        }
+
+        public void Update(GameTime gameTime, Boolean covered)
+        {
+            depth = getDepth();
+            angle = getAngle();
+        }
+
+        public override void Draw(GameTime gameTime)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
