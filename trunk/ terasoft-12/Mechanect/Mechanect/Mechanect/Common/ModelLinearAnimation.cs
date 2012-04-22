58da467445ff53465ef49da2b2f09f2a07bec286
bishoybassem@gmail.com <bishoybassem@gmail.com@113cb9fd-de2b-231c-2074-2ec382a84f02>
2012-04-22 20:55:55 +0000

    Updated ModelLinearAnimation class added animationStoped flag changed constructor to include TimeSpan duration attribute

--- trunk/ terasoft-12/Mechanect/Mechanect/Mechanect/Common/ModelLinearAnimation.cs
+++ trunk/ terasoft-12/Mechanect/Mechanect/Mechanect/Common/ModelLinearAnimation.cs
@@ -4,8 +4,17 @@ using System.Linq;
 using System.Text;
 using Microsoft.Xna.Framework;
 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
 namespace Mechanect.Common
 {
+    /// <summary>
+    /// represents 3d model linear animation with acceleration
+    /// </summary>
     public class ModelLinearAnimation
     {
 
@@ -14,9 +23,17 @@ namespace Mechanect.Common
         private TimeSpan duration;
         private CustomModel model;
         private Boolean enableRotation;
-        private Boolean objectWillStop;
+        public Boolean animationStoped { get; private set; }
 
-        public ModelLinearAnimation(CustomModel model, Vector3 velocity, Vector3 acceleration, Boolean enableRotation)
+        /// <summary>
+        /// constructs a ModelLinearAnimation instance
+        /// </summary>
+        /// <param name="model">the 3d custom model</param>
+        /// <param name="velocity">the object's velocity vector</param>
+        /// <param name="acceleration">the object's acceleration vector</param>
+        /// <param name="enableRotation">enables object rotation</param>
+        /// <param name="duration">animation duration</param>
+        public ModelLinearAnimation(CustomModel model, Vector3 velocity, Vector3 acceleration, TimeSpan duration, Boolean enableRotation)
         {
             this.model = model;
             this.velocity = velocity;
@@ -25,26 +42,39 @@ namespace Mechanect.Common
             elapsedTime = TimeSpan.FromSeconds(0);
             if (Vector3.Add(velocity, acceleration).Length() < velocity.Length())
             {
-                objectWillStop = true;
-                duration = TimeSpan.FromSeconds(velocity.Length() / acceleration.Length() / 2);
+                this.duration = TimeSpan.FromSeconds(velocity.Length() / acceleration.Length() / 2f);
+            }
+            else
+            {
+                this.duration = duration;
             }
             startPosition = model.position;
             startAngle = model.rotation;
         }
 
+        /// <summary>
+        /// updates the model position and orientation according to the time elapsed
+        /// </summary>
+        /// <param name="elapsed">time offset from the last update</param>
         public void Update(TimeSpan elapsed)
         {
             this.elapsedTime += elapsed;
-            if (!objectWillStop || elapsedTime.TotalSeconds < duration.TotalSeconds)
+            if (elapsedTime.TotalSeconds < duration.TotalSeconds)
             {
                 float ms = ((float)elapsedTime.TotalMilliseconds);
                 model.position = startPosition + (velocity * ms / 1000f) + (acceleration * ms * ms / 1000000f);
                 if (enableRotation)
                 {
-                    model.rotation = startAngle + (velocity * ms / 1000f + acceleration * ms * ms / 1000000f) / 180;
+                    Vector3 delta = (velocity * ms / 1000f + acceleration * ms * ms / 1000000f) / 180;
+                    model.rotation = startAngle + new Vector3(delta.Z, delta.Y, -1 * delta.X);
                 }
+            }
+            else
+            {
+                animationStoped = true;
             }
 
         }
     }
 }
+
