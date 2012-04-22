f4818a9f2e17c150989b5d8c5120d4f7fd8ac173
khaled.osmaan <khaled.osmaan@113cb9fd-de2b-231c-2074-2ec382a84f02>
2012-04-22 22:03:34 +0000

    update issue 36 Status: In Progress Message: implemented method draw which draws the new how to play screen related to experiment 3 and added its documentation

--- trunk/ terasoft-12/Mechanect/Mechanect/Mechanect/Screens/InstructionsScreen3.cs
+++ trunk/ terasoft-12/Mechanect/Mechanect/Mechanect/Screens/InstructionsScreen3.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Mechanect.Common;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
 
 namespace Mechanect.Screens
 {
-    class InstructionsScreen3:GameScreen
+    class InstructionsScreen3 : GameScreen
     {
         public InstructionsScreen3()
         {
@@ -15,7 +22,7 @@ namespace Mechanect.Screens
 
         public override void LoadContent()
         {
-           
+
         }
 
         public override void UnloadContent()
@@ -27,12 +34,22 @@ namespace Mechanect.Screens
         {
             base.Update(gameTime, covered);
         }
-
+        /// <remarks>
+        ///<para>AUTHOR: Khaled Salah </para>
+        ///</remarks>
+        /// <summary>
+        /// Draws the how to play instruction screen related to experiment3
+        /// </summary>
+        /// <param name="gameTime">
+        /// an instance variable of class GameTime in Microsoft.Xna.Framework.GameTime package
+        /// </param>
         public override void Draw(Microsoft.Xna.Framework.GameTime gameTime)
         {
-            throw new NotImplementedException();
+            Vector2 position = new Vector2(100, 20);
+            Instruction I = new Instruction("The goal of this game is to shoot the ball such that it reaches the hole with 0 velocity", position);
+            I.Draw();
         }
-
+    
         public override void Remove()
         {
             base.Remove();
