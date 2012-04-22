a9ac5875080c4f84dd3ae19ce98a15a4b10f426a
mk.cena92@gmail.com <mk.cena92@gmail.com@113cb9fd-de2b-231c-2074-2ec382a84f02>
2012-04-22 13:23:50 +0000

    Cena - adding InstructionsScreen3 class

---
+++ trunk/ terasoft-12/Mechanect/Mechanect/Mechanect/Screens/InstructionsScreen3.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Mechanect.Common;
+
+namespace Mechanect.Screens
+{
+    class InstructionsScreen3:GameScreen
+    {
+        public InstructionsScreen3()
+        {
+        }
+
+
+        public override void LoadContent()
+        {
+           
+        }
+
+        public override void UnloadContent()
+        {
+
+        }
+
+        public override void Update(Microsoft.Xna.Framework.GameTime gameTime, bool covered)
+        {
+            base.Update(gameTime, covered);
+        }
+
+        public override void Draw(Microsoft.Xna.Framework.GameTime gameTime)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Remove()
+        {
+            base.Remove();
+        }
+    }
+}
