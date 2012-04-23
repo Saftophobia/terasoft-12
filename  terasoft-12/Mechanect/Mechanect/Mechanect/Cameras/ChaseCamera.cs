f0a99701aeb121b175d8ad5888c0f6e6de5f5170
bishoybassem@gmail.com <bishoybassem@gmail.com@113cb9fd-de2b-231c-2074-2ec382a84f02>
2012-04-23 17:57:38 +0000

    updated classes documentation

---  terasoft-12/Mechanect/Mechanect/Mechanect/Cameras/ChaseCamera.cs
+++  terasoft-12/Mechanect/Mechanect/Mechanect/Cameras/ChaseCamera.cs
@@ -7,6 +7,12 @@ using Microsoft.Xna.Framework.Graphics;
 
 namespace Mechanect.Cameras
 {
+    /// <summary>
+    /// represents the chase camera type
+    /// </summary>
+    /// <remarks>
+    /// Auther : Bishoy Bassem
+    /// </remarks>
     public class ChaseCamera : Camera
     {
         private Vector3 position;
@@ -17,8 +23,15 @@ namespace Mechanect.Cameras
         private Vector3 positionOffset;
         private Vector3 targetOffset;
 
-        private Vector3 relativeCameraRotation { get; set; }
+        private Vector3 relativeCameraRotation;
 
+        /// <summary>
+        /// constructs a ChaseCamera instance
+        /// </summary>
+        /// <param name="graphicsDevice"></param>
+        /// <param name="positionOffset">offset between the object and the camera positions</param>
+        /// <param name="relativeCameraRotation">start orientation of the camera</param>
+        /// <param name="targetOffset">offset between the object and the camera's target positions</param>
         public ChaseCamera(Vector3 positionOffset, Vector3 targetOffset, Vector3 relativeCameraRotation, GraphicsDevice graphicsDevice)
             : base(graphicsDevice)
         {
@@ -27,16 +40,27 @@ namespace Mechanect.Cameras
             this.relativeCameraRotation = relativeCameraRotation;
         }
 
+        /// <summary>
+        /// changes the objects postion
+        /// </summary>
+        /// <param name="newFollowTargetPosition">the new object's postion</param>
         public void Move(Vector3 newFollowTargetPosition)
         {
             this.followTargetPosition = newFollowTargetPosition;
         }
 
+        /// <summary>
+        /// changes the camera orientation
+        /// </summary>
+        /// <param name="rotationChange">the change value for the orientation</param>
         public void Rotate(Vector3 rotationChange)
         {
             this.relativeCameraRotation += rotationChange;
         }
 
+        /// <summary>
+        /// causes the camera to update its view and projection matrices according to the new object's position and camera orientation
+        /// </summary>
         public override void Update()
         {
             Matrix rotation = Matrix.CreateFromYawPitchRoll(relativeCameraRotation.Y, relativeCameraRotation.X, relativeCameraRotation.Z);
