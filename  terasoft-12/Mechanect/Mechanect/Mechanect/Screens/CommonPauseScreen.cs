c39117f3b3241508d8d82f9303cd54d6d6319a81
khaled.osmaan <khaled.osmaan@113cb9fd-de2b-231c-2074-2ec382a84f02>
2012-04-23 17:06:19 +0000

    update issue 36 Status: In Progress Message: uncommented the lines, errors fixed

---  terasoft-12/Mechanect/Mechanect/Mechanect/Screens/CommonPauseScreen.cs
+++  terasoft-12/Mechanect/Mechanect/Mechanect/Screens/CommonPauseScreen.cs
@@ -1,9 +1,11 @@
-﻿/*using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Mechanect.Classes;
 using Microsoft.Xna.Framework;
+using Mechanect.Common;
+using MechanectXNA;
 
 
 namespace Mechanect.Screens
@@ -25,7 +27,7 @@ namespace Mechanect.Screens
 
         }
 
-        public override void Update(Microsoft.Xna.Framework.GameTime gameTime, bool covered)
+        public override void Update(GameTime gameTime, bool covered)
         {
             base.Update(gameTime, covered);
         }
@@ -36,11 +38,11 @@ namespace Mechanect.Screens
         /// <summary>
         /// Draws the pause screen which should be shown if the user is not detected by the kinect device
         /// </summary>     
-        public override void Draw(Microsoft.Xna.Framework.GameTime gameTime)
+        public override void Draw(GameTime gameTime)
         {
-            //var position = new Vector2(100, 20);
-         //   var I = new Instruction("No Player Detected, Please stand in the correct position then press ok ", position);
-           // I.Draw();
+            var position = new Vector2(100, 20);
+            var I = new Instruction("No Player Detected, Please stand in the correct position then press ok ", position);
+            I.Draw(gameTime);
         }
 
         public override void Remove()
@@ -49,4 +51,3 @@ namespace Mechanect.Screens
         }
     }
 }
-*/
