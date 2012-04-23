504ff8371862cb4df67be6818e53cf8bef6b8902
mohamedra2fat@gmail.com <mohamedra2fat@gmail.com@113cb9fd-de2b-231c-2074-2ec382a84f02>
2012-04-23 14:31:16 +0000

    Modified measureAngle method. Added measureVelocity method. Added instance variables, measuredVelocity, initialLeftHand, currentTime, startTime, startPosition

--- trunk/ terasoft-12/Mechanect/Mechanect/Mechanect/Classes/User2.cs
+++ trunk/ terasoft-12/Mechanect/Mechanect/Mechanect/Classes/User2.cs
@@ -3,12 +3,90 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Microsoft.Kinect;
-using Microsoft.Xna.Framework; 
+using Microsoft.Xna.Framework;
 
-namespace Mechanect.Classes 
+namespace Mechanect.Classes
 {
     class User2 : Mechanect.Classes.User
     {
+
+        private Vector3 finalHandPosition;
+        public Vector3 FinalHandPosition
+        {
+            get
+            {
+                return finalHandPosition;
+            }
+            set
+            {
+                finalHandPosition = value;
+            }
+        }
+
+        private double finalAngle;
+        private double FinalAngle
+        {
+            get
+            {
+                return finalAngle;
+            }
+            set
+            {
+                finalAngle = value;
+            }
+        }
+
+        private double startPosition;
+        public double StartPosition
+        {
+            get
+            {
+                return startPosition;
+            }
+            set
+            {
+                startPosition = USER.Joints[JointType.HandLeft].Position.Y;
+            }
+        }
+
+        private TimeSpan currentTime;
+        public TimeSpan CurrentTime
+        {
+            get
+            {
+                return currentTime;
+            }
+            set
+            {
+                currentTime = value;
+            }
+        }
+
+        private TimeSpan startTime;
+        public TimeSpan StartTime
+        {
+            get
+            {
+                return startTime;
+            }
+            set
+            {
+                startTime = value;
+            }
+        }
+        private Skeleton skeleton;
+        public Skeleton Skeleton
+        {
+            get
+            {
+                return skeleton;
+            }
+            set
+            {
+                skeleton = value;
+            }
+        }
+
 
         private double measuredAngle;
         public double MeasuredAngle
@@ -35,55 +113,125 @@ namespace Mechanect.Classes
                 measuredAngle = value;
             }
         }
-
-
+        private double initialLeftHand;
+        public double InitialLeftHand
+        {
+            get
+            {
+                return initialLeftHand;
+            }
+            set
+            {
+                initialLeftHand = value;
+            }
+        }
+        // the constructor should initialize the initial left hand position to Zero
 
         internal void MeasureVelocityAndAngle()
         {
             //throw new NotImplementedException();
         }
-       /// <summary>
-       /// Gets the angle between two Vectors, from left hip to left shoulder and from left shoulder to left hand
-       /// and convert it to degrees.
-       /// <Author>Mohamed Raafat </Author>
-       /// </summary>
-       /// <remarks>The counter variable ensure to unify difference in Fps of XNA and Kinect</remarks>
-       /// <returns> the angle in degrees</returns>
+
+        /// <summary>
+        /// Checks if user has started moving his hand from his initial position
+        /// </summary>
+        /// <returns>True if moved his hand, false if did not move his hand </returns>
+        public Boolean hasStartedMoving(GameTime gametime)
+        {
+            if ((USER.Joints[JointType.HandLeft].Position.Y) > startPosition)
+            {
+                startTime = gametime.TotalGameTime - gametime.ElapsedGameTime;
+                return true;
+            }
+            else
+                return false;
+
+        }
+
+
+        /// <summary>
+        /// Gets the angle between two Vectors, from left hip to left shoulder and from left shoulder to left hand
+        /// and convert it to degrees.
+        /// <Author>Mohamed Raafat </Author>
+        /// </summary>
+        /// <remarks>The counter variable ensure to unify difference in Fps of XNA and Kinect</remarks>
+        /// <returns> The angle in degrees</returns>
         int counter = 0;
-        public double calulateAngle()
+        public double measureAngle(GameTime gametime)
         {
 
-            counter += 1 ;
-            if (counter % 3 == 0)
+            if (hasStartedMoving(gametime))
             {
-                counter = 0;
-                Vector3 leftHip = new Vector3(USER.Joints[JointType.HipLeft].Position.X
-                    , USER.Joints[JointType.HipLeft].Position.Y
-                    , USER.Joints[JointType.HipLeft].Position.Z);
+                counter += 1;
 
-                Vector3 leftShoulder = new Vector3(USER.Joints[JointType.ShoulderLeft].Position.X
-                    , USER.Joints[JointType.ShoulderLeft].Position.Y
-                    , USER.Joints[JointType.ShoulderLeft].Position.Z);
+                if (counter % 3 == 0)
+                {
+                    counter = 0;
+                    Vector3 leftHip = new Vector3(USER.Joints[JointType.HipLeft].Position.X
+                        , USER.Joints[JointType.HipLeft].Position.Y
+                        , USER.Joints[JointType.HipLeft].Position.Z);
 
-                Vector3 leftHand = new Vector3(USER.Joints[JointType.HandLeft].Position.X
-                    , USER.Joints[JointType.HandLeft].Position.Y
-                    , USER.Joints[JointType.HandLeft].Position.Z);
+                    Vector3 leftShoulder = new Vector3(USER.Joints[JointType.ShoulderLeft].Position.X
+                        , USER.Joints[JointType.ShoulderLeft].Position.Y
+                        , USER.Joints[JointType.ShoulderLeft].Position.Z);
 
-                Vector3 HipToShoulder = new Vector3(leftShoulder.X - leftHip.X, leftShoulder.Y - leftHip.Y, leftShoulder.Z - leftHip.Z);
+                    Vector3 leftHand = new Vector3(USER.Joints[JointType.HandLeft].Position.X
+                        , USER.Joints[JointType.HandLeft].Position.Y
+                        , USER.Joints[JointType.HandLeft].Position.Z);
 
-                Vector3 ShoulderToHand = new Vector3(leftHand.X - leftShoulder.X, leftHand.Y - leftShoulder.Y, leftHand.Z - leftShoulder.Z);
+                    //sets the final coordinates of the left hand
+                    finalHandPosition.X = leftHand.X;
+                    finalHandPosition.Y = leftHand.Y;
+                    finalHandPosition.Z = leftHand.Z;
 
-                HipToShoulder.Normalize();
-                ShoulderToHand.Normalize();
+                    Vector3 HipToShoulder = new Vector3(leftShoulder.X - leftHip.X, leftShoulder.Y - leftHip.Y, leftShoulder.Z - leftHip.Z);
 
-                //return value the angle, direction from the left hip to the hand.
-                double angle = (float)Math.Acos(Vector3.Dot(HipToShoulder, ShoulderToHand));
-                double radianToDegree = (double)angle * 180 / Math.PI;
+                    Vector3 ShoulderToHand = new Vector3(leftHand.X - leftShoulder.X, leftHand.Y - leftShoulder.Y, leftHand.Z - leftShoulder.Z);
 
-                return radianToDegree;
+                    HipToShoulder.Normalize();
+                    ShoulderToHand.Normalize();
+
+                    //return value the angle, direction from the left hip to the hand.
+                    double angle = (float)Math.Acos(Vector3.Dot(HipToShoulder, ShoulderToHand));
+                    double radianToDegree = (double)angle * 180 / Math.PI;
+
+                    this.MeasuredAngle = radianToDegree;
+
+                    if (MeasuredAngle == finalAngle)
+                        currentTime = gametime.TotalGameTime - startTime;
+
+                    finalAngle = radianToDegree;
+
+
+
+                    return radianToDegree;
+                }
+                else
+                    return 0;
             }
             else
                 return 0;
         }
+
+
+
+        /// <summary>
+        /// Calculate the angular velocity and then the linear velocity
+        /// </summary>
+        /// <Author>Mohamed Raafat</Author>
+        /// <returns>Returns the linear velocity of the arm</returns>
+        public double measureVelocity()
+        {
+
+            double angularVelocity = this.finalAngle / currentTime.Seconds;
+            double lengthHipToHand = Math.Sqrt((Math.Pow((finalHandPosition.X - USER.Joints[JointType.HipLeft].Position.X), 2))
+                + Math.Pow((finalHandPosition.Y - USER.Joints[JointType.HipLeft].Position.Y), 2));
+            measuredVelocity = angularVelocity * lengthHipToHand;
+            return angularVelocity * lengthHipToHand;
+
+
+
+        }
+
     }
 }
