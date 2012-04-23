63eb11ac2d56d1ce8cce7b9327efef819d53623c
bishoybassem@gmail.com <bishoybassem@gmail.com@113cb9fd-de2b-231c-2074-2ec382a84f02>
2012-04-23 09:37:40 +0000

    Added missing using statements

---  terasoft-12/Mechanect/Mechanect/Mechanect/Classes/ResultSimulation.cs
+++  terasoft-12/Mechanect/Mechanect/Mechanect/Classes/ResultSimulation.cs
@@ -2,6 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Mechanect.Common;
+using Mechanect.Common.Cameras;
 
 namespace Mechanect.Classes
 {
