da442f711943dccc8f9074cfa247a4d25cd039d7
bishoybassem@gmail.com <bishoybassem@gmail.com@113cb9fd-de2b-231c-2074-2ec382a84f02>
2012-04-23 17:57:38 +0000

    updated classes documentation

--- trunk/ terasoft-12/Mechanect/Mechanect/Mechanect/Cameras/Camera.cs
+++ trunk/ terasoft-12/Mechanect/Mechanect/Mechanect/Cameras/Camera.cs
@@ -7,20 +7,39 @@ using Microsoft.Xna.Framework.Graphics;
 
 namespace Mechanect.Cameras
 {
+    /// <summary>
+    /// represents a generic type for all camera types
+    /// </summary>
+    /// <remarks>
+    /// Auther : Bishoy Bassem
+    /// </remarks>
     public abstract class Camera
     {
-
+        /// <summary>
+        /// the view matrix
+        /// </summary>
         public Matrix view { get; set; }
+        
+        /// <summary>
+        /// the projection matrix
+        /// </summary>
         public Matrix projection { get; set; }
 
-        protected GraphicsDevice GraphicsDevice { get; set; }
+        protected GraphicsDevice graphicsDevice { get; set; }
 
+        /// <summary>
+        /// takes a GraphicsDevice as a parameter
+        /// </summary>
+        /// <param name="graphicsDevice"></param>
         public Camera(GraphicsDevice graphicsDevice)
         {
-            this.GraphicsDevice = graphicsDevice;
+            this.graphicsDevice = graphicsDevice;
             this.projection = Matrix.CreatePerspectiveFieldOfView(MathHelper.ToRadians(45), graphicsDevice.Viewport.AspectRatio, 0.1f, 1000000.0f);
         }
 
+        /// <summary>
+        /// causes the camera to update its view and projection matrices
+        /// </summary>
         public abstract void Update();
 
     }
--- trunk/ terasoft-12/Mechanect/Mechanect/Mechanect/Cameras/TargetCamera.cs
+++ trunk/ terasoft-12/Mechanect/Mechanect/Mechanect/Cameras/TargetCamera.cs
@@ -7,12 +7,24 @@ using Microsoft.Xna.Framework.Graphics;
 
 namespace Mechanect.Cameras
 {
+    /// <summary>
+    /// represents the target camera type
+    /// </summary>
+    /// <remarks>
+    /// Auther : Bishoy Bassem
+    /// </remarks>
     public class TargetCamera : Camera
     {
 
         private Vector3 position;
         private Vector3 target;
 
+        /// <summary>
+        /// constructs a TargetCamera instance
+        /// </summary>
+        /// <param name="graphicsDevice"></param>
+        /// <param name="position">is the position of the camera</param>
+        /// <param name="target">is the postion the camera is looking at</param>
         public TargetCamera(Vector3 position, Vector3 target, GraphicsDevice graphicsDevice)
             : base(graphicsDevice)
         {
@@ -20,6 +32,9 @@ namespace Mechanect.Cameras
             this.target = target;
         }
 
+        /// <summary>
+        /// causes the camera to update its view and projection matrices
+        /// </summary>
         public override void Update()
         {
             this.view = Matrix.CreateLookAt(position, target, Vector3.Up);
