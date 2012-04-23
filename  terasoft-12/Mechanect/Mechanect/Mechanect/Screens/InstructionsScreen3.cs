651e2f89059d24e217e9e5060bcf5338d7b86ae1
khaled.osmaan <khaled.osmaan@113cb9fd-de2b-231c-2074-2ec382a84f02>
2012-04-23 20:25:54 +0000

    update issue 36 status InProgress removed unused imports

---  terasoft-12/Mechanect/Mechanect/Mechanect/Screens/InstructionsScreen3.cs
+++  terasoft-12/Mechanect/Mechanect/Mechanect/Screens/InstructionsScreen3.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using Mechanect.Common;
-using Mechanect.Classes;
+﻿using Mechanect.Common;
 using Microsoft.Xna.Framework;
 using MechanectXNA;
 namespace Mechanect.Screens
