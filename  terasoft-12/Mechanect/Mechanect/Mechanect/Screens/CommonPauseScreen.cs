4432addf23feccbbb9b453aca9894e79532bd222
khaled.osmaan <khaled.osmaan@113cb9fd-de2b-231c-2074-2ec382a84f02>
2012-04-23 15:11:16 +0000

    update issue 30 Status: In Progress Message: class CommonPauseScreen was not viewable by reviewer so I had to recommit it

---
+++  terasoft-12/Mechanect/Mechanect/Mechanect/Screens/CommonPauseScreen.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Mechanect.Classes;
+using Microsoft.Xna.Framework;
+
+
+namespace Mechanect.Screens
+{
+    class CommonPauseScreen : Mechanect.Common.GameScreen
+    {
+        public CommonPauseScreen()
+        {
+        }
+
+
+        public override void LoadContent()
+        {
+           
+        }
+
+        public override void UnloadContent()
+        {
+
+        }
+
+        public override void Update(Microsoft.Xna.Framework.GameTime gameTime, bool covered)
+        {
+            base.Update(gameTime, covered);
+        }
+
+        /// <remarks>
+        ///<para>AUTHOR: Khaled Salah </para>
+        ///</remarks>
+        /// <summary>
+        /// Draws the pause screen which should be shown if the user is not detected by the kinect device
+        /// </summary>     
+        public override void Draw(Microsoft.Xna.Framework.GameTime gameTime)
+        {
+            var position = new Vector2(100, 20);
+            var I = new Instruction("No Player Detected, Please stand in the correct position then press ok ", position);
+            I.Draw();
+            throw new NotImplementedException();
+        }
+
+        public override void Remove()
+        {
+            base.Remove();
+        }
+    }
+}
