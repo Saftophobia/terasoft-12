8c359ea1e752211dcb0ab4e071425c9f836ddaf7
khaled.osmaan <khaled.osmaan@113cb9fd-de2b-231c-2074-2ec382a84f02>
2012-04-23 20:25:10 +0000

    update issue 30 status InProgress removed unused imports

---  terasoft-12/Mechanect/Mechanect/Mechanect/Screens/CommonPauseScreen.cs
+++  terasoft-12/Mechanect/Mechanect/Mechanect/Screens/CommonPauseScreen.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using Mechanect.Classes;
-using Microsoft.Xna.Framework;
+﻿using Microsoft.Xna.Framework;
 using Mechanect.Common;
 using MechanectXNA;
 
