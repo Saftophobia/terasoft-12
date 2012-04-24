3f2a74942514b3dacf1c01214b89030bc980bf0b
bishoybassem@gmail.com <bishoybassem@gmail.com@113cb9fd-de2b-231c-2074-2ec382a84f02>
2012-04-24 14:11:12 +0000

    update issue 206 stats in-progress removed button from InstructionScreen3 class

---  terasoft-12/Mechanect/Mechanect/Mechanect/Screens/InstructionsScreen3.cs
+++  terasoft-12/Mechanect/Mechanect/Mechanect/Screens/InstructionsScreen3.cs
@@ -6,7 +6,7 @@ namespace Mechanect.Screens
     class InstructionsScreen3 : Instruction
     {
         Vector2 position = new Vector2(100, 20);
-        private OKButton button;
+
         string instructions = " The point of this game is to shoot the ball that it reaches the hole with zero velocity";
         public InstructionsScreen3()
             : base()
@@ -18,16 +18,10 @@ namespace Mechanect.Screens
             this.instructions = instructions;
         }
 
-        /// <summary>
-        /// load the screen's content
-        /// </summary>
-        /// <remarks>
-        /// Auther : Bishoy Bassem
-        /// </remarks>
         public override void LoadContent()
         {
             base.LoadContent();
-            button = new OKButton(this.ScreenManager.Game.Content, new Vector2(300, 300), this.ScreenManager.GraphicsDevice.Viewport.Width, this.ScreenManager.GraphicsDevice.Viewport.Height);
+           
         }
 
         public override void UnloadContent()
@@ -35,26 +29,16 @@ namespace Mechanect.Screens
             base.UnloadContent();
         }
 
-        /// <summary>
-        /// updates the screen
-        /// </summary>
-        /// <param name="gameTime"></param>
-        /// <param name="covered"></param>
-        /// <remarks>
-        /// Auther : Bishoy Bassem
-        /// </remarks>
+
         public override void Update(Microsoft.Xna.Framework.GameTime gameTime, bool covered)
         {
 
-            if (button.isClicked())
-            {
-                this.Remove();
-            }
+
             base.Update(gameTime, covered);
         }
 
         /// <remarks>
-        ///<para>AUTHOR: Khaled Salah, Bishoy Bassem </para>
+        ///<para>AUTHOR: Khaled Salah </para>
         ///</remarks>
         /// <summary>
         /// Draws the how to play instruction screen related to experiment3.
@@ -65,8 +49,6 @@ namespace Mechanect.Screens
         public override void Draw(GameTime gameTime)
         {
             base.Draw(gameTime);
-
-            button.draw(this.ScreenManager.SpriteBatch);
         }
 
         public override void Remove()
