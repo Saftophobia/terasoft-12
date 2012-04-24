7d858b7f7157487d6fe401745ab04fed8947a3b6
3abdelazim@gmail.com <3abdelazim@gmail.com@113cb9fd-de2b-231c-2074-2ec382a84f02>
2012-04-24 21:39:54 +0000

    0.09 Added documentation to all methods

---  terasoft-12/Mechanect/Mechanect/Mechanect/Screens/AdjustPosition.cs
+++  terasoft-12/Mechanect/Mechanect/Mechanect/Screens/AdjustPosition.cs
@@ -43,6 +43,16 @@ namespace Mechanect.Screens
 
         SpriteFont font;
 
+
+        ///<remarks>
+        ///<para>
+        ///Author: Mohamed AbdelAzim
+        ///</para>
+        ///</remarks>
+        /// <summary>
+        /// gets the content manager from the screen manager
+        /// and returns it.
+        /// </summary>
         ContentManager ContentManager
         {
             get
@@ -51,6 +61,20 @@ namespace Mechanect.Screens
             }
         }
 
+
+        ///<remarks>
+        ///<para>
+        ///Author: Mohamed AbdelAzim
+        ///</para>
+        ///</remarks>
+        /// <summary>
+        /// <returns>returns object "AdjustPosition"</returns>
+        /// </summary>
+        /// <param name="user"> the object User which tracks the skeleton of the player</param>
+        /// <param name="minDepth"> an integer representing the minimum distance in centimeters the player should stand at.</param>
+        /// <param name="maxDepth"> an integer representing the maximum distance in centimeters the player should stand at.</param>
+        /// <param name="minAngle"> a float representing the minimum angle the player should make with the kinect sensor.</param>
+        /// <param name="maxAngle"> a float representing the minimum angle the player should make with the kinect sensor.</param>
         public AdjustPosition(User user, int minDepth, int maxDepth, float minAngle, float maxAngle)
         {
             users = new User[1];
@@ -61,6 +85,21 @@ namespace Mechanect.Screens
             this.maxAngle = maxAngle;
         }
 
+
+        ///<remarks>
+        ///<para>
+        ///Author: Mohamed AbdelAzim
+        ///</para>
+        ///</remarks>
+        /// <summary>
+        /// <returns>returns object "AdjustPosition"</returns>
+        /// </summary>
+        /// <param name="user1"> the object User which tracks the skeleton of the first player</param>
+        /// <param name="user2"> the object User which tracks the skeleton of the second player</param>
+        /// <param name="minDepth"> an integer representing the minimum distance in centimeters players should stand at.</param>
+        /// <param name="maxDepth"> an integer representing the maximum distance in centimeters players should stand at.</param>
+        /// <param name="minAngle"> a float representing the minimum angle players should make with the kinect sensor.</param>
+        /// <param name="maxAngle"> a float representing the minimum angle players should make with the kinect sensor.</param>
         public AdjustPosition(User user1,User user2 ,int minDepth, int maxDepth, float minAngle, float maxAngle)
         {
             users = new User[2];
@@ -72,6 +111,10 @@ namespace Mechanect.Screens
             this.maxAngle = maxAngle;
         }
 
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
         public override void LoadContent()
         {
             title = "Adjust Position";
@@ -100,10 +143,16 @@ namespace Mechanect.Screens
             font = ContentManager.Load<SpriteFont>("Ariel");
         }
 
-        public override void UnloadContent()
-        {
 
-        }
+        ///<remarks>
+        ///<para>
+        ///Author: Mohamed AbdelAzim
+        ///</para>
+        ///</remarks>
+        /// <summary>
+        /// <returns>returns the distance of users[ID] from the kinect sensor</returns>
+        /// </summary>
+        /// <param name="ID"> the index of the User in the users array</param>
         public int getDepth(int ID)
         {
             if (ID < users.Length)
@@ -111,6 +160,19 @@ namespace Mechanect.Screens
             else return 0;
         }
 
+
+        ///<remarks>
+        ///<para>
+        ///Author: Mohamed AbdelAzim
+        ///</para>
+        ///</remarks>
+        /// <summary>
+        /// <returns>returns the angle users[ID] makes with the kinect sensor. </returns>
+        /// <example>a player standing facing the kinect sensor will have zero angle, </example>
+        /// <example>a player turned to his right with respect to the kinect sensor will a positive angle, </example>
+        /// <example>a player turned to his left with respect to the kinect sensor will a negative angle, </example>
+        /// </summary>
+        /// <param name="ID"> the index of the User in the users array</param>
         public float getAngle(int ID)
         {
             if (ID < users.Length)
@@ -125,11 +187,35 @@ namespace Mechanect.Screens
             else return 0;
         }
 
+        ///<remarks>
+        ///<para>
+        ///Author: Mohamed AbdelAzim
+        ///</para>
+        ///</remarks>
+        /// <summary>
+        /// <returns>returns a color according to the location with respect to the start and end points of the gradient. </returns>
+        /// </summary>
+        /// <param name="start"> the start position of the gradient</param>
+        /// <param name="end"> the end position of the gradient</param>
+        /// <param name="index"> the pixel's position</param>
+        /// <param name="top"> the color at the top of the gradient</param>
+        /// <param name="bot"> the color at the bottom of the gradient</param>
         public Color color(int start, int end, int index, Color top, Color bot)
         {
-            return new Color((bot.R * (index - start) + top.R * (end - index)) / (end - start), (bot.G * (index - start) + top.G * (end - index)) / (end - start), (bot.B * (index - start) + top.B * (end - index)) / (end - start));
+            int R = (bot.R * (index - start) + top.R * (end - index)) / (end - start);
+            int G = (bot.G * (index - start) + top.G * (end - index)) / (end - start);
+            int B = (bot.B * (index - start) + top.B * (end - index)) / (end - start);
+            return new Color(R, G, B);
         }
 
+        ///<remarks>
+        ///<para>
+        ///Author: Mohamed AbdelAzim
+        ///</para>
+        ///</remarks>
+        /// <summary>
+        /// updates the gradient in the Depth bar to represent the players
+        /// </summary>
         public void updateDepthBar()
         {
             Color[] data = new Color[depthBarHeight];
@@ -157,6 +243,16 @@ namespace Mechanect.Screens
             depthBar.SetData(finalData);
         }
 
+
+        ///<remarks>
+        ///<para>
+        ///Author: Mohamed AbdelAzim
+        ///</para>
+        ///</remarks>
+        /// <summary>
+        /// Updates the User's depth and angle, and updates the command the user shall see
+        /// </summary>
+        /// <param name="ID"> The ID of the user in users[]</param>
         public void UpdateUser(int ID)
         {
             if (users[ID].USER == null)
@@ -194,6 +290,16 @@ namespace Mechanect.Screens
             }
         }
 
+        ///<remarks>
+        ///<para>
+        ///Author: Mohamed AbdelAzim
+        ///</para>
+        ///</remarks>
+        /// <summary>
+        /// Runs every frame gathering players' data and updating screen parameters.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        /// <param name="covered">determines whether the screen is covered by another screen or not.</param>
         public override void Update(GameTime gameTime, Boolean covered)
         {
             for (int i = 0; i < users.Length; i++)
@@ -202,6 +308,15 @@ namespace Mechanect.Screens
             updateDepthBar();
         }
 
+        ///<remarks>
+        ///<para>
+        ///Author: Mohamed AbdelAzim
+        ///</para>
+        ///</remarks>
+        /// <summary>
+        /// This is called when the screen should draw itself. displays depth bar and user's rules and commands that allow him to stand correctly
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
         public override void Draw(Microsoft.Xna.Framework.GameTime gameTime)
         {
             ScreenManager.SpriteBatch.Begin();
@@ -222,13 +337,6 @@ namespace Mechanect.Screens
             ScreenManager.SpriteBatch.Begin();
             ScreenManager.SpriteBatch.Draw(depthBar, new Vector2(100, 520), Color.White);
             ScreenManager.SpriteBatch.End();
-        }
-
-
-
-        public override void Remove()
-        {
-            base.Remove();
         }
     }
 }
