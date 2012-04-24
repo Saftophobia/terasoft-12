c395581a1cf23f07292120a04e96370b59ea02bb
khaled.osmaan <khaled.osmaan@113cb9fd-de2b-231c-2074-2ec382a84f02>
2012-04-24 13:54:24 +0000

    update issue 30 status inProgress Let the class extend class Instruction and use its base (super) methods, implementation might need to be reviewed again

---  terasoft-12/Mechanect/Mechanect/Mechanect/Screens/CommonPauseScreen.cs
+++  terasoft-12/Mechanect/Mechanect/Mechanect/Screens/CommonPauseScreen.cs
@@ -1,25 +1,36 @@
 ﻿using Microsoft.Xna.Framework;
-using Mechanect.Common;
 using MechanectXNA;
 
 
 namespace Mechanect.Screens
 {
-    class CommonPauseScreen : GameScreen
+    class CommonPauseScreen : Instruction
     {
+        Vector2 position = new Vector2(100, 20);
+        string instructions = "User is not detected by kinect device please stand in correct position then press ok";
         public CommonPauseScreen()
+            : base()
         {
         }
 
+        public CommonPauseScreen(string instructions)
+            : base(instructions)
+        {
+            this.instructions = instructions;
+        }
 
+        public override void Initialize()
+        {
+            base.Initialize();
+        }
         public override void LoadContent()
         {
-           
+            base.LoadContent();
         }
 
         public override void UnloadContent()
         {
-
+            base.UnloadContent();
         }
 
         public override void Update(GameTime gameTime, bool covered)
@@ -35,15 +46,14 @@ namespace Mechanect.Screens
         /// </summary>     
         public override void Draw(GameTime gameTime)
         {
-            var position = new Vector2(100, 20);
-            var I = new Instruction("No Player Detected, Please stand in the correct position then press ok ", position);
-            I.Draw(gameTime);
-
+            base.Draw(gameTime);
         }
 
         public override void Remove()
         {
             base.Remove();
         }
+
+
     }
 }
