712f74788075b2f447dc7f5222f25030cb593c35
khaled.osmaan <khaled.osmaan@113cb9fd-de2b-231c-2074-2ec382a84f02>
2012-04-25 11:52:27 +0000

    update issue 36 status inProgress fixed documentation, edited empty constructor, replaced Microsoft.Xna.Framework.GameTime with GameTime, removed empty method.

---  terasoft-12/Mechanect/Mechanect/Mechanect/Screens/InstructionsScreen3.cs
+++  terasoft-12/Mechanect/Mechanect/Mechanect/Screens/InstructionsScreen3.cs
@@ -7,31 +7,68 @@ namespace Mechanect.Screens
 
         string instructions = " The point of this game is to shoot the ball that it reaches the hole with zero velocity";
         public InstructionsScreen3()
-            : base()
         {
+            new Instruction(instructions);
         }
+
         public InstructionsScreen3(string instructions)
             : base(instructions)
         {
             this.instructions = instructions;
         }
 
+        /// <remarks>
+        ///<para>AUTHOR: Khaled Salah </para>
+        ///</remarks>
+        /// <summary>
+        /// Allows the screen to perform any initialization it needs to before starting to draw.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+
+        public override void Initialize()
+        {
+            base.Initialize();
+        }
+        /// <remarks>
+        ///<para>AUTHOR: Khaled Salah </para>
+        ///</remarks>
+        /// <summary>
+        /// LoadContent will be called only once before drawing and its the place to load
+        /// all of your content.
+        /// </summary>
+        
         public override void LoadContent()
         {
             base.LoadContent();
            
         }
-
+        /// <remarks>
+        ///<para>AUTHOR: Khaled Salah </para>
+        ///</remarks>
+        /// <summary>
+        /// UnloadContent will be called only once and its the place to unload
+        /// all content.
+        /// </summary>        
+        
         public override void UnloadContent()
         {
             base.UnloadContent();
         }
 
-
-        public override void Update(Microsoft.Xna.Framework.GameTime gameTime, bool covered)
+        /// <remarks>
+        ///<para>AUTHOR: Khaled Salah </para>
+        ///</remarks>
+        /// <summary>
+        /// Allows the game screen to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        /// <param name="covered">Determines whether you want this screen to be covered by another screen or not.</param>
+        
+        public override void Update(GameTime gameTime, bool covered)
         {
-
-
             base.Update(gameTime, covered);
         }
 
@@ -39,19 +76,24 @@ namespace Mechanect.Screens
         ///<para>AUTHOR: Khaled Salah </para>
         ///</remarks>
         /// <summary>
-        /// Draws the how to play instruction screen related to experiment3.
+        /// This is called when the game screen should draw itself.
         /// </summary>
-        /// <param name="gameTime">
-        /// An instance variable of class GameTime in Microsoft.Xna.Framework.GameTime package.
-        /// </param>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>    
         public override void Draw(GameTime gameTime)
         {
             base.Draw(gameTime);
         }
 
+        /// <remarks>
+        ///<para>AUTHOR: Khaled Salah </para>
+        ///</remarks>
+        /// <summary>
+        /// Removes this screen from the screen manager.
+        /// </summary>
         public override void Remove()
         {
             base.Remove();
         }
+
     }
 }
