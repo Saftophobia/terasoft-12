af5ea2b3ba2511cfeb574665dce38e91a7c1316b
khaled.osmaan <khaled.osmaan@113cb9fd-de2b-231c-2074-2ec382a84f02>
2012-04-25 11:47:39 +0000

    update issue 30 status inProgress added/fixed documentations, edited empty constructor

---  terasoft-12/Mechanect/Mechanect/Mechanect/Screens/CommonPauseScreen.cs
+++  terasoft-12/Mechanect/Mechanect/Mechanect/Screens/CommonPauseScreen.cs
@@ -8,8 +8,8 @@ namespace Mechanect.Screens
         Vector2 position = new Vector2(100, 20);
         string instructions = "User is not detected by kinect device please stand in correct position then press ok";
         public CommonPauseScreen()
-            : base()
         {
+            new Instruction(instructions);
         }
 
         public CommonPauseScreen(string instructions)
@@ -17,21 +17,51 @@ namespace Mechanect.Screens
         {
             this.instructions = instructions;
         }
-
+        /// <remarks>
+        ///<para>AUTHOR: Khaled Salah </para>
+        ///</remarks>
+        /// <summary>
+        /// Allows the screen to perform any initialization it needs to before starting to draw.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+         
         public override void Initialize()
         {
             base.Initialize();
         }
+        /// <remarks>
+        ///<para>AUTHOR: Khaled Salah </para>
+        ///</remarks>
+        /// <summary>
+        /// LoadContent will be called only once before drawing and its the place to load
+        /// all of your content.
+        /// </summary>
         public override void LoadContent()
         {
             base.LoadContent();
         }
-
-        public override void UnloadContent()
+        /// <remarks>
+        ///<para>AUTHOR: Khaled Salah </para>
+        ///</remarks>
+        /// <summary>
+        /// UnloadContent will be called only once and its the place to unload
+        /// all content.
+        /// </summary>        
+         public override void UnloadContent()
         {
             base.UnloadContent();
         }
-
+        /// <remarks>
+        ///<para>AUTHOR: Khaled Salah </para>
+        ///</remarks>
+        /// <summary>
+        /// Allows the game screen to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        /// <param name="covered">Determines whether you want this screen to be covered by another screen or not.</param>
         public override void Update(GameTime gameTime, bool covered)
         {
             base.Update(gameTime, covered);
@@ -41,18 +71,24 @@ namespace Mechanect.Screens
         ///<para>AUTHOR: Khaled Salah </para>
         ///</remarks>
         /// <summary>
-        /// Draws the pause screen which should be shown if the user is not detected by the kinect device.
-        /// </summary>     
+        /// This is called when the game screen should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>    
         public override void Draw(GameTime gameTime)
         {
             base.Draw(gameTime);
         }
 
+        /// <remarks>
+        ///<para>AUTHOR: Khaled Salah </para>
+        ///</remarks>
+        /// <summary>
+        /// Removes this screen from the screen manager.
+        /// </summary>
         public override void Remove()
         {
             base.Remove();
         }
-
 
     }
 }
