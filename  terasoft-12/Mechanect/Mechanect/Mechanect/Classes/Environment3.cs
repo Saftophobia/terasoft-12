5d073b42536041c32f0ab0800e053205f1d9e6c0
q3ahmd@gmail.com <q3ahmd@gmail.com@113cb9fd-de2b-231c-2074-2ec382a84f02>
2012-04-25 08:50:39 +0000

    update issue 14 Status: In Progress
    Modified the instantiation line of the distanceBar to make it's position, totally, relative to the screen size

---  terasoft-12/Mechanect/Mechanect/Mechanect/Classes/Environment3.cs
+++  terasoft-12/Mechanect/Mechanect/Mechanect/Classes/Environment3.cs
@@ -60,7 +60,7 @@ namespace Mechanect.Classes
             Vector3 ballPos = ball.Position;
             Vector3 ballInitPos = ball.InitialBallPosition;
             Vector3 shootingPos = user.ShootingPosition;
-            distanceBar = new Bar(new Vector2(device.Viewport.Width - 10, device.Viewport.Height - 10), spriteBatch, new Vector2(ballInitPos.X, ballInitPos.Z), new Vector2(ballPos.X, ballPos.Z), new Vector2(shootingPos.X, shootingPos.Z), Content);
+            distanceBar = new Bar(new Vector2((0.95f*device.Viewport.Width), (0.90f*device.Viewport.Height)), spriteBatch, new Vector2(ballInitPos.X, ballInitPos.Z), new Vector2(ballPos.X, ballPos.Z), new Vector2(shootingPos.X, shootingPos.Z), Content);
         }
 
 
