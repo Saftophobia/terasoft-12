237a4b916f2ddefc9d03c9b9e3bfebe8a84e1c74
hollaxyman@gmail.com <hollaxyman@gmail.com@113cb9fd-de2b-231c-2074-2ec382a84f02>
2012-04-25 20:16:30 +0000

    Update Issue : 22 Status : In-Progress
    Refined all the code for effiency and conventions

--- trunk/ terasoft-12/Mechanect/Mechanect/Mechanect/Classes/Tools2.cs
+++ trunk/ terasoft-12/Mechanect/Mechanect/Mechanect/Classes/Tools2.cs
@@ -3,31 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Windows;
+using Microsoft.Xna.Framework;
 
 namespace Mechanect.Classes
 {
     class Tools2
     {
     
-     
-       /// <summary>
-        /// Generate 2D point randomly through given range.
+      
+        
+        /// <summary>
+        /// Generate 2D vector point randomly through given range.
         /// </summary>
-         /// <remarks>
+        /// <remarks>
         /// <para>AUTHOR: Tamer Nabil </para>
         /// </remarks>
-        /// <param name="x"></param>
-        /// <param name="y"></param>
-        /// <returns>returns Point </returns>
-        public Point generate2Dpoint(int x,int y)
+        /// <param name="minX"></param>
+        /// <param name="maxX"></param>
+        /// <param name="minY"></param>
+        /// <param name="maxY"></param>
+        /// <returns>returns Vector2 with the random x and y</returns>
+        
+        public Vector2 Generate2Dpoint(float minX, float maxX, float minY, float maxY)
         {
+            Vector2 vec = new Vector2();
             Random rand = new Random();
-           int xpoint = rand.Next(x,y);
-           int ypoint = rand.Next(x,y);
-            Point point = new Point(xpoint,ypoint);
-
-            return point;
+           
+             vec.X = rand.Next((int)minX,(int)maxX);
+            vec.Y = rand.Next((int)minY,(int)maxY);
+            return vec;
 
         }
     }
 }
+
