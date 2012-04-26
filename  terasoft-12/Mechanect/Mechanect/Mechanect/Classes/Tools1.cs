3ad190b498f3d3b568aeddd0b63b23daf37a8c7c
michel.nader.mansour@gmail.com <michel.nader.mansour@gmail.com@113cb9fd-de2b-231c-2074-2ec382a84f02>
2012-04-26 09:49:03 +0000

    Changed the logic in IncreasingAcceleration, DecreasingAcceleration and CommandSatisfied, and edited variable names and types in CheckEachSecond and SetPositions Update issue 24 Changed the logic in IncreasingAcceleration, DecreasingAcceleration and CommandSatisfied, and edited variable names and types in CheckEachSecond and SetPositions

---  terasoft-12/Mechanect/Mechanect/Mechanect/Classes/Tools1.cs
+++  terasoft-12/Mechanect/Mechanect/Mechanect/Classes/Tools1.cs
@@ -127,8 +127,8 @@ namespace Mechanect.Classes
          /// this method should be called on each update and it will do the check on both players to see if they followed the commands
          /// </summary>
          /// <param name="timeInSeconds">the second the game is in</param>
-         /// <param name="User11">the first User1</param>
-         /// <param name="User12">the second User1</param>
+         /// <param name="user11">the first User1</param>
+         /// <param name="user12">the second User1</param>
          /// <param name="timeOfCommands">the time of each command throughout the whole race</param>
          /// <param name="CurrentCommands">the list of commands that should be satisfied for the whole race</param>
          /// <param name="tolerance">the tolerance level</param>
@@ -137,38 +137,38 @@ namespace Mechanect.Classes
          /// <remarks>
          /// <para>AUTHOR: Michel Nader </para>
          /// <para>DATE WRITTEN: 19/4/12 </para>
-         /// <para>DATE MODIFIED: 23/4/12 </para>
+         /// <para>DATE MODIFIED: 26/4/12 </para>
          /// </remarks>
-         public static void CheckEachSecond(int timeInSeconds, User1 User11, User1 User12, List<int> timeOfCommands, List<String> currentCommands, float tolerance, SpriteBatch spriteBatch, SpriteFont spFont)
+         public static void CheckEachSecond(int timeInSeconds, User1 user11, User1 user12, List<int> timeOfCommands, List<String> currentCommands, float tolerance, SpriteBatch spriteBatch, SpriteFont spFont)
          {
-             int pastSecondsFor1 = 4;
-             for (int i = 0; i < User11.ActiveCommand; i++)
+             int pastSecondsFor1 = 0;
+             for (int i = 0; i < user11.ActiveCommand; i++)
                  pastSecondsFor1 += timeOfCommands[i];
 
-             int pastSecondsFor2 = 4;
-             for (int i = 0; i < User11.ActiveCommand; i++)
+             int pastSecondsFor2 = 0;
+             for (int i = 0; i < user12.ActiveCommand; i++)
                  pastSecondsFor2 += timeOfCommands[i];
 
              List<float> User11Displacement = new List<float>();
              List<float> User12Displacement = new List<float>();
-             for (int i = (pastSecondsFor1 - 1) * 12; i < User11.Positions.Count; i++)
-                 User11Displacement.Add(User11.Positions[i]);
+             for (int i = (pastSecondsFor1 - 1) * 12; i < user11.Positions.Count; i++)
+                 User11Displacement.Add(user11.Positions[i]);
 
-             for (int i = (pastSecondsFor2 - 1) * 12; i < User12.Positions.Count; i++)
-                 User12Displacement.Add(User12.Positions[i]);
+             for (int i = (pastSecondsFor2 - 1) * 12; i < user12.Positions.Count; i++)
+                 User12Displacement.Add(user12.Positions[i]);
 
-             if (!CommandSatisfied(currentCommands[User11.ActiveCommand], User11Displacement, tolerance))
+             if (!CommandSatisfied(currentCommands[user11.ActiveCommand], User11Displacement, tolerance))
              {
-                 User11.Disqualified = true;
-                 User11.DisqualificationTime = timeInSeconds;
+                 user11.Disqualified = true;
+                 user11.DisqualificationTime = timeInSeconds;
                  spriteBatch.Begin();
                  spriteBatch.DrawString(spFont, "User1 1 got Disqualified", new Vector2(50.0f, 50.0f), Color.Red);
                  spriteBatch.End();
              }
-             if (!CommandSatisfied(currentCommands[User12.ActiveCommand], User12Displacement, tolerance))
+             if (!CommandSatisfied(currentCommands[user12.ActiveCommand], User12Displacement, tolerance))
              {
-                 User12.Disqualified = true;
-                 User12.DisqualificationTime = timeInSeconds;
+                 user12.Disqualified = true;
+                 user12.DisqualificationTime = timeInSeconds;
                  spriteBatch.Begin();
                  spriteBatch.DrawString(spFont, "User1 2 got Disqualified", new Vector2(50.0f, 50.0f), Color.Blue);
                  spriteBatch.End();
@@ -186,12 +186,10 @@ namespace Mechanect.Classes
          /// <remarks>
          /// <para>AUTHOR: Michel Nader </para>
          /// <para>DATE WRITTEN: 19/4/12 </para>
-         /// <para>DATE MODIFIED: 20/4/12 </para>
+         /// <para>DATE MODIFIED: 26/4/12 </para>
          /// </remarks>
-         public static bool SetPositions(User1 User1, User1 User2, SpriteBatch spriteBatch, SpriteFont spFont, float tolerance)
+         public static bool SetPositions(Skeleton sk1, Skeleton sk2, SpriteBatch spriteBatch, SpriteFont spFont, float tolerance)
          {
-             Skeleton sk1 = User1.USER;
-             Skeleton sk2 = User1.USER;
              bool result = true;
 
                  float z;
@@ -230,21 +228,21 @@ namespace Mechanect.Classes
          /// <summary>
          /// this method checks whether the User1 is standing in about 4m or not(with certain tolerance)
          /// </summary>
-         /// <param name="UserPosition">the position that the User1 is currently standing at</param>
+         /// <param name="userPosition">the position that the User1 is currently standing at</param>
          /// <param name="tolerance">the tolerance level</param>
          /// <returns>if the position sent is in the range or not</returns>
          /// <remarks>
          /// <para>AUTHOR: Michel Nader </para>
          /// <para>DATE WRITTEN: 19/4/12 </para>
-         /// <para>DATE MODIFIED: 20/4/12 </para>
+         /// <para>DATE MODIFIED: 26/4/12 </para>
          /// </remarks>
-         public static bool CheckPosition(float UserPosition, float tolerance)
+         public static bool CheckPosition(float userPosition, float tolerance)
          {
              //initiate the minimum distance from the Kinect which means
              //the maximum distance between the two players which is actually the tolerance on a scale from 0-10 mm
              float min = 4.0f - (tolerance / 1000);
              //check if the actual position of the player is within the range or not
-             if (UserPosition >= min && UserPosition <= 4.0f)
+             if (userPosition >= min && userPosition <= 4.0f)
                  //if it is within the range then it will be true
                  return true;
              //if it is out of this range and/or the kinect range it will be false
@@ -261,40 +259,40 @@ namespace Mechanect.Classes
          /// <remarks>
          /// <para>AUTHOR: Michel Nader </para>
          /// <para>DATE WRITTEN: 19/4/12 </para>
-         /// <para>DATE MODIFIED: 23/4/12 </para>
+         /// <para>DATE MODIFIED: 26/4/12 </para>
          /// </remarks>
          public static bool CommandSatisfied(String command, List<float> positions, float tolerance)
          {
              bool result = true;
-             float currentTolerance = (10 - tolerance) / 100;
+             float currentTolerance = 10 - tolerance;
 
-             if (command.Equals("constantVelocity"))
+             if (command.Equals("ConstantVelocity"))
              {
-                 result = ConstantVelocity(positions, currentTolerance);
+                 result = ConstantVelocity(positions, currentTolerance / 10);
              }
              else
              {
-                 if (command.Equals("constantAcceleration"))
+                 if (command.Equals("ConstantAcceleration"))
                  {
-                     result = ConstantAcceleration(positions, currentTolerance);
+                     result = ConstantAcceleration(positions, currentTolerance / 10);
                  }
                  else
                  {
-                     if (command.Equals("constantDisplacement"))
+                     if (command.Equals("ConstantDisplacement"))
                      {
-                         result = ConstantDisplacement(positions, currentTolerance);
+                         result = ConstantDisplacement(positions, currentTolerance / 100);
                      }
                      else
                      {
-                         if (command.Equals("increasingAcceleration"))
+                         if (command.Equals("IncreasingAcceleration"))
                          {
-                             result = IncreasingAcceleration(positions, currentTolerance);
+                             result = IncreasingAcceleration(positions, currentTolerance / 10);
                          }
                          else
                          {
-                             if (command.Equals("decreasingAcceleration"))
+                             if (command.Equals("DecreasingAcceleration"))
                              {
-                                 result = DecreasingAcceleration(positions, currentTolerance);
+                                 result = DecreasingAcceleration(positions, currentTolerance / 10);
                              }
                          }
                      }
@@ -420,7 +418,7 @@ namespace Mechanect.Classes
          /// <remarks>
          /// <para>AUTHOR: Michel Nader </para>
          /// <para>DATE WRITTEN: 23/4/12 </para>
-         /// <para>DATE MODIFIED: 23/4/12 </para>
+         /// <para>DATE MODIFIED: 26/4/12 </para>
          /// </remarks>
          public static bool IncreasingAcceleration(List<float> positions, float currentTolerance)
          {
@@ -428,11 +426,10 @@ namespace Mechanect.Classes
 
 
              bool result = true;
-             float firstAcceleration = accelerations[1] - accelerations[0];
-             for (int i = 2; i < accelerations.Count; i++)
+             for (int i = 1; i < accelerations.Count; i++)
              {
-                 float currentAcceleration = (accelerations[i] - accelerations[i - 1]);
-                 if (!(currentAcceleration >= (firstAcceleration - currentTolerance)))
+                 float currentAcceleration = accelerations[i];
+                 if (!(currentAcceleration > (accelerations[i - 1] - currentTolerance)))
                 {
                     if (positions[positions.Count - 1] == 0.8)
                     {
@@ -458,7 +455,7 @@ namespace Mechanect.Classes
          /// <remarks>
          /// <para>AUTHOR: Michel Nader </para>
          /// <para>DATE WRITTEN: 23/4/12 </para>
-         /// <para>DATE MODIFIED: 23/4/12 </para>
+         /// <para>DATE MODIFIED: 26/4/12 </para>
          /// </remarks>
          public static bool DecreasingAcceleration(List<float> positions, float currentTolerance)
          {
@@ -466,11 +463,10 @@ namespace Mechanect.Classes
 
 
              bool result = true;
-             float firstAcceleration = accelerations[1] - accelerations[0];
-             for (int i = 2; i < accelerations.Count; i++)
+             for (int i = 1; i < accelerations.Count; i++)
              {
-                 float currentAcceleration = (accelerations[i] - accelerations[i - 1]);
-                 if (!(currentAcceleration <= (firstAcceleration - currentTolerance)))
+                 float currentAcceleration = accelerations[i];
+                 if (!(currentAcceleration < (accelerations[i - 1] + currentTolerance)))
                  {
                      if (positions[positions.Count - 1] == 0.8)
                      {
