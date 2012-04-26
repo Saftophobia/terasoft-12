484ffddfac83d1cc40581c798e596df248a7606b
khaled.osmaan <khaled.osmaan@113cb9fd-de2b-231c-2074-2ec382a84f02>
2012-04-26 14:35:43 +0000

    update issue 32 status inProgress implemented the common Instruction screen which should be shown at the beginning before starting any of the 3 experiments

---  terasoft-12/Mechanect/Mechanect/Mechanect/Screens/AllExperiments.cs
+++  terasoft-12/Mechanect/Mechanect/Mechanect/Screens/AllExperiments.cs
@@ -1,42 +1,73 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
+﻿using Microsoft.Xna.Framework;
+using Mechanect.Common;
+using Microsoft.Xna.Framework.Graphics;
 namespace Mechanect.Screens
 {
-    class AllExperiments:Mechanect.Common.GameScreen
+    class AllExperiments : GameScreen
     {
-             public AllExperiments()
+        string instructions = "Welcome to mechanect made by terasoft";
+        Instruction instruction;
+
+        public AllExperiments()
         {
         }
-
-          public override void LoadContent()
+        public AllExperiments(string instructions)
         {
-            base.LoadContent();
+            this.instructions = instructions;
+        }
+        /// <remarks>
+        ///<para>AUTHOR: Khaled Salah </para>
+        ///</remarks>
+        /// <summary>
+        /// LoadContent will be called only once before drawing and its the place to load
+        /// all of your content.
+        /// </summary>
+
+        public override void LoadContent()
+        {
+            instruction = new Instruction(instructions, ScreenManager.Game.Content, ScreenManager.SpriteBatch, ScreenManager.GraphicsDevice);
+            instruction.Font1 = ScreenManager.Game.Content.Load<SpriteFont>("SpriteFont1");
+            instruction.MyTexture = ScreenManager.Game.Content.Load<Texture2D>(@"Textures/screen");
+
         }
 
-        public override void UnloadContent()
+
+        /// <remarks>
+        ///<para>AUTHOR: Khaled Salah </para>
+        ///</remarks>
+        /// <summary>
+        /// Allows the game screen to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        /// <param name="covered">Determines whether you want this screen to be covered by another screen or not.</param>
+
+        public override void Update(GameTime gameTime, bool covered)
         {
-            base.UnloadContent();
+            if (instruction.Button.isClicked())
+            {
+                ExitScreen();
+            }
+            instruction.Button.update(gameTime);
+            base.Update(gameTime, false);
         }
 
-        public override void Update(Microsoft.Xna.Framework.GameTime gameTime, bool covered)
+        /// <remarks>
+        ///<para>AUTHOR: Khaled Salah </para>
+        ///</remarks>
+        /// <summary>
+        /// This is called when the game screen should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>    
+        public override void Draw(GameTime gameTime)
         {
-            base.Update(gameTime, covered);
+            instruction.Draw(gameTime);
         }
-
-        public override void Draw(Microsoft.Xna.Framework.GameTime gameTime)
-        {
-            throw new NotImplementedException();
-        }
-
-
-
         public override void Remove()
         {
             base.Remove();
+
         }
-    
+
     }
 }
