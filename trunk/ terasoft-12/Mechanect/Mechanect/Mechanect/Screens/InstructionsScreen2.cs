20a8b11be83c4878b8cd17ab655f5c3e52096ca3
3abdelazim@gmail.com <3abdelazim@gmail.com@113cb9fd-de2b-231c-2074-2ec382a84f02>
2012-04-27 05:05:21 +0000

    class InstructionsScreen2.cs (by Tamer) Integrating code with others Added class InstructionsScreen2 that displays How to play instructions, it was written by tamer but he had no Internet connection so I committed it instead

---
+++ trunk/ terasoft-12/Mechanect/Mechanect/Mechanect/Screens/InstructionsScreen2.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.Xna.Framework;
+using Mechanect.Common;
+using Microsoft.Xna.Framework.Graphics;
+using Mechanect.Classes;
+namespace Mechanect.Screens
+{
+    class InstructionsScreen2 : GameScreen
+    {
+        string instructions = " Hey there instructions : \n \nThe Goal of the game is to throw the fish in the aquarium passing by the prey. You should move your arm from back to front to a certain angle with some velocity to determine the angle and velocity of projectile.";
+        Instruction instruction;
+        User2 user2;
+
+        public InstructionsScreen2(User2 user2)
+        {
+            this.user2 = user2;
+        }
+        public InstructionsScreen2(string instructions, User2 user)
+        {
+            this.instructions = instructions;
+            user2 = user;
+        }
+
+        public override void LoadContent()
+        {
+            instruction = new Instruction(instructions, ScreenManager.Game.Content, ScreenManager.SpriteBatch, ScreenManager.GraphicsDevice, user2);
+            instruction.Font1 = ScreenManager.Game.Content.Load<SpriteFont>("SpriteFont1");
+            instruction.MyTexture = ScreenManager.Game.Content.Load<Texture2D>(@"Textures/screen");
+
+        }
+
+        public override void Update(GameTime gameTime, bool covered)
+        {
+            user2.setSkeleton();
+            instruction.Button.Update(gameTime);
+            
+            if (instruction.Button.IsClicked())
+            {
+                ExitScreen();
+                ScreenManager.AddScreen(new AdjustPosition(user2, 150, 350, 10, 50,2));
+            }
+            base.Update(gameTime, false);
+        }
+
+        public override void Draw(GameTime gameTime)
+        {
+
+            instruction.Draw(gameTime);
+        }
+        public override void Remove()
+        {
+            base.Remove();
+
+        }
+
+
+    }
+}
