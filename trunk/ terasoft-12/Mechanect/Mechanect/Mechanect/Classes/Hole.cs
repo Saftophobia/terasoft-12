e7bb097ab4660cbc40c3d20708181d73ab9a28f7
khaled.osmaan <khaled.osmaan@113cb9fd-de2b-231c-2074-2ec382a84f02>
2012-04-27 13:34:27 +0000

    update issue 35 status inProgress edited documentation for GenerateRandomValue()

--- trunk/ terasoft-12/Mechanect/Mechanect/Mechanect/Classes/Hole.cs
+++ trunk/ terasoft-12/Mechanect/Mechanect/Mechanect/Classes/Hole.cs
@@ -76,6 +76,9 @@ namespace Mechanect.Classes
         /// /// <param name="max">
         /// The maximum value.
         /// </param>
+        /// <returns>
+        /// Float number which is the generated random value.
+        /// </returns>
 
         public float GenerateRandomValue(float min, float max)
         {
