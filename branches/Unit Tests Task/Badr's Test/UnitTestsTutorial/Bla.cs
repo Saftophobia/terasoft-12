a9f2e984d3bdb46f00270a2a6b197dd0e8c12be6
q3ahmd@gmail.com <q3ahmd@gmail.com@113cb9fd-de2b-231c-2074-2ec382a84f02>
2012-05-10 21:36:51 +0000

    Renaming my test task

---
+++ branches/Unit Tests Task/Badr's Test/UnitTestsTutorial/Bla.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UnitTestsTutorial
+{
+    class Bla
+    {
+
+        public int num;
+        
+        //sets num to a value between 5,6
+        public Bla()
+        {
+            Random r = new Random();
+            num = r.Next(5, 6);
+        }
+
+        public int Factorial()
+        {
+            if (num <= 1)
+                return 1;
+            return (num--) * Factorial();
+        }
+
+        
+    }
+}
---
+++ branches/Unit Tests Task/Badr's Test/UnitTestsTutorial/TestClass.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace UnitTestsTutorial
+{
+    [TestFixture]
+    public class TestClass
+    {
+        Bla b;
+        //this method will be automatically called before any tests are run
+        [TestFixtureSetUp]
+        public void Init()
+        {
+            //a random number is chosen here between 5,6
+            b = new Bla();
+        }
+        
+        //this method will be automatically called after all the tests run
+        [TestFixtureTearDown]
+        public void Dispose()
+        {
+
+        }
+
+        [Test]
+        public void Test1()
+        {
+            //will succeed if the number was 5
+            Assert.AreEqual(120,b.Factorial());
+        }
+        
+        [Test]
+        public void Test2()
+        {
+            //will succeed if the number was 6
+            Assert.AreEqual(720, b.Factorial());
+        }
+    }
+}
