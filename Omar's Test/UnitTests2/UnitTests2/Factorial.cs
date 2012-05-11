90d204891a93745a7de74e5ecd18cdf16b9c6788
vendettax6@gmail.com <vendettax6@gmail.com@113cb9fd-de2b-231c-2074-2ec382a84f02>
2012-05-11 11:33:12 +0000

    Added Omar's Unit Test

---
+++ Omar's Test/UnitTests2/UnitTests2/Factorial.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UnitTests2
+{
+    class Factorial
+    {
+        public Factorial()
+        {
+        }
+
+        public int factorial(int num)
+        {
+            if (num <= 1) return 1;
+            else return num * factorial(num - 1);
+        }
+    }
+}
