c960a63e94335fa438fc3e1f0830b505a51fdb7d
hollaxyman@gmail.com <hollaxyman@gmail.com@113cb9fd-de2b-231c-2074-2ec382a84f02>
2012-05-11 20:25:04 +0000

    Adding My Testing Units sample

---
+++ Tamer_Test/unittest/unittest/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace unittest
+{
+    class Program
+    {
+       
+
+        public int  fibo(int n)
+        {
+            if (n < 2)
+                return n;
+            else
+                return fibo(n - 1) + fibo(n - 2);
+        }
+        public static void Main()
+        {
+
+        }
+
+   
+    }
+}
---
+++ Tamer_Test/unittest/unittest/tester.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace unittest
+{
+    [TestFixture]
+
+    public class tester
+    {
+
+        [Test]
+
+        public void test()
+        {
+
+            Program m = new Program();
+            Assert.AreEqual(0, m.fibo(0));
+            Assert.AreEqual(1, m.fibo(1));
+            Assert.AreEqual(1, m.fibo(2));
+            Assert.AreEqual(2, m.fibo(3));
+            Assert.AreEqual(3, m.fibo(4));
+         
+
+        }
+    }
+}
