7598c7d8244e4be58887896827d4bbfdb1243f5a
michel.nader.mansour@gmail.com <michel.nader.mansour@gmail.com@113cb9fd-de2b-231c-2074-2ec382a84f02>
2012-05-11 11:38:44 +0000

    Added Michel's Test folder and classes

---
+++ Michel's Test/Michel's Task/Fibonacci.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Michel_s_Task
+{
+    class Fibonacci
+    {
+        public Fibonacci()
+        {
+        }
+
+        public int GetFibonnacci(int i)
+        {
+            if (i < 0)
+                return -1;
+            else
+            {
+                if (i <= 1)
+                    return 1;
+                else
+                {
+                    return (GetFibonnacci(i - 1) + GetFibonnacci(i - 2));
+                }
+            }
+        }
+    }
+}
---
+++ Michel's Test/Michel's Task/TheTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace Michel_s_Task
+{
+    [TestFixture]
+    public class TheTest
+    {
+        Fibonacci fibo;
+
+        [TestFixtureSetUp]
+        public void Init()
+        {
+            fibo = new Fibonacci();
+        }
+
+        [TestFixtureTearDown]
+        public void Dispose()
+        {
+
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.AreEqual(-1, fibo.GetFibonnacci(-5));
+            Assert.AreEqual(-1, fibo.GetFibonnacci(-2));
+            Assert.AreEqual(-1, fibo.GetFibonnacci(-1));
+            Assert.AreEqual(1, fibo.GetFibonnacci(0));
+            Assert.AreEqual(1, fibo.GetFibonnacci(1));
+            Assert.AreEqual(2, fibo.GetFibonnacci(2));
+            Assert.AreEqual(8, fibo.GetFibonnacci(5));
+        }
+    }
+}
