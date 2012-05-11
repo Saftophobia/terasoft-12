618d1ed91a2746c78fdf925fc8474f4401fd0436
bishoybassem@gmail.com <bishoybassem@gmail.com@113cb9fd-de2b-231c-2074-2ec382a84f02>
2012-05-11 21:35:42 +0000

    added Bishoy's Test

---
+++ Bishoy's Test/Project1/Functions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Project1
+{
+    class Functions
+    {
+
+        public static int Sum(int[] array)
+        {
+            if (array == null)
+            {
+                throw new ArgumentException();
+            }
+            int sum = 0;
+            foreach (int i in array)
+            {
+                sum += i;
+            }
+            return sum;
+        }
+    }
+}
---
+++ Bishoy's Test/Project1/Test.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace Project1
+{
+    [TestFixture]
+    public class Test
+    {
+
+        [TestFixtureSetUp]
+        public void Init()
+        {
+
+        }
+
+        [Test]
+        public void TestSum1()
+        {
+            try
+            {
+                Functions.Sum(null);
+                Assert.Fail("sum should throw an ArgumentException if passed null as a parameter");
+            } catch (Exception ex) {
+                Assert.IsInstanceOf<ArgumentException>(ex, "sum should throw an ArgumentException if passed null as a parameter");
+            }
+        }
+
+        [Test]
+        public void TestSum2()
+        {
+            Assert.AreEqual(10, Functions.Sum(new int[] { 1, 2, 3, 4 }), "passed 1, 2, 3, 4 as parameters, expected sum is 10");
+        }
+
+        [Test]
+        public void TestSum()
+        {
+            Assert.AreEqual(0, Functions.Sum(new int[] { }), "passed empty array as parameter, expected sum is 0");
+        }
+
+        [TestFixtureTearDown]
+        public void Dispose()
+        {
+        }
+
+    }
+
+}
