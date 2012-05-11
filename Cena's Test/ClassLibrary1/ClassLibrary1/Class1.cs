6454091c410d3af557adb9fe049c5a74e296cd1e
mk.cena92@gmail.com <mk.cena92@gmail.com@113cb9fd-de2b-231c-2074-2ec382a84f02>
2012-05-11 21:01:49 +0000

    Cena: Committing the Unit Tests Task.

---
+++ Cena's Test/ClassLibrary1/ClassLibrary1/Class1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace ClassLibrary1
+{
+
+    [TestFixture]
+    class Class1
+    {
+
+        NoobAdder adder;
+        [TestFixtureSetUp]
+        public void Init()
+        {
+
+            adder = new NoobAdder();
+            adder.noobAdder();
+           
+        }
+
+        [TestFixtureTearDown]
+        public void Dispose()
+        {
+
+        }
+        [Test]
+        public void False()
+        {
+           
+
+            Assert.AreEqual(5, adder.number);
+        }
+        [Test]
+        public void True()
+        {
+          
+
+            Assert.AreEqual(10, adder.number);
+        }
+
+
+        public static void main()
+        {
+        }
+
+    }
+}
