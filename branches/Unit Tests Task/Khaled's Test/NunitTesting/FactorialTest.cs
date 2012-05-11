edc0595200e8be2778a10c1e39c474b388a3d66a
khaled.osmaan <khaled.osmaan@113cb9fd-de2b-231c-2074-2ec382a84f02>
2012-05-11 15:00:39 +0000

    Commited my Nunit test task

---
+++ branches/Unit Tests Task/Khaled's Test/NunitTesting/FactorialTest.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+
+namespace NunitTesting
+{
+ 
+    [TestFixture]
+    public class FactorialTest
+    {
+        Factorial f;
+        //marks this method as the setup method for this test class. Meaning, before any tests are run this method will be automatically called, so you should put here all the things that might be common among all your tests.
+        [TestFixtureSetUp]
+        public void Init()
+        {
+            f = new Factorial();
+        }
+        //marks this method as the test method for this test class. this is the test method that will be run to make sure the code works as expected.
+        [Test]
+        public void TestFactorial()
+        {
+            Assert.AreEqual(1, f.GenerateFactorial(0));
+            Assert.AreEqual(1, f.GenerateFactorial(1));
+            Assert.AreEqual(24, f.GenerateFactorial(4));
+            Assert.AreEqual(-1, f.GenerateFactorial(-10));
+        }
+        //marks this method as the TearDown method for this class. Meaning, after any tests are run this method will be automatically called, here's where you should put all the unloads and such stuff.
+        [TestFixtureTearDown]
+        public void Dispose()
+        {
+        }
+
+    }
+}
