11413e3e78556cdaf292355f440643bebeb56d37
ahmedhegazy835 <ahmedhegazy835@113cb9fd-de2b-231c-2074-2ec382a84f02>
2012-05-11 20:28:54 +0000

    Adding my NUnit Testing Project

---
+++ HegazY's Test/ClassLibrary1/ClassLibrary1/Fac.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClassLibrary1
+{
+    class Fac
+    {
+        public static int Factorial(int number)
+        {
+            if (number < 0)
+                return -1;
+            if (number == 0)
+                return 1;
+            return (Factorial(number - 1) * number);
+        }
+    }
+}
