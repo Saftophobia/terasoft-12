2562ce7327d8b39251191754baec892e0b608e97
mk.cena92@gmail.com <mk.cena92@gmail.com@113cb9fd-de2b-231c-2074-2ec382a84f02>
2012-05-11 21:01:49 +0000

    Cena: Committing the Unit Tests Task.

---
+++ branches/Unit Tests Task/Cena's Test/ClassLibrary1/ClassLibrary1/NoobAdder.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClassLibrary1
+{
+    class NoobAdder
+    {
+        public int number;
+
+        public NoobAdder()
+        {
+            this.number = 0;
+        }
+
+        public void noobAdder()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                number += i;
+            }
+        }
+    }
+}
