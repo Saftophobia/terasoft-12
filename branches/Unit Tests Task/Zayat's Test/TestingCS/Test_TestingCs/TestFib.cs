b2024c6011aec9d7cf6610bc8bd13daca2032c7e
mohamed.alzayat.eng@gmail.com <mohamed.alzayat.eng@gmail.com@113cb9fd-de2b-231c-2074-2ec382a84f02>
2012-05-11 13:07:11 +0000

    Added Zayat's Test Folder and a project with a test suite testing Fibonacci

---
+++ branches/Unit Tests Task/Zayat's Test/TestingCS/Test_TestingCs/TestFib.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestingCS;
+namespace Test_TestingCs
+{
+    /// <summary>
+    /// Summary description for TestFib
+    /// </summary>
+    [TestClass]
+    public class TestFib
+    {
+        int[] input;
+        int[] expectedOutput;
+        public TestFib()
+        {
+            //
+            // TODO: Add constructor logic here
+            //
+        }
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        //
+        // You can use the following additional attributes as you write your tests:
+        //
+        // Use ClassInitialize to run code before running the first test in the class
+        // [ClassInitialize()]
+        // public static void MyClassInitialize(TestContext testContext) { }
+        //
+        // Use ClassCleanup to run code after all tests in a class have run
+        // [ClassCleanup()]
+        // public static void MyClassCleanup() { }
+        //
+        // Use TestInitialize to run code before running each test 
+        [TestInitialize()]
+        public void MyTestInitialize()
+        {
+            input = new int[] { -5, 0, 1,2,3, 10, 40 };
+            expectedOutput = new int[] { -1, 0, 1,1,2, 55, 102334155 };
+        }
+        //
+        // Use TestCleanup to run code after each test has run
+         [TestCleanup()]
+         public void MyTestCleanup() { }
+        //
+        #endregion
+
+        [TestMethod]
+        public void TestMethodFib()
+         {
+             for (int i = 0; i < input.Length; i++)
+             {
+                 Assert.AreEqual(expectedOutput[i],Fib.fib(input[i]));
+             }
+            
+        }
+        [TestMethod]
+        public void TestMethodFibDP()
+        {
+            for (int i = 0; i < input.Length; i++)
+            {
+                Assert.AreEqual(expectedOutput[i], Fib.fibDP(input[i]));
+            }
+            Assert.AreEqual(3736710778780434371, Fib.fibDP(100));
+        }
+    }
+}
---
+++ branches/Unit Tests Task/Zayat's Test/TestingCS/TestingCS/Fib.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace TestingCS
+{
+    public  static class Fib
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Int64 x = fibDP(5);
+            Console.WriteLine(x);
+            
+        }
+
+        public static Int64 fib(int n)
+        {
+            if (n < 0)
+            {
+                return -1;
+            }
+            if (n < 2)
+            {
+                return n;
+            }
+            return fib(n - 1) + fib(n - 2);
+        }
+        public static Int64 fibDP(int n)
+        {
+            if (n < 0)
+            {
+                return -1;
+            }
+            List<Int64> fib = new List<Int64>();
+            fib.Add(0);
+            fib.Add(1);
+            for (int i = 2; i <= n; i++)
+            {
+                //Console.WriteLine(i);
+                fib.Add(fib[i - 1] + fib[i - 2]);
+            }
+            return fib[n];
+
+        }
+        
+    }
+}
