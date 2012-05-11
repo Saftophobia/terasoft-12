b1ef954a10722b8d0e88eb69c86adc65ae7c4a9a
ahmedhegazy835 <ahmedhegazy835@113cb9fd-de2b-231c-2074-2ec382a84f02>
2012-05-11 20:28:54 +0000

    Adding my NUnit Testing Project

---
+++ branches/Unit Tests Task/HegazY's Test/ClassLibrary1/ClassLibrary1/Test.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace ClassLibrary1
+{
+    [TestFixture]
+    class Test
+    {
+        [TestFixtureSetUp]
+        public void Init()
+        {
+        }
+
+        [Test]
+        public void TestFac()
+        {
+            Assert.True(Fac.Factorial(-2) == -1, "negative values should return -1");
+            Assert.True(Fac.Factorial(0) == 1, "factorial 0 is 1");
+            Assert.True(Fac.Factorial(1) == 1, "fac 1 is 1");
+            Assert.True(Fac.Factorial(5) == 120, "factorial 5 is 120");
+        }
+
+        [TestFixtureTearDown]
+        public void Dispose()
+        {
+        }
+    }
+}
