25b389792613474cbfb7d9f59e311479b8499082
ams.sanad@gmail.com <ams.sanad@gmail.com@113cb9fd-de2b-231c-2074-2ec382a84f02>
2012-05-11 17:07:31 +0000

    Added the unit test task.

---
+++ Sanad's Test/UnitTest/UnitTest/Test.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace UnitTest
+{
+     [TestFixture]
+    class Test
+    {
+         power p;
+         power s;
+         [TestFixtureSetUp]
+         public void Init()
+         {
+             p = new power(5);
+             s = new power(19);
+         }
+
+         [TestFixtureTearDown]
+         public void Dispose()
+         {
+
+         }
+
+         [Test]
+         public void Test1()
+         {
+             Assert.AreEqual(32, p.twoPower());
+         }
+
+         [Test]
+         public void Test2()
+         {
+             Assert.AreEqual(524288, s.twoPower());
+         }
+    }
+}
---
+++ Sanad's Test/UnitTest/UnitTest/power.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UnitTest
+{
+    public class power
+    {
+        int num;
+        int result;
+        public power(int n)
+        {
+            num = n;
+            result = 1;
+        }
+
+        public int twoPower()
+        {
+            for (int i = 0; i < num; i++)
+            {
+                result *= 2;
+            }
+            return result;
+            
+        }
+
+
+    }
+}
