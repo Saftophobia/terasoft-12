7000d6d327967d4ed3ec3eb522ba91c35de33eeb
3abdelazim@gmail.com <3abdelazim@gmail.com@113cb9fd-de2b-231c-2074-2ec382a84f02>
2012-05-11 21:39:40 +0000

    Task : Unit Test added Unit Tests Task/Abdelazim's Test

---
+++ branches/Unit Tests Task/Abdelazim's Test/Maths.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Tests
+{
+    public class Maths
+    {
+
+        public static int Fibonacci(int n)
+        {
+            if (n < 2) return n;
+            else return Fibonacci(n - 1) + Fibonacci(n - 2);
+        }
+
+    }
+}
