3cd2bfa748d6bf72551a881c81014d914b14b037
vendettax6@gmail.com <vendettax6@gmail.com@113cb9fd-de2b-231c-2074-2ec382a84f02>
2012-05-11 11:33:12 +0000

    Added Omar's Unit Test

---
+++ branches/Unit Tests Task/Omar's Test/UnitTests2/UnitTests2/FactorialTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace UnitTests2
+{
+    [TestFixture]
+    public class FactorialTest
+    {
+        Factorial f;
+        [TestFixtureSetUp]
+        public void Init()
+        {
+            f = new Factorial();
+        }
+
+        [Test]
+        public void checkFactorial()
+        {
+            Assert.AreEqual(1, f.factorial(0));
+            Assert.AreEqual(120, f.factorial(5));
+        }
+
+        [TestFixtureTearDown]
+        public void Dispose()
+        {
+        }
+
+    }
+}
