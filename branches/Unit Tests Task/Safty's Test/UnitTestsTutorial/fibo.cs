2de2a247c012290a1b0e6df291a185ad98903c2c
asafty2003@gmail.com <asafty2003@gmail.com@113cb9fd-de2b-231c-2074-2ec382a84f02>
2012-05-11 19:38:06 +0000

    Committed my folder and my Testcase - Safty

---
+++ branches/Unit Tests Task/Safty's Test/UnitTestsTutorial/fibo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UnitTestsTutorial
+{
+    class fibo
+    {
+        
+        public fibo()
+        {
+            
+        }
+
+        public int getFibonacci(int n)
+        {
+            if (n == 0 || n == 1)
+                return n;
+            else
+                return getFibonacci(n - 1) + getFibonacci(n - 2);
+        }
+        
+    }
+}
