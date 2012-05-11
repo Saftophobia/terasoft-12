42b9d2118752fbd09a62ce22936191cd5c954781
mohamedra2fat@gmail.com <mohamedra2fat@gmail.com@113cb9fd-de2b-231c-2074-2ec382a84f02>
2012-05-11 13:52:10 +0000

    This is my Test files for Fibonacci

---
+++ Raafat's Test/Project2/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Project2
+{
+    class Class1
+    {
+        public int fib(int a)
+        {
+            if (a < 0)
+                return -1;
+            if (a < 2)
+                return a;
+            else
+                return fib(a - 1) + fib(a - 2);
+        }
+        public static void Main(String[] args)
+        {
+             Class1 x = new Class1();
+             int a = x.fib(12);
+             Console.Out.WriteLine(a);
+             Console.ReadLine();
+            
+        }
+    }
+}
