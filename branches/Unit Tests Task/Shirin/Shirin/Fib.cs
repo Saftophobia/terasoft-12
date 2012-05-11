ade1e6296aa2d12e3a9e49dcb2f7e83b6cc881f3
asamgdla@gmail.com <asamgdla@gmail.com@113cb9fd-de2b-231c-2074-2ec382a84f02>
2012-05-11 19:38:51 +0000

    Added my Unit test task

---
+++ branches/Unit Tests Task/Shirin/Shirin/Fib.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Shirin
+{
+    public class Fib
+    {
+         public Fib()
+        {
+
+        }
+
+        public int GetFib (int n)
+        {
+            if (n == 0)
+            {
+                return 0;
+            }
+            if (n == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return GetFib(n - 1) + GetFib(n - 2);
+            }
+        }
+    }
+}
---
+++ branches/Unit Tests Task/Shirin/Shirin/Test.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace Shirin
+{
+    [TestFixture]
+    class Test
+    {
+        Fib fib;
+
+        [TestFixtureSetUp]
+        public void Init()
+        {
+            fib = new Fib();
+        }
+
+        [TestFixtureTearDown]
+        public void Dispose()
+        {
+
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.AreEqual(0, fib.GetFib(0));
+        }
+        [Test]
+        public void Test2()
+        {
+            Assert.AreEqual(1, fib.GetFib(1));
+        }
+        [Test]
+        public void Test3()
+        {
+            Assert.AreEqual(3, fib.GetFib(4));
+        }
+        [Test]
+        public void Test4()
+        {
+            Assert.AreEqual(34, fib.GetFib(9));
+        }
+    }
+}
