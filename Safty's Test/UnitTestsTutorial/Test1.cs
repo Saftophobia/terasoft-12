5a7716724497431b031d976e21dc73731eb6f94f
asafty2003@gmail.com <asafty2003@gmail.com@113cb9fd-de2b-231c-2074-2ec382a84f02>
2012-05-11 19:38:06 +0000

    Committed my folder and my Testcase - Safty

---
+++ Safty's Test/UnitTestsTutorial/Test1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace UnitTestsTutorial
+{
+    [TestFixture]
+    public class Test1
+    {
+        fibo fibo;
+        [TestFixtureSetUp]
+        public void Init()
+        {
+        
+            fibo = new fibo();
+        }
+        
+        [TestFixtureTearDown]
+        public void Dispose()
+        {
+
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.AreEqual(120, fibo.getFibonacci(5));
+        }
+        
+        
+    }
+}
