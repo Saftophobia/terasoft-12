9d26dea04395eda488fbdd68487fd00a8a889312
khaled.osmaan <khaled.osmaan@113cb9fd-de2b-231c-2074-2ec382a84f02>
2012-05-11 18:17:16 +0000

    recommited my work after adding it to a library and writing a console project for testing

---
+++ Khaled's Test/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using MyLibrary;
+namespace NunitTesting
+{
+    public class btengan
+    {
+        public static void Main(String[] args)
+        {
+            Console.Write(new Factorial().GenerateFactorial(4));
+        }
+    }
+}
---
+++ Khaled's Test/MyLibrary/MyLibrary/Factorial.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace MyLibrary
+{
+    public class Factorial
+    {
+        public Factorial()
+        {
+        }
+        public int GenerateFactorial(int number)
+        {
+            if (number == 0) return 1;
+            if (number < 0) return -1;
+            return number * GenerateFactorial(number - 1);
+        }
+    }
+
+}
