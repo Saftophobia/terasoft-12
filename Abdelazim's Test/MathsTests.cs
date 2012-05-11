4f10264a1cf4ba32bdd813f9d01b857d10fe6e6c
3abdelazim@gmail.com <3abdelazim@gmail.com@113cb9fd-de2b-231c-2074-2ec382a84f02>
2012-05-11 21:39:40 +0000

    Task : Unit Test added Unit Tests Task/Abdelazim's Test

---
+++ Abdelazim's Test/MathsTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace Tests
+{
+    public class MathsTests
+    {
+
+        //this method will be automatically called before any tests are run
+        [TestFixtureSetUp]
+        public void Init()
+        {
+        }
+
+        //this method will be automatically called after all the tests run
+        [TestFixtureTearDown]
+        public void Dispose()
+        {
+
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.AreEqual(0, Maths.Fibonacci(0));
+        }
+
+        [Test]
+        public void Test2()
+        {
+            Assert.AreEqual(1, Maths.Fibonacci(1));
+        }
+
+        [Test]
+        public void Test3()
+        {
+            Assert.AreEqual(55, Maths.Fibonacci(10));
+        }
+
+    }
+}
