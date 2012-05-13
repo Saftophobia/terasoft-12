f0f18187e16e1236f5849045367d94cd6c9382f7
mohamed.alzayat.eng@gmail.com <mohamed.alzayat.eng@gmail.com@113cb9fd-de2b-231c-2074-2ec382a84f02>
2012-05-13 22:01:02 +0000

    update issue 224
    Status: In-Progress
    
    Made the SayText method static.

--- trunk/ terasoft-12/Mechanect/UI/Class1.cs
+++ trunk/ terasoft-12/Mechanect/UI/Class1.cs
@@ -9,7 +9,7 @@ namespace UI
     public class Class1
     {
 
-        SpeechSynthesizer synth = new SpeechSynthesizer();
+        static SpeechSynthesizer synth = new SpeechSynthesizer();
 
         /// <summary>
         /// This method takes a string of text and reads it to the user.
@@ -21,7 +21,7 @@ namespace UI
         /// </remarks>
         /// <param name="text">The string of text to be said.</param>
         /// <returns> True if the string was said and false if it wasn't.</returns>
-        public bool SayText(string text)
+        public static bool SayText(string text)
         {
             try
             {
@@ -30,7 +30,7 @@ namespace UI
                 return true;
             }
             catch (Exception e)
-            {   
+            {
                 return false;
             }
 
