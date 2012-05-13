39af03c2a687acb5292a18daf2092ae7a3a969ce
mohamed.alzayat.eng@gmail.com <mohamed.alzayat.eng@gmail.com@113cb9fd-de2b-231c-2074-2ec382a84f02>
2012-05-13 21:40:39 +0000

    update issue 224
    Status: Ready
    
    Implementation ready,
    Design Ready,
    Documentation Ready,
    Scenario Ready

---  terasoft-12/Mechanect/UI/Class1.cs
+++  terasoft-12/Mechanect/UI/Class1.cs
@@ -2,10 +2,38 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Speech.Synthesis;
 
 namespace UI
 {
     public class Class1
     {
+
+        SpeechSynthesizer synth = new SpeechSynthesizer();
+
+        /// <summary>
+        /// This method takes a string of text and reads it to the user.
+        /// </summary>
+        ///  <remarks>
+        /// <para>AUTHOR: Mohamed Alzayat </para>   
+        /// <para>DATE WRITTEN: May, 13 </para>
+        /// <para>DATE MODIFIED: May, 13  </para>
+        /// </remarks>
+        /// <param name="text">The string of text to be said.</param>
+        /// <returns> True if the string was said and false if it wasn't.</returns>
+        public bool SayText(string text)
+        {
+            try
+            {
+                synth.Volume = 100;
+                synth.SpeakAsync(text);
+                return true;
+            }
+            catch (Exception e)
+            {   
+                return false;
+            }
+
+        }
     }
 }
