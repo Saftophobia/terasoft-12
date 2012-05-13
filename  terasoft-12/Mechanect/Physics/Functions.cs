8e3048a9278cf591373d86ae9e725f0a5d56450b
bishoybassem@gmail.com <bishoybassem@gmail.com@113cb9fd-de2b-231c-2074-2ec382a84f02>
2012-05-13 23:19:56 +0000

    update issue 40
    Status: In Progress
    
    updated documentation of GetVectorInDirectionOf method in  physics library

---  terasoft-12/Mechanect/Physics/Functions.cs
+++  terasoft-12/Mechanect/Physics/Functions.cs
@@ -7,8 +7,8 @@ namespace Physics
         /// <summary>
         /// calculates the vector in direction of another one given its magnitude
         /// </summary>
-        /// <param name="velocity">the other vector</param>
-        /// <param name="frictionValue">the magnitude of the vector</param>
+        /// <param name="anotherVector">the other vector</param>
+        /// <param name="magnitude">the magnitude of the vector</param>
         /// <remarks>Auther : Bishoy Bassem</remarks>
         public static Vector3 GetVectorInDirectionOf(float magnitude, Vector3 anotherVector)
         {
