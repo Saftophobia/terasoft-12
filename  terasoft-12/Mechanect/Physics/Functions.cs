16892eb425eaad1679325f51bde7eb0e99ac1cba
bishoybassem@gmail.com <bishoybassem@gmail.com@113cb9fd-de2b-231c-2074-2ec382a84f02>
2012-05-13 23:04:25 +0000

    update issue 40
    Status: In Progress
    
    created Functions class in Physics library and added GetVectorInDirectionOf method

---
+++  terasoft-12/Mechanect/Physics/Functions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace Physics
+{
+    public class Functions
+    {
+        /// <summary>
+        /// calculates the friction vector that will cause the velocity to stop without changing the velocity direction
+        /// </summary>
+        /// <param name="velocity">the velocity vector</param>
+        /// <param name="frictionValue">the value of the friction</param>
+        /// <remarks>Auther : Bishoy Bassem</remarks>
+        public static Vector3 GetVectorInDirectionOf(float magnitude, Vector3 anotherVector)
+        {
+            anotherVector.Normalize();
+            return magnitude * anotherVector;
+        }
+    }
+}
