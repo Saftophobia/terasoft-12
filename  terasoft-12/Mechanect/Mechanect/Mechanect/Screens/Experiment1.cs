d0dfb54e0d1f3080b8bb71a6a6a5527a6e42d427
asamgdla@gmail.com <asamgdla@gmail.com@113cb9fd-de2b-231c-2074-2ec382a84f02>
2012-05-14 13:37:12 +0000

    Changed the way the function DrawGraphs was called.

---  terasoft-12/Mechanect/Mechanect/Mechanect/Screens/Experiment1.cs
+++  terasoft-12/Mechanect/Mechanect/Mechanect/Screens/Experiment1.cs
@@ -511,7 +511,7 @@ namespace Mechanect.Screens
 
 
                         // Graph.drawGraphs(player1.Positions, player2.Positions, racecommands,timeslicedouble,player1disqualification, player2disqualification, ScreenManager.GraphicsDevice.Viewport.Width, ScreenManager.GraphicsDevice.Viewport.Height);
-                        Graph.DrawGraphs(player1.Positions, player2.Positions, racecommands, timeofrace, player1disqualification, player2disqualification, ScreenManager.GraphicsDevice.Viewport.Width, ScreenManager.GraphicsDevice.Viewport.Height);
+                        Graph.DrawGraphs(Graph,player1.Positions, player2.Positions, racecommands, timeofrace, player1disqualification, player2disqualification, ScreenManager.GraphicsDevice.Viewport.Width, ScreenManager.GraphicsDevice.Viewport.Height);
 
 
                         graphmutex = true;
