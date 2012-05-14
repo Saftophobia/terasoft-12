8558adb2e7d371edc5c4eb06d41ec04c0f3ba52e
asafty2003@gmail.com <asafty2003@gmail.com@113cb9fd-de2b-231c-2074-2ec382a84f02>
2012-05-14 22:51:20 +0000

    update issue 240
    Status: In Progress
    
    Added Environment1.cs, with a 3d environment and called in Experimentnew1.cs

---
+++ trunk/ terasoft-12/Mechanect/Mechanect/Mechanect/Screens/Experimentnew1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
+using Mechanect.Classes;
+
+namespace Mechanect.Screens
+{
+    class Experimentnew1 : Mechanect.Common.GameScreen
+    {
+
+        Viewport ViewPort
+        {
+            get
+            {
+                return ScreenManager.GraphicsDevice.Viewport;
+            }
+        }
+        ContentManager Content
+        {
+            get
+            {
+                return ScreenManager.Game.Content;
+            }
+        }
+        SpriteBatch SpriteBatch
+        {
+            get
+            {
+                return ScreenManager.SpriteBatch;
+            }
+        }
+
+
+        Environment1 Environment1;
+        GraphicsDevice graphics;
+        User1 user1, user2;
+
+        public Experimentnew1(User1 user1, User1 user2)
+        {
+            this.user1 = user1;
+            this.user2 = user2;
+        }
+        public override void Initialize()
+        {
+            base.Initialize();
+        }
+
+        
+        public override void LoadContent()
+        {
+            graphics = this.ScreenManager.GraphicsDevice;
+            Environment1 = new Environment1(ScreenManager.Game.Content, ScreenManager.Game.GraphicsDevice);
+            Environment1.LoadContent();
+        }
+        public override void UnloadContent()
+        {
+
+        }
+
+        public override void Update(Microsoft.Xna.Framework.GameTime gameTime, bool covered)
+        {
+            Environment1.update(gameTime);
+
+            base.Update(gameTime, covered);
+        }
+
+        public override void Draw(Microsoft.Xna.Framework.GameTime gameTime)
+        {
+            Environment1.Draw(gameTime);
+        }
+
+    }
+}
