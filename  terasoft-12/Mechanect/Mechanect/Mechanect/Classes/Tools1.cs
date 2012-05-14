eebe661e0e3e047c5a94a77e49fc23c1131a77cd
michel.nader.mansour@gmail.com <michel.nader.mansour@gmail.com@113cb9fd-de2b-231c-2074-2ec382a84f02>
2012-05-14 19:47:52 +0000

    commented Shirin's methods calls until he submits them in the correct folders and classes Update issue 24 commented Shirin's methods calls until he submits them in the correct folders and classes

---  terasoft-12/Mechanect/Mechanect/Mechanect/Classes/Tools1.cs
+++  terasoft-12/Mechanect/Mechanect/Mechanect/Classes/Tools1.cs
@@ -372,8 +372,8 @@ namespace Mechanect.Classes
          /// </remarks>
          public static bool ConstantAcceleration(List<float> positions, float currentTolerance)
          {
-             List<float> velocities = PerformanceGraph.GetPlayerVelocity(positions);
-
+             //List<float> velocities = PerformanceGraph.GetPlayerVelocity(positions);
+             List<float> velocities = new List<float>();//this line must be replaced by the line above after shirin solves the class problems
 
              bool result = true;
              float firstVelocity = velocities[1] - velocities[0];
@@ -445,8 +445,8 @@ namespace Mechanect.Classes
          /// </remarks>
          public static bool IncreasingAcceleration(List<float> positions, float currentTolerance)
          {
-             List<float> accelerations = PerformanceGraph.GetPlayerAcceleration(PerformanceGraph.GetPlayerVelocity(positions));
-
+             //List<float> accelerations = PerformanceGraph.GetPlayerAcceleration(PerformanceGraph.GetPlayerVelocity(positions));
+             List<float> accelerations = new List<float>();//this line must be replaced by the line above after shirin solves the class problems
 
              bool result = true;
              for (int i = 1; i < accelerations.Count; i++)
@@ -482,8 +482,8 @@ namespace Mechanect.Classes
          /// </remarks>
          public static bool DecreasingAcceleration(List<float> positions, float currentTolerance)
          {
-             List<float> accelerations = PerformanceGraph.GetPlayerAcceleration(PerformanceGraph.GetPlayerVelocity(positions));
-
+             //List<float> accelerations = PerformanceGraph.GetPlayerAcceleration(PerformanceGraph.GetPlayerVelocity(positions));
+             List<float> accelerations = new List<float>();//this line must be replaced by the line above after shirin solves the class problems
 
              bool result = true;
              for (int i = 1; i < accelerations.Count; i++)
