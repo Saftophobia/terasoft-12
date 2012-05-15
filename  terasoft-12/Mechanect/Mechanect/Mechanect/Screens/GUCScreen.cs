514dd9a4e9f6a0b67fdec750fc812cd421d3fe82
q3ahmd@gmail.com <q3ahmd@gmail.com@113cb9fd-de2b-231c-2074-2ec382a84f02>
2012-05-15 12:16:20 +0000

    update issue 219 Status: In Progress
    Added Screens for the intro screen

---
+++  terasoft-12/Mechanect/Mechanect/Mechanect/Screens/GUCScreen.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace Mechanect.Screens
+{
+    class GUCScreen : FadingScreen
+    {
+        public GUCScreen()
+            : base("Resources/Images/GUC", 1f, 0, 0, -70)
+        { }
+
+        public override void Update(GameTime gameTime, bool covered)
+        {
+            base.Update(gameTime, false);
+            if (Done)
+            {
+                base.Remove();
+                ScreenManager.AddScreen(new ITworxScreen());
+            }
+        }
+    }
+}
---
+++  terasoft-12/Mechanect/Mechanect/Mechanect/Screens/ITWroxScreen.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace Mechanect.Screens
+{
+    class ITworxScreen : FadingScreen
+    {
+        public ITworxScreen()
+            : base("Resources/Images/ITWorx", 1f, 0, 0, -70)
+        { }
+
+        public override void Update(GameTime gameTime, bool covered)
+        {
+            base.Update(gameTime, false);
+            if (Done)
+            {
+                base.Remove();
+                ScreenManager.AddScreen(new GUCScreen());
+            }
+        }
+    }
+}
