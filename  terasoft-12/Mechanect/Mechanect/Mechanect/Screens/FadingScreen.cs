7ed61cdc8348ad006a3b19d5e5f3d6c78bf844ca
q3ahmd@gmail.com <q3ahmd@gmail.com@113cb9fd-de2b-231c-2074-2ec382a84f02>
2012-05-15 12:07:38 +0000

    update issue 219 Status: In Progress
    Updated the fading values
    Updated the constructor
    Added new fields
    Changed the background color to white

---  terasoft-12/Mechanect/Mechanect/Mechanect/Screens/FadingScreen.cs
+++  terasoft-12/Mechanect/Mechanect/Mechanect/Screens/FadingScreen.cs
@@ -2,6 +2,7 @@
 using Mechanect.Common;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using System;
 
 namespace Mechanect.Screens
 {
@@ -13,12 +14,37 @@ namespace Mechanect.Screens
         private Texture2D logo;
         private string path;
         private float scale;
-        public FadingScreen(string path, float logoScale)
+        private float rotation;
+        private float xPositionOffset;
+        private float yPositionOffset;
+        private bool done;
+        public bool Done
+        {
+            set
+            {
+                done = value;
+            }
+            get
+            {
+                return done;
+            }
+        }
+
+        public FadingScreen()
+        {
+        }
+        
+        public FadingScreen(string path, float logoScale,float rotation,float xPositionOffset, float yPositionOffset)
         {
             this.path = path;
+            this.rotation = rotation;
+            this.xPositionOffset = xPositionOffset;
+            this.yPositionOffset = yPositionOffset;
             first = true;
             fading = 1f;
             scale = logoScale;
+            done = false;
+            
         }
 
         public override void LoadContent()
@@ -29,29 +55,31 @@ namespace Mechanect.Screens
 
         public override void Update(GameTime gameTime, bool covered)
         {
-            if (fading <= 0.1f || !first)
+            if (fading <= 0.01f || !first)
             {
                 if (first)
                 {
                     first = false;
-                    Thread.Sleep(2000);
+                    Thread.Sleep(1500);
                 }
 
                 fading /= 0.96f;
+                if (fading > 0.999)
+                    Done = true;
             }
             else
-                fading *= 0.99f;
+                fading *= 0.97f;
         }
  
         public override void Draw(GameTime gameTime)
         {
-            ScreenManager.GraphicsDevice.Clear(Color.Black);
+            ScreenManager.GraphicsDevice.Clear(Color.White);
             ScreenManager.SpriteBatch.Begin();
-            ScreenManager.SpriteBatch.Draw(logo, new Vector2((ScreenManager.GraphicsDevice.Viewport.Width - logo.Width * scale) / 2,
-                (ScreenManager.GraphicsDevice.Viewport.Height - (logo.Height - 100) * scale) / 2),
-                null, Color.White, -0.1f, new Vector2(0, 0), new Vector2(scale, scale), SpriteEffects.None, 0);
+            ScreenManager.SpriteBatch.Draw(logo, new Vector2(((ScreenManager.GraphicsDevice.Viewport.Width - logo.Width * scale) / 2) + xPositionOffset,
+                ((ScreenManager.GraphicsDevice.Viewport.Height - (logo.Height - 100) * scale) / 2) + yPositionOffset),
+                null, Color.White, rotation, new Vector2(0, 0), new Vector2(scale, scale), SpriteEffects.None, 0);
 
-            ScreenManager.SpriteBatch.Draw(black, Vector2.Zero, null, Color.White * fading, -0.1f, new Vector2(0, 0),
+            ScreenManager.SpriteBatch.Draw(black, Vector2.Zero, null, Color.White * fading, 0, new Vector2(0, 0),
                 new Vector2(ScreenManager.GraphicsDevice.Viewport.Width, ScreenManager.GraphicsDevice.Viewport.Height), SpriteEffects.None, 0);
             ScreenManager.SpriteBatch.End();
         }        
