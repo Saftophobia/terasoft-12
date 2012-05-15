d50b888dd5bd5f5b76465a8ef459b5ec998fb943
hollaxyman@gmail.com <hollaxyman@gmail.com@113cb9fd-de2b-231c-2074-2ec382a84f02>
2012-05-15 20:46:05 +0000

    Update Issue : 244 Status : In-Progress
    Added class ThemeAndLevel and Settings2.cs containing all methods used for Theme change and Level with its implementation + Documentation.

---
+++  terasoft-12/Mechanect/Mechanect/Mechanect/Screens/Settings2.cs
@@ -0,0 +1,68 @@
+using Mechanect.Common;
+using Microsoft.Xna.Framework;
+using Mechanect.Classes;
+
+namespace Mechanect.Screens
+{
+    class Settings2 : GameScreen
+    {
+        private Button _oKbutton;
+        User2 user;
+        private ThemeAndLevel _level;
+
+        public Settings2(User2 user)
+        {
+            this.user = user;
+        }
+        /// <summary>
+        /// Load the ok button and showing level and theme screen
+        /// </summary>
+        /// <remarks>
+        /// <para>AUTHOR: Tamer Nabil </para>
+        /// </remarks>
+        public override void LoadContent()
+        {
+            _oKbutton = Tools3.OKButton(ScreenManager.Game.Content,
+              new Vector2(ScreenManager.GraphicsDevice.Viewport.Width - 200, ScreenManager.GraphicsDevice.Viewport.Height - 250),
+             ScreenManager.GraphicsDevice.Viewport.Width,
+             ScreenManager.GraphicsDevice.Viewport.Height, user);
+            _level = new ThemeAndLevel(ScreenManager.Game, new Vector2(150, 150), ScreenManager.SpriteBatch, user);
+            _level.Initialize(ScreenManager.GraphicsDevice.Viewport.Width, ScreenManager.GraphicsDevice.Viewport.Height);
+
+        }
+
+        /// <summary>
+        /// Update method that  updae at gameTime and check for button clicking to exit
+        /// </summary>
+        /// <remarks>
+        /// <para>AUTHOR: Tamer Nabil </para>
+        /// </remarks>
+        /// <param name="gameTime">gameTime</param>
+        /// <param name="covered">covered</param>
+        public override void Update(GameTime gameTime, bool covered)
+        {
+            if (_oKbutton.IsClicked())
+            {
+                ExitScreen();
+                ScreenManager.AddScreen(new InstructionsScreen2(user));
+                Remove();
+            }
+            _oKbutton.Update(gameTime);
+            _level.Update(gameTime);
+            base.Update(gameTime, covered);
+        }
+        /// <summary>
+        /// drwing the OK button and the Theme And level selection bar
+        /// </summary>
+        /// <remarks>
+        /// <para>AUTHOR: Tamer Nabil </para>
+        /// </remarks>
+        /// <param name="gameTime">gameTime</param>
+        public override void Draw(GameTime gameTime)
+        {
+            _level.Draw(gameTime);
+            _oKbutton.Draw(ScreenManager.SpriteBatch);
+        }
+
+    }
+}
