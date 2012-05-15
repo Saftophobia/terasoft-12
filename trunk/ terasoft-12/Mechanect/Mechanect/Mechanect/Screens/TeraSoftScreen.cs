824fdf3b1007e0569d889a7a5f8f01eb6adb9391
q3ahmd@gmail.com <q3ahmd@gmail.com@113cb9fd-de2b-231c-2074-2ec382a84f02>
2012-05-15 12:16:20 +0000

    update issue 219 Status: In Progress
    Added Screens for the intro screen

---
+++ trunk/ terasoft-12/Mechanect/Mechanect/Mechanect/Screens/TeraSoftScreen.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading;
+using Mechanect.Common;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+
+namespace Mechanect.Screens
+{
+    class TeraSoftScreen : FadingScreen
+    {
+        public TeraSoftScreen()
+            : base("Resources/Images/Terasoft", 0.5f,0,0,-50)
+        { }
+
+        public override void Update(GameTime gameTime, bool covered)
+        {
+            base.Update(gameTime,false);
+            if (Done)
+            {
+                base.Remove();
+                ScreenManager.AddScreen(new ITworxScreen());
+            }
+        }
+    }
+}
