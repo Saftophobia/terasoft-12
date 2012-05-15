e3f07f636d7a5347b61d3e126c4c37c480c6d766
q3ahmd@gmail.com <q3ahmd@gmail.com@113cb9fd-de2b-231c-2074-2ec382a84f02>
2012-05-15 14:46:35 +0000

    update issue 219 Status: In Progress Labels: Documentation-Ready
    Added documentation to class TeraSoftScreen

--- trunk/ terasoft-12/Mechanect/Mechanect/Mechanect/Screens/TeraSoftScreen.cs
+++ trunk/ terasoft-12/Mechanect/Mechanect/Mechanect/Screens/TeraSoftScreen.cs
@@ -6,12 +6,23 @@ using System;
 
 namespace Mechanect.Screens
 {
+    /// <summary>
+    /// This is a screen containg the GUC logo.
+    /// </summary>
     class TeraSoftScreen : FadingScreen
     {
+        /// <summary>
+        /// This is the default construtor for TeraSoftScreen class.
+        /// </summary>
         public TeraSoftScreen()
             : base("Resources/Images/Terasoft", 0.5f,0,0,-50)
         { }
 
+        /// <summary>
+        /// Updates the content of this screen.
+        /// </summary>
+        /// <param name="gameTime">represents the time of the game.</param>
+        /// <param name="covered">specifies wether the screen is covered.</param>
         public override void Update(GameTime gameTime, bool covered)
         {
             base.Update(gameTime,false);
