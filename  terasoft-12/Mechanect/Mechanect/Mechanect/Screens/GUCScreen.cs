31631552048454d52eb9417e48c911d6cd1defc9
q3ahmd@gmail.com <q3ahmd@gmail.com@113cb9fd-de2b-231c-2074-2ec382a84f02>
2012-05-15 14:38:36 +0000

    update issue 219 Status: In Progress Labels: Documentation-Ready
    Added documentation to class GUCScreen

---  terasoft-12/Mechanect/Mechanect/Mechanect/Screens/GUCScreen.cs
+++  terasoft-12/Mechanect/Mechanect/Mechanect/Screens/GUCScreen.cs
@@ -6,12 +6,23 @@ using Microsoft.Xna.Framework;
 
 namespace Mechanect.Screens
 {
+    /// <summary>
+    /// This is a screen containg the GUC logo.
+    /// </summary>
     class GUCScreen : FadingScreen
     {
+        /// <summary>
+        /// This is the default construtor for GUCScreen class.
+        /// </summary>
         public GUCScreen()
             : base("Resources/Images/GUC", 1f, 0, 0, -70)
         { }
 
+        /// <summary>
+        /// Updates the content of this screen.
+        /// </summary>
+        /// <param name="gameTime">represents the time of the game.</param>
+        /// <param name="covered">specifies wether the screen is covered.</param>
         public override void Update(GameTime gameTime, bool covered)
         {
             base.Update(gameTime, false);
