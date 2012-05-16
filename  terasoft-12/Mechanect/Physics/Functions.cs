dd31b3d2b599bf2f57dae3e1d11f109b0f17a06c
bishoybassem@gmail.com <bishoybassem@gmail.com@113cb9fd-de2b-231c-2074-2ec382a84f02>
2012-05-16 16:45:11 +0000

    update issue 40
    Status: In Progress
    
    updated documentation of Physics/Functions.cs
    added CalculateTime method
    changed CalculateDisplacement, CalculateIntialVelocity parameter types

---  terasoft-12/Mechanect/Physics/Functions.cs
+++  terasoft-12/Mechanect/Physics/Functions.cs
@@ -5,41 +5,59 @@ namespace Physics
 {
     public class Functions
     {
+
         /// <summary>
         /// calculates the vector in direction of another one given its magnitude
         /// </summary>
         /// <param name="anotherVector">the other vector</param>
         /// <param name="magnitude">the magnitude of the vector</param>
-        /// <remarks>Auther : Bishoy Bassem</remarks>
+        /// <remarks>Author : Bishoy Bassem</remarks>
         public static Vector3 GetVectorInDirectionOf(float magnitude, Vector3 anotherVector)
         {
             anotherVector.Normalize();
             return magnitude * anotherVector;
         }
 
+
         /// <summary>
-        /// calculates the displacement vector using the equation r - r0 = v0 * t + 0.5 * a * (t^2) 
+        /// calculates the displacement vector using the equation r - r0 = v0t + 0.5a(t^2) 
         /// </summary>
         /// <param name="intialVelocity">start velocity vector</param>
         /// <param name="acceleration">acceleration vector</param>
         /// <param name="time">time passed</param>
-        /// <remarks>Auther : Bishoy Bassem</remarks>
-        public static Vector3 CalculateDisplacement(Vector3 intialVelocity, Vector3 acceleration, TimeSpan time)
+        /// <remarks>Author : Bishoy Bassem</remarks>
+        public static Vector3 CalculateDisplacement(Vector3 intialVelocity, float acceleration, TimeSpan time)
         {
             float seconds = (float) time.TotalSeconds;
-            return (intialVelocity * seconds) + (0.5f * acceleration * seconds * seconds);
+            Vector3 accelerationVector = GetVectorInDirectionOf(acceleration, intialVelocity);
+            return (intialVelocity * seconds) + (0.5f * accelerationVector * seconds * seconds);
         }
+
 
         /// <summary>
         /// calculates the intial velocity magnitude using the equation vf^2 = v0^2 + 2as
         /// </summary>
+        /// <param name="displacement">displacement vector</param>
         /// <param name="finalVelocity">final velocity magnitude</param>
         /// <param name="acceleration">acceleration magnitude</param>
-        /// <param name="distance">distance</param>
-        /// <remarks>Auther : Bishoy Bassem</remarks>
-        public static float CalculateIntialVelocity(float finalVelocity, float acceleration, float distance)
+        /// <remarks>Author : Bishoy Bassem</remarks>
+        public static Vector3 CalculateIntialVelocity(Vector3 displacement, float finalVelocity, float acceleration)
         {
-            return (float)Math.Sqrt(finalVelocity * finalVelocity - 2 * acceleration * distance);
+            float intialVelocity = (float)Math.Sqrt(finalVelocity * finalVelocity - 2 * acceleration * displacement.Length());
+            return Physics.Functions.GetVectorInDirectionOf(intialVelocity, displacement); 
+        }
+
+
+        /// <summary>
+        /// calculates the time using the equation s = (v0 + vf)t / 2
+        /// </summary>
+        /// <param name="distance">distance magnitude</param>
+        /// <param name="intialVelocity">intial velocity magnitude</param>
+        /// <param name="finalVelocity">final velocity magnitude</param>
+        /// <returns></returns>
+        public static TimeSpan CalculateTime(float distance, float intialVelocity, float finalVelocity)
+        {
+            return TimeSpan.FromSeconds(2 * distance / (intialVelocity + finalVelocity));
         }
 
     }
