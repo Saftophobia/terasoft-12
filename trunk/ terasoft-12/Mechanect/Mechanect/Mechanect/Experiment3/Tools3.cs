2903055040c52763b0a74991d97b3c393915aefb
mk.cena92@gmail.com <mk.cena92@gmail.com@113cb9fd-de2b-231c-2074-2ec382a84f02>
2012-05-17 17:46:55 +0000

    update issue 243
    moved exp3 classes to package expirement3

---
+++ trunk/ terasoft-12/Mechanect/Mechanect/Mechanect/Experiment3/Tools3.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Kinect;
+using Microsoft.Xna.Framework;
+using Mechanect.Common;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Mechanect.Classes
+{
+    public class Tools3
+    {
+      
+
+        /// <summary>
+        /// calculates the friction vector that will cause the velocity to stop without changing the velocity direction
+        /// </summary>
+        /// <param name="velocity">the velocity vector</param>
+        /// <param name="frictionValue">the value of the friction</param>
+        /// <remarks>Auther : Bishoy Bassem</remarks>
+        public static Vector3 calculateFriction(Vector3 velocity, float frictionValue)
+        {
+            return velocity * frictionValue / velocity.Length() * -1;
+        }
+
+
+
+
+        ///<remarks>
+        ///<para>
+        ///Author: Cena
+        ///</para>
+        ///</remarks>
+        /// <summary>
+        /// calculates the final velocity using Vf = Vi + a*t
+        /// </summary>
+        /// <param name="acceleration">acceleration of the body</param>
+        /// <param name="velocityInitial">initial veloctiy of the body</param>
+        /// <param name="totalTime">the total time the body moved</param>
+        /// <returns></returns>
+
+        public static Vector3 GetVelocity(Vector3 position, double totalTime)
+        {
+            return new Vector3(position.X / (float)totalTime, 0, position.Z / (float)totalTime);
+        }
+
+
+
+
+        ///<remarks>
+        ///<para>
+        ///Author: HegazY
+        ///</para>
+        ///</remarks>
+        /// <summary>
+        /// Used to get the customized button with OKButton
+        /// </summary>
+        /// <param name="c">content managaer to load pictures</param>
+        /// <param name="p">position of the button</param>
+        /// <param name="sw">screen width</param>
+        /// <param name="sh">screen height</param>
+        /// <param name="u">instance of user<param>
+        /// <returns>returns OK button</returns>
+        public static Button OKButton(ContentManager c, Vector2 p, int sw, int sh, User u)
+        {
+            return new Button(c.Load<GifAnimation.GifAnimation>("Textures/Buttons/ok-s"),
+           c.Load<GifAnimation.GifAnimation>("Textures/Buttons/ok-m"), p, sw, sh,
+           c.Load<Texture2D>("Textures/Buttons/hand"), u);
+        }
+
+
+        ///<remarks>
+        ///<para>
+        ///Author: HegazY
+        ///</para>
+        ///</remarks>
+        /// <summary>
+        /// Used to get the customized button with MainMenu
+        /// </summary>
+        /// <param name="c">content managaer to load pictures</param>
+        /// <param name="p">position of the button</param>
+        /// <param name="sw">screen width</param>
+        /// <param name="sh">screen height</param>
+        /// <param name="u">instance of user<param>
+        /// <returns>returns MainMenu button</returns>
+        public static Button MainMenuButton(ContentManager c, Vector2 p, int sw, int sh, User u)
+        {
+            return new Button(c.Load<GifAnimation.GifAnimation>("Textures/Buttons/menu-s"),
+           c.Load<GifAnimation.GifAnimation>("Textures/Buttons/menu-m"), p, sw, sh,
+           c.Load<Texture2D>("Textures/Buttons/hand"), u);
+        }
+
+
+        ///<remarks>
+        ///<para>
+        ///Author: HegazY
+        ///</para>
+        ///</remarks>
+        /// <summary>
+        /// Used to get the customized button with NewGame
+        /// </summary>
+        /// <param name="c">content managaer to load pictures</param>
+        /// <param name="p">position of the button</param>
+        /// <param name="sw">screen width</param>
+        /// <param name="sh">screen height</param>
+        /// <param name="u">instance of user<param>
+        /// <returns>returns NewGame button</returns>
+        public static Button NewGameButton(ContentManager c, Vector2 p, int sw, int sh, User u)
+        {
+            return new Button(c.Load<GifAnimation.GifAnimation>("Textures/Buttons/newgame-s"),
+           c.Load<GifAnimation.GifAnimation>("Textures/Buttons/newgame-m"), p, sw, sh,
+           c.Load<Texture2D>("Textures/Buttons/hand"), u);
+        }
+
+
+        ///<remarks>
+        ///<para>
+        ///Author: HegazY
+        ///</para>
+        ///</remarks>
+        /// <summary>
+        /// displaying the wining word on the screen.
+        /// </summary>
+        /// <param name="spriteBatch">used to draw images on the screen</param>
+        /// <param name="content">used to load the images</param>
+        /// <param name="position">the desired position</param>
+        /// <param name="status">true: if the user won, false: if the user lost</param>
+        public static void DislayIsWin(SpriteBatch spriteBatch, ContentManager content, Vector2 position, bool status)
+        {
+            spriteBatch.Begin();
+            if (status)
+            {
+                Texture2D winningPicture = content.Load<Texture2D>("Textures/WorL/winner");
+                spriteBatch.Draw(winningPicture, position, Color.White);
+            }
+            else
+            {
+                Texture2D losingPicture = content.Load<Texture2D>("Textures/WorL/looser");
+                spriteBatch.Draw(losingPicture, position, Color.White);
+            }
+            spriteBatch.End();
+        }
+
+        /// <summary>
+        /// Generates a random float value between two float numbers.
+        /// </summary>
+        /// <remarks>
+        ///<para>AUTHOR: Khaled Salah </para>
+        ///</remarks>
+        /// <param name="min">
+        /// The minimum value. 
+        /// </param>
+        /// /// <param name="max">
+        /// The maximum value.
+        /// </param>
+        /// <returns>
+        /// Float number which is the generated random value.
+        /// </returns>
+
+        public static float GenerateRandomValue(float min, float max)
+        {
+            if (max > min)
+            {
+                var random = new Random();
+                var value = ((float)(random.NextDouble() * (max - min))) + min;
+                return value;
+            }
+            else throw new ArgumentException("max value has to be greater than min value");
+        }
+    }
+}
---
+++ trunk/ terasoft-12/Mechanect/Mechanect/Mechanect/Experiment3/User3.cs
@@ -0,0 +1,628 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Kinect;
+using Mechanect.Screens;
+using Mechanect.Common;
+
+namespace Mechanect.Classes
+{
+    public class User3:User
+    {
+       
+
+        private Joint trackedJoint;
+        public Joint TrackedJoint  //  the user's tracked joint
+        {
+            get
+            {
+                return trackedJoint;
+            }
+            set
+            {
+                trackedJoint = value;
+            }
+        }
+
+        private bool rightLeg;  // is the user using his right leg or left
+        public bool RightLeg
+        {
+            get
+            {
+                return rightLeg;
+            }
+            set
+            {
+                rightLeg = value;
+            }
+        }
+
+        private bool movedForward; // has the user moved his leg forward
+        public bool MovedForward
+        {
+            get
+            {
+                return movedForward;
+            }
+            set
+            {
+                movedForward = value;
+            }
+        }
+
+        //x position
+        private double initialRightLegPositionX; //meters // stores the initial position of the right leg
+        public double InitialRightLegPositionX
+        {
+            get
+            {
+                return initialRightLegPositionX;
+            }
+            set
+            {
+                initialRightLegPositionX = value;
+            }
+        }
+        private double currentRightLegPositionX; //meters // stores the current position of the right leg 
+        public double CurrentRightLegPositionX
+        {
+            get
+            {
+                return currentRightLegPositionX;
+            }
+            set
+            {
+                currentRightLegPositionX = value;
+            }
+        }
+
+        //z position
+        private double initialRightLegPositionZ; //meters
+        public double InitialRightLegPositionZ
+        {
+            get
+            {
+                return initialRightLegPositionZ;
+            }
+            set
+            {
+                initialRightLegPositionZ = value;
+            }
+        }
+
+
+        private double currentRightLegPositionZ; //meters
+        public double CurrentRightLegPositionZ
+        {
+            get
+            {
+                return currentRightLegPositionZ;
+            }
+            set
+            {
+                currentRightLegPositionZ = value;
+            }
+        }
+
+        //left leg
+
+        //x position
+        private double initialLeftLegPositionX; //meters // stores the initial position of the left leg
+        public double InitialLeftLegPositionX
+        {
+            get
+            {
+                return initialLeftLegPositionX;
+            }
+            set
+            {
+                initialLeftLegPositionX = value;
+            }
+        }
+
+        private double currentLeftLegPositionX; //meters // stores the current position of the left leg 
+        public double CurrentLeftLegPositionX
+        {
+            get
+            {
+                return currentLeftLegPositionX;
+            }
+            set
+            {
+                currentLeftLegPositionX = value;
+            }
+        }
+
+        //z position
+        private double initialLeftLegPositionZ; //meters
+        public double InitialLeftLegPositionZ
+        {
+            get
+            {
+                return initialLeftLegPositionZ;
+            }
+            set
+            {
+                initialLeftLegPositionZ = value;
+            }
+        }
+
+
+        private double currentLeftLegPositionZ; //meters
+        public double CurrentLeftLegPositionZ
+        {
+            get
+            {
+                return currentLeftLegPositionZ;
+            }
+            set
+            {
+                currentLeftLegPositionZ = value;
+            }
+        }
+
+
+
+        public Vector3 velocity { get; set; }
+
+        private bool trying; // is the user in the trying mode or shooting
+        public bool Trying
+        {
+            get
+            {
+                return trying;
+            }
+            set
+            {
+                trying = value;
+            }
+        }
+
+        private double angle; // angle of the leg
+        public double Angle
+        {
+            get
+            {
+                return angle;
+            }
+            set
+            {
+                angle = value;
+            }
+        }
+        private double assumedLegMass;
+        public double AssumedLegMass
+        {
+            get
+            {
+                return assumedLegMass;
+            }
+            set
+            {
+                assumedLegMass = value;
+            }
+        }
+        private Vector3 shootingPosition;
+        public Vector3 ShootingPosition
+        {
+            get
+            {
+                return shootingPosition;
+            }
+            set
+            {
+                shootingPosition = value;
+            }
+        }
+
+        public double currentTime { get; set; }
+        public double initialTime { get; set; }
+        public int timeCounter { get; set; }
+        public bool hasJustStarted { get; set; }
+        public bool hasJustSlipped { get; set; }
+        public bool hasShot { get; set; }
+        public bool firstUpdate { get; set; }
+        public int consecutiveFrame { get; set; }
+        public double previousLeftLegPositionX;
+        public double previousLeftLegPositionZ;
+        public double previousRightLegPositionX;
+        public double previousRightLegPositionZ;
+
+
+        public User3()
+        {
+
+
+
+            initialLeftLegPositionX = 0;
+            currentLeftLegPositionX = 0;
+            initialLeftLegPositionZ = 0;
+            currentLeftLegPositionZ = 0;
+
+            initialRightLegPositionX = 0;
+            currentRightLegPositionX = 0;
+            initialRightLegPositionZ = 0;
+            currentRightLegPositionZ = 0;
+
+            velocity = Vector3.Zero;
+            angle = 0;
+
+            movedForward = false;
+            trying = true;
+            assumedLegMass = 0.01f;
+            currentTime = 0;
+            initialTime = 0;
+            timeCounter = 0;
+            hasJustSlipped = false;
+            hasJustStarted = true;
+            hasShot = false;
+            firstUpdate = true;
+            consecutiveFrame = 0;
+
+
+
+        }
+        /// <summary>
+        /// Takes the minimum and maximum possible values for the mass of the foot and sets it to a random float number between these two numbers.
+        /// </summary>
+        /// <remarks>
+        ///<para>AUTHOR: Khaled Salah </para>
+        ///</remarks>
+        /// <param name="minMass">
+        /// The minimum possible value for the foot mass.
+        /// </param>
+        /// /// <param name="maxMass">
+        /// The maximum possible value for the foot mass.
+        /// </param>
+     
+         public void SetFootMassInThisRange(float minMass, float maxMass)
+         {
+             assumedLegMass = GenerateFootMass(minMass, maxMass);
+         } 
+         /// <summary>
+         /// Takes the minimum and maximum possible values for the mass of the foot and generates a random float between these two numbers.
+         /// </summary>
+         /// <remarks>
+         ///<para>AUTHOR: Khaled Salah </para>
+         ///</remarks>
+         /// <param name="minMass">
+         /// The minimum possible value for the foot mass.
+         /// </param>
+         /// /// <param name="maxMass">
+         /// The maximum possible value for the foot mass.
+         /// </param>
+         /// <returns>
+         /// Float number which is the generated random value.
+         /// </returns>
+         public float GenerateFootMass(float min, float max)
+         {
+             if ((min >= 0) && (max >= 0) && (max > min))
+             {
+                 var random = new Random();
+                 var generatedMass = ((float)(random.NextDouble() * (max - min))) + min;
+                 return generatedMass;
+             }
+             else throw new ArgumentException("parameters have to be non negative numbers and max value has to be greater than min value");
+         }
+
+         ///<remarks>
+         ///<para>
+         ///Author: Cena
+         ///</para>
+         ///</remarks>>
+         /// <summary>
+         ///  Updates the velocity, angle variables of the User3 after each captured skeleton frame.
+         /// </summary>
+
+         public void UpdateMeasuringVelocityAndAngle(GameTime gameTime)
+         {
+             setSkeleton();
+             Skeleton skeleton = USER;
+             if (skeleton != null)
+             {
+                 if (GameScreen.frameNumber % 2 == 0)
+                 {
+                     StoreTime(gameTime);
+                     if (!hasShot)
+                     {
+                         consecutiveFrame++;
+                         if (HasJustStarted())
+                         {
+                             StoreInitialPosition();
+                             SetStarted();
+                         }
+                         else
+                         {
+                             StorePreviousPosition();
+                             StoreCurrentPosition();
+
+                             if (HasPlayerMoved())
+                             {
+                                 if (IsMovingForward())
+                                 {
+                                     UpdateSpeed();
+                                     UpdateAngle();
+                                 }
+                                 else
+                                 {
+                                     if (HasAlreadyMovedForward())
+                                     {
+                                         //if (consecutiveFrame > 2)
+                                         //{
+                                         //    consecutiveFrame = 0;
+                                         //    hasJustSlipped = false;
+                                         //}
+
+                                         //if (HasJustSlipped())  // done shooting
+                                         //    hasShot = true;
+                                         //else
+                                         //    hasJustSlipped = true;
+                                         hasShot = true;
+
+
+                                     }
+                                     else
+                                     {
+                                         StoreInitialPosition(); // the player is moving his leg backwards
+                                         StoreInitialTime(gameTime);
+                                     }
+                                 }
+                             }
+                         }
+                     }
+
+                 }
+
+             }
+             else
+                 ResetUserForShootingOrTryingAgain();
+
+             GameScreen.frameNumber++;
+         }
+
+
+         ///<remarks>
+         ///<para>
+         ///Author: Cena
+         ///</para>
+         ///</remarks>
+         /// <summary>
+         /// Checks if the User3 is currently moving their leg forward or not
+         /// </summary>
+         /// <returns>returns true iff the User3 moved his leg forward</returns>
+
+         public bool IsMovingForward()
+         {
+             if (this.USER != null)
+             {
+                 double currentZ = CurrentLeftLegPositionZ;
+                 double previousZ = previousLeftLegPositionZ;
+                 if (RightLeg)
+                 {
+                     currentZ = CurrentRightLegPositionZ;
+                     previousZ = previousRightLegPositionZ;
+                 }
+
+                 if (currentZ - previousZ < (-1 * Constants3.legMovementTolerance))
+                 {
+                     MovedForward = true;
+                     return true;
+
+                 }
+
+                 if (RightLeg)
+                 {
+                     initialRightLegPositionZ = CurrentRightLegPositionZ;
+                     InitialRightLegPositionX = CurrentRightLegPositionX;
+                 }
+                 else
+                 {
+                     initialLeftLegPositionZ = CurrentLeftLegPositionZ;
+                     InitialLeftLegPositionX = currentLeftLegPositionX;
+                 }
+
+
+             }
+             return false;
+         }
+
+         public void UpdateAngle()
+         {
+             double positionX1 = InitialLeftLegPositionX;
+             double positionX2 = currentLeftLegPositionX;
+             double positionZ1 = initialLeftLegPositionZ;
+             double positionZ2 = currentLeftLegPositionZ;
+             if (rightLeg)
+             {
+                 positionX1 = initialRightLegPositionX;
+                 positionX2 = CurrentRightLegPositionX;
+                 positionZ1 = initialRightLegPositionZ;
+                 positionZ2 = CurrentRightLegPositionZ;
+             }
+             if (positionZ2 != positionZ1)
+                 Angle = (Math.Atan((positionX2 - positionX1) / (Math.Abs(positionZ2 - positionZ1))));
+             else
+                 if (positionX1 < positionX2)
+                     Angle = (Math.PI / 2);
+                 else
+                     Angle = -(Math.PI / 2);
+
+         }
+
+
+         public bool HasPlayerMoved()
+         {
+
+             Skeleton player = USER;
+             if (Math.Abs(CurrentLeftLegPositionZ - initialLeftLegPositionZ) > Constants3.legMovementTolerance)
+             {
+                 RightLeg = false;
+                 TrackedJoint = player.Joints[JointType.AnkleLeft];
+                 return true;
+             }
+             if (Math.Abs(CurrentRightLegPositionZ - initialLeftLegPositionZ) > Constants3.legMovementTolerance)
+             {
+                 RightLeg = true;
+                 TrackedJoint = player.Joints[JointType.AnkleRight];
+                 return true;
+             }
+             return false;
+         }
+         public bool HasJustSlipped()
+         {
+             return hasJustSlipped;
+         }
+         public bool HasAlreadyMovedForward()
+         {
+             return movedForward;
+         }
+         public bool HasShot()
+         {
+             return hasShot;
+         }
+         public void UpdateSpeed()
+         {
+             double currentZ = CurrentLeftLegPositionZ;
+             double initialZ = InitialLeftLegPositionZ;
+             double currentX = CurrentLeftLegPositionX;
+             double initialX = initialLeftLegPositionX;
+             if (RightLeg)
+             {
+                 currentZ = CurrentRightLegPositionZ;
+                 initialZ = InitialRightLegPositionZ;
+                 currentX = currentRightLegPositionX;
+                 initialX = InitialRightLegPositionX;
+             }
+             double deltaTime = Math.Abs(currentTime - initialTime);
+             Vector3 deltaPosition = new Vector3((float)(currentZ - initialZ), 0, (float)(currentX - initialX));
+             Vector3 finalVelocity = Tools3.GetVelocity(deltaPosition, deltaTime);
+             if (Trying)
+                 if (velocity.Length() <= finalVelocity.Length())
+                     velocity = finalVelocity;
+                 else
+                     velocity = finalVelocity;
+         }
+
+
+         public bool HasJustStarted()
+         {
+             return hasJustStarted;
+         }
+         public void SetStarted()
+         {
+             hasJustStarted = false;
+         }
+
+         public void StoreInitialPosition()
+         {
+             initialLeftLegPositionX = USER.Joints[JointType.AnkleLeft].Position.X;
+             initialLeftLegPositionZ = USER.Joints[JointType.AnkleLeft].Position.Z;
+             initialRightLegPositionX = USER.Joints[JointType.AnkleRight].Position.X;
+             initialRightLegPositionZ = USER.Joints[JointType.AnkleRight].Position.Z;
+             previousLeftLegPositionX = initialLeftLegPositionX;
+             previousLeftLegPositionZ = initialLeftLegPositionZ;
+             previousRightLegPositionX = initialRightLegPositionX;
+             previousRightLegPositionZ = initialRightLegPositionZ;
+         }
+
+
+         public void StoreCurrentPosition()
+         {
+             CurrentLeftLegPositionX = USER.Joints[JointType.AnkleLeft].Position.X;
+             CurrentLeftLegPositionZ = USER.Joints[JointType.AnkleLeft].Position.Z;
+             CurrentRightLegPositionX = USER.Joints[JointType.AnkleRight].Position.X;
+             CurrentRightLegPositionZ = USER.Joints[JointType.AnkleRight].Position.Z;
+         }
+
+
+         public void StoreInitialTime(GameTime gameTime)
+         {
+             initialTime = gameTime.TotalGameTime.TotalSeconds;
+         }
+         public void StoreTime(GameTime gameTime)
+         {
+             if (firstUpdate)
+             {
+                 StoreInitialTime(gameTime);
+                 firstUpdate = false;
+             }
+             else
+                 currentTime = gameTime.TotalGameTime.TotalSeconds;
+         }
+
+         public void StorePreviousPosition()
+         {
+             if (currentRightLegPositionZ != 0)
+             {
+                 previousLeftLegPositionX = currentLeftLegPositionX;
+                 previousLeftLegPositionZ = currentLeftLegPositionZ;
+                 previousRightLegPositionX = currentRightLegPositionX;
+                 previousRightLegPositionZ = currentRightLegPositionZ;
+             }
+         }
+
+
+         ///<remarks>
+         ///<para>
+         ///Author: Cena
+         ///</para>
+         ///</remarks>
+         /// <summary>
+         /// scales the velocity of the leg's User3 relative to the assumed mass of the User3's leg and maps the velocity in meters to pixels
+         /// </summary>
+         /// <returns>returns the scales velocity</returns>
+         public Vector3 SetVelocityRelativeToGivenMass()
+         {
+             float ratio = (float)(Constants3.normalLegMass / assumedLegMass);
+             return new Vector3(velocity.X * ratio, 0, velocity.Z * ratio);
+
+         }
+
+
+         ///<remarks>
+         ///<para>
+         ///Author: Cena
+         ///</para>
+         ///</remarks>
+         /// <summary>
+         /// initializes all the variables that stores the User3's movement inorder to try shooting again
+         /// </summary>
+
+         public void ResetUserForShootingOrTryingAgain()
+         {
+
+             InitialLeftLegPositionX = 0;
+             CurrentLeftLegPositionX = 0;
+             InitialLeftLegPositionZ = 0;
+             CurrentLeftLegPositionZ = 0;
+
+             InitialRightLegPositionX = 0;
+             CurrentRightLegPositionX = 0;
+             InitialRightLegPositionZ = 0;
+             CurrentRightLegPositionZ = 0;
+
+             currentTime = 0;
+             initialTime = 0;
+             timeCounter = 0;
+             hasJustSlipped = false;
+             hasJustStarted = true;
+             hasShot = false;
+             firstUpdate = true;
+             consecutiveFrame = 0;
+
+             velocity = Vector3.Zero;
+             Angle = 0;
+
+             MovedForward = false;
+             Trying = true;
+             GameScreen.frameNumber = 0;
+
+
+
+
+         }
+       
+    }
+}
