7dc2e18eede116201df00bf09483c58464808315
q3ahmd@gmail.com <q3ahmd@gmail.com@113cb9fd-de2b-231c-2074-2ec382a84f02>
2012-05-17 11:00:07 +0000

    update issue 16 Status: In Progress
    Renamed tests class

---
+++ trunk/ terasoft-12/Mechanect/TestsLib/badr/SolvabilityTests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using Mechanect.Classes;
+using Microsoft.Xna.Framework;
+using Mechanect;
+using System.Threading;
+
+namespace Tests
+{
+    [TestFixture]
+    public class SolvabilityTests
+    {
+        Game1 game1;
+        Environment3 environment3;
+
+        [SetUp]
+        public void Init()
+        {
+            game1 = new Game1();
+            game1.Content.RootDirectory = @"F:\SE\ terasoft-12\Mechanect\TestsLib\bin\Debug\Content";
+            game1.Run();
+            environment3 = Constants3.environment3;
+        }
+
+        [Test]
+        public void TestsHoleXPosition()
+        {
+            Assert.LessOrEqual(Math.Abs(environment3.HoleProperty.Position.X), Constants3.maxHolePosX - environment3.HoleProperty.Radius);
+        }
+
+        [Test]
+        public void TestsHoleZPosition()
+        {
+            Assert.LessOrEqual(Math.Abs(environment3.HoleProperty.Position.Z), Constants3.maxHolePosZ - environment3.HoleProperty.Radius,
+                "pos: " + environment3.HoleProperty.Position.Z);
+
+        }
+
+        [Test]
+        public void TestsHoleAfterShootingPosition()
+        {
+            Assert.LessOrEqual(environment3.HoleProperty.Position.Z, environment3.user.ShootingPosition.Z);
+        }
+    }
+}
