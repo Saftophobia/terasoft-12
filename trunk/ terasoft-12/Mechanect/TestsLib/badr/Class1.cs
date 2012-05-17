fc34c3dee1b843f35c623538807f87792832f325
q3ahmd@gmail.com <q3ahmd@gmail.com@113cb9fd-de2b-231c-2074-2ec382a84f02>
2012-05-17 10:15:25 +0000

    update issue 16 Status: In Progress Integration
    Added Tests for this story

--- trunk/ terasoft-12/Mechanect/TestsLib/badr/Class1.cs
+++ trunk/ terasoft-12/Mechanect/TestsLib/badr/Class1.cs
@@ -13,13 +13,36 @@ namespace Tests
     [TestFixture]
     public class Class1
     {
-        Game1 game1 = new Game1();
+        Game1 game1;
+        Environment3 environment3;
+
+        [SetUp]
+        public void Init()
+        {
+            game1 = new Game1();
+            game1.Content.RootDirectory = @"F:\SE\ terasoft-12\Mechanect\TestsLib\bin\Debug\Content";
+            game1.Run();
+            environment3 = Constants3.environment3;
+        }
         
         [Test]
-        public void Test1()
+        public void TestsHoleXPosition()
         {
-            game1.Content.RootDirectory = @"F:\SE\ terasoft-12\Mechanect\Tests\bin\Debug\Content";
-            game1.Run();
+            Assert.LessOrEqual(Math.Abs(environment3.HoleProperty.Position.X), Constants3.maxHolePosX - environment3.HoleProperty.Radius);
+        }
+
+        [Test]
+        public void TestsHoleZPosition()
+        {
+            Assert.LessOrEqual(Math.Abs(environment3.HoleProperty.Position.Z), Constants3.maxHolePosZ - environment3.HoleProperty.Radius,
+                "pos: "+environment3.HoleProperty.Position.Z);
+            
+        }
+
+        [Test]
+        public void TestsHoleAfterShootingPosition()
+        {
+            Assert.LessOrEqual(environment3.HoleProperty.Position.Z,environment3.user.ShootingPosition.Z);
         }
     }
 }
