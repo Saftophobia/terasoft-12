d6afdb532b70b6d3832a940012bef770f4e226a7
asamgdla@gmail.com <asamgdla@gmail.com@113cb9fd-de2b-231c-2074-2ec382a84f02>
2012-05-17 15:06:07 +0000

    update issue 235 Status: In-progress Added the class Moving2DAvatar to use a 2D model instead of 3D

---
+++  terasoft-12/Mechanect/Mechanect/Mechanect/Classes/Moving2DAvatar.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Mechanect.Classes
+{
+    class Moving2DAvatar
+    {
+        Vector2 Position;
+        Texture2D Texture;
+
+        public Moving2DAvatar(Texture2D a, Vector2 b)
+        {
+            Position = b;
+            Texture = a;
+        }
+
+        /// <remarks>
+        /// <para>Author: Ahmed Shirin</para>
+        /// <para>Date Written 17/5/2012</para>
+        /// <para>Date Modified 17/5/2012</para>
+        /// </remarks>
+        /// <summary>
+        /// The function Move is used to move the avatar in along the Y-axis.
+        /// </summary>
+        /// <param name="value">The value to be decremented from the Y position of the avatar.</param>
+        /// <returns>void.</returns>
+        public void Move(int value)
+        {
+            Position.Y -= value;
+        }
+
+        /// <remarks>
+        /// <para>Author: Ahmed Shirin</para>
+        /// <para>Date Written 17/5/2012</para>
+        /// <para>Date Modified 17/5/2012</para>
+        /// </remarks>
+        /// <summary>
+        /// The function Draw is used to draw the 2D avatar.
+        /// </summary>
+        /// <param name="value">An instance of the spriteBatch.</param>
+        /// <returns>void.</returns>
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Begin();
+            spriteBatch.Draw(Texture, Position, Microsoft.Xna.Framework.Color.White);
+            spriteBatch.End();
+        }
+    }
+}
