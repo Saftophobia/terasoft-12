983ad8ccb7b0d510753f03b0c6024a9dba553816
ahmedhegazy835 <ahmedhegazy835@113cb9fd-de2b-231c-2074-2ec382a84f02>
2012-05-17 12:38:46 +0000

    update issue 2221 Status: In Progress adding Classes/SkinnedModel.cs

---
+++ trunk/ terasoft-12/Mechanect/Mechanect/Mechanect/Classes/SkinnedModel.cs
@@ -0,0 +1,111 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using SkinnedModel;
+
+namespace Mechanect.Classes
+{
+    class SkinnedModel
+    {
+
+        # region Fields
+
+        private Model model;
+        private SkinningData skinningData;
+
+        private Matrix[] originalBones;
+        private Matrix[] boneTransforms;
+        private Matrix[] worldTransforms;
+        private Matrix[] skinTransforms;
+
+        public Vector3 Position { get; set; }
+        public Vector3 Rotation { get; set; }
+        public Vector3 Scale { get; set; }
+
+        #endregion
+
+        #region Initialization
+
+        public SkinnedModel(Model model, Vector3 position, Vector3 rotation, Vector3 scale)
+        {
+            this.model = model;
+            this.skinningData = model.Tag as SkinningData;
+            
+            this.originalBones = new Matrix[skinningData.BindPose.Count];
+            this.skinningData.CopyBindPose(originalBones);
+
+            this.boneTransforms = new Matrix[skinningData.BindPose.Count];
+            this.skinningData.CopyBindPose(boneTransforms);
+
+            this.worldTransforms = new Matrix[skinningData.BindPose.Count];
+            this.skinTransforms = new Matrix[skinningData.BindPose.Count];
+
+            Position = position;
+            Rotation = rotation;
+            Scale = scale;
+        }
+
+        #endregion
+
+        #region Update and Draw
+
+        public void Update()
+        {
+            UpdateWorldTransforms();
+            UpdateSkinTransforms();
+        }
+
+        private void UpdateWorldTransforms()
+        {
+            Matrix rootTransform = Matrix.Identity;
+            // Root bone.
+            worldTransforms[0] = boneTransforms[0] * rootTransform;
+
+            // Child bones.
+            for (int bone = 1; bone < worldTransforms.Length; bone++)
+            {
+                int parentBone = skinningData.SkeletonHierarchy[bone];
+
+                worldTransforms[bone] = boneTransforms[bone] *
+                                             worldTransforms[parentBone];
+            }
+        }
+
+        private void UpdateSkinTransforms()
+        {
+            for (int bone = 0; bone < skinTransforms.Length; bone++)
+            {
+                skinTransforms[bone] = skinningData.InverseBindPose[bone] *
+                                            worldTransforms[bone];
+            }
+        }
+
+        public void Draw(GameTime gameTimem, Matrix view, Matrix projection)
+        {
+            Matrix[] bones = skinTransforms;
+
+            // Render the skinned mesh.
+            foreach (ModelMesh mesh in model.Meshes)
+            {
+                Matrix world = Matrix.CreateScale(Scale) * Matrix.CreateFromYawPitchRoll(Rotation.Y, Rotation.X, Rotation.Z) * Matrix.CreateTranslation(Position);
+                foreach (SkinnedEffect effect in mesh.Effects)
+                {
+                    effect.SetBoneTransforms(bones);
+
+                    effect.View = view;
+                    effect.Projection = projection;
+                    effect.World = world;
+
+                    effect.EnableDefaultLighting();
+                    effect.SpecularColor = new Vector3(0.25f);
+                    effect.SpecularPower = 16;
+                }
+
+                mesh.Draw();
+            }
+        }
+
+        #endregion
+    }
+}
