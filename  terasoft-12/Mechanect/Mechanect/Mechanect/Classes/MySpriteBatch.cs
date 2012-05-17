2048e6a1bdcbd82eb1ea73df14b53e6ec0cf7ea4
mohamed.alzayat.eng@gmail.com <mohamed.alzayat.eng@gmail.com@113cb9fd-de2b-231c-2074-2ec382a84f02>
2012-05-17 15:53:59 +0000

    update issue 3
    Status: In-Progress
    
    Modifyed the predator, prey, and aquarium class to have their objects drawn in them.
    Documentation of mentioned classes are ready

---  terasoft-12/Mechanect/Mechanect/Mechanect/Classes/MySpriteBatch.cs
+++  terasoft-12/Mechanect/Mechanect/Mechanect/Classes/MySpriteBatch.cs
@@ -8,6 +8,9 @@ using Microsoft.Xna.Framework;
 using Mechanect.Common;
 namespace Mechanect.Experiment2.UI
 {
+    /// <summary>
+    /// This class was created just to eliminate the duplication of the customized Draw method.
+    /// </summary>
     class MySpriteBatch 
     {
         SpriteBatch SpriteBatch;
@@ -20,28 +23,21 @@ namespace Mechanect.Experiment2.UI
             this.SpriteBatch = SpriteBatch;
         }
         /// <summary>
-        /// Draws The object specified from it's origin
+        /// Draws the texture specified from it's origin
         /// </summary>
+        /// <para>AUTHOR: Mohamed Alzayat </para>   
+        /// <para>DATE WRITTEN: May, 17 </para>
+        /// <para>DATE MODIFIED: May, 17  </para>
+        /// </remarks>
         /// <param name="texture2D">The texture to be drawn</param>
-        /// <param name="position">The position of drawing on screen</param>
+        /// <param name="position">The position of drawing on screen (origin of drawing)</param>
         /// <param name="angle">The rotation angle of your texture</param>
         /// <param name="scale">The scaling of the texture</param>
-        void Draw(Texture2D texture2D, Vector2 position, float angle,float scale)
+        public void DrawTexture(Texture2D texture2D, Vector2 position, float angle,float scale)
         {
-            SpriteBatch.Draw(texture2D, position, null, Color.White, angle, CenterOfTexture(texture2D,position,scale), scale, SpriteEffects.None, 0);
+            SpriteBatch.Draw(texture2D, position, null, Color.White, angle, new Vector2(position.X,position.Y), scale, SpriteEffects.None, 0);
         }
-        /// <summary>
-        /// Enables you to draw the Texture2D from it's origin
-        /// N.B: This Method doesn't draw, it just returns a position Vector2 that will be used as origin in drawing.
-        /// </summary>
-        /// <param name="texture2D">Texture that needs to be drawn with the position at it's center</param>
-        /// <param name="position">The position of the texture</param>
-        /// <param name="scale">Takes the scaling of the Texture2D</param>
-        /// <returns></returns>
-        private Vector2 CenterOfTexture(Texture2D texture2D, Vector2 position, float scale)
-        {
-            return new Vector2(position.X + texture2D.Width*scale/2, position.Y + texture2D.Height*scale/2);          
-        }
+      
        
     }
 }
