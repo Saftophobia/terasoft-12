6a6cd05af233aa10b01f1f4299c9b6d6b370184c
mk.cena92@gmail.com <mk.cena92@gmail.com@113cb9fd-de2b-231c-2074-2ec382a84f02>
2012-05-17 19:09:53 +0000

    update issue 243
    renaming package  Experiment3 to Exp3

---
+++ trunk/ terasoft-12/Mechanect/Mechanect/Mechanect/Exp3/BallAnimation.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using UI.Animation;
+
+namespace Mechanect.Exp3
+{
+    public class BallAnimation : ModelLinearAnimation
+    {
+        
+        private ModelFramedAnimation fallAnimation;
+        private Ball ball;
+        private Vector3 holePosition;
+        private Vector3 stopPosition;
+
+        public BallAnimation(Ball ball, Vector3 shootVelocity, float friction, Vector3 holePosition)
+            : base(ball, shootVelocity, friction, TimeSpan.FromSeconds(10))
+        {
+            this.ball = ball;
+            ball.Rotation = Vector3.Zero;
+            this.holePosition = holePosition;
+            fallAnimation = new ModelFramedAnimation(ball);
+            stopPosition = Physics.Functions.CalculateDisplacement(shootVelocity, friction, TimeSpan.FromSeconds(Math.Abs(shootVelocity.Length() / friction)));
+        }
+
+        public override void Update(TimeSpan elapsed)
+        {
+            if (!base.Finished())
+            {
+                base.Update(elapsed);
+                ball.Rotate(Displacement);
+                return;
+            }
+            // check if ball fell and animte
+        }
+
+        public override bool Finished()
+        {
+            bool ballFell = true;
+            if (ballFell)
+            {
+                return base.Finished() && fallAnimation.Finished();
+            }
+            else
+            {
+                return base.Finished();
+            }
+        }
+
+    }
+}
