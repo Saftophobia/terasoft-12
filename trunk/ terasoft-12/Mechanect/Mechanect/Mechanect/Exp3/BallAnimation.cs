80b3df51cc245c74dbe3d842f7fdae0c26b7d894
bishoybassem@gmail.com <bishoybassem@gmail.com@113cb9fd-de2b-231c-2074-2ec382a84f02>
2012-05-17 22:10:43 +0000

    update issue update issue 226
    Status: In Progress
    
    edited Update and Finished methods in BallAnimation class

--- trunk/ terasoft-12/Mechanect/Mechanect/Mechanect/Exp3/BallAnimation.cs
+++ trunk/ terasoft-12/Mechanect/Mechanect/Mechanect/Exp3/BallAnimation.cs
@@ -9,34 +9,20 @@ namespace Mechanect.Exp3
 {
     public class BallAnimation : ModelLinearAnimation
     {
-        
-        private ModelFramedAnimation fallAnimation;
+
         private Ball ball;
-        private Vector3 holePosition;
+        private ModelFramedAnimation fallAnimation;
         private Vector3 stopPosition;
-        private bool ballWillFall;
 
-        [System.Obsolete("This Constructor will no longer be used.", false)]
-        public BallAnimation(Ball ball, Vector3 shootVelocity, float friction, Vector3 holePosition)
-            : base(ball, shootVelocity, friction, TimeSpan.FromSeconds(10))
+        public BallAnimation(Ball ball, Vector3 velocity, float friction, Vector3 holePosition, float holeRadius)
+            : base(ball, velocity, friction, TimeSpan.FromSeconds(10))
         {
             this.ball = ball;
-            this.holePosition = holePosition;
-            fallAnimation = new ModelFramedAnimation(ball);
-            stopPosition = Physics.Functions.CalculateDisplacement(shootVelocity, friction, TimeSpan.FromSeconds(Math.Abs(shootVelocity.Length() / friction)));
-
-        }
-
-        public BallAnimation(Ball ball, Vector3 shootVelocity, float friction, Vector3 holePosition, float holeRadius)
-            : base(ball, shootVelocity, friction, TimeSpan.FromSeconds(10))
-        {
-            this.ball = ball;
-            this.holePosition = holePosition;
-            fallAnimation = new ModelFramedAnimation(ball);
-            stopPosition = Physics.Functions.CalculateDisplacement(shootVelocity, friction, TimeSpan.FromSeconds(Math.Abs(shootVelocity.Length() / friction)));
+            stopPosition = Physics.Functions.CalculateDisplacement(velocity, friction, TimeSpan.FromSeconds(Math.Abs(velocity.Length() / friction)));
 
             if (Vector3.Distance(stopPosition, holePosition) < (ball.Radius + holeRadius))
             {
+                fallAnimation = new ModelFramedAnimation(ball);
                 if (stopPosition.X < holePosition.X)
                     fallAnimation.AddFrame(new Vector3(stopPosition.X + 0.5f, -0.5f, stopPosition.Z - 0.5f), Vector3.Zero, TimeSpan.FromSeconds(0.2));
                 else
@@ -45,44 +31,32 @@ namespace Mechanect.Exp3
                     else fallAnimation.AddFrame(stopPosition, Vector3.Zero, TimeSpan.FromSeconds(0.2));
                 }
                 fallAnimation.AddFrame(holePosition, Vector3.Zero, TimeSpan.FromSeconds(0.75));
-
-                ballWillFall = true;
             }
 
         }
 
         public override void Update(TimeSpan elapsed)
         {
-            Vector3 displacement = base.Displacement;
-
-
-            if (!base.Finished())
+            if (fallAnimation == null || (Displacement.Length() / Vector3.Distance(startPosition, stopPosition) < 0.95))
             {
                 base.Update(elapsed);
                 ball.Rotate(Displacement);
-                return;
-            }
-            // check if ball fell and animte
-            /*if ((displacement.Length() / Vector3.Distance(initialPosition, stopPosition) < 0.95) || !ballFell)
-            {
-                base.Update(elapsed);
-                ball.Rotate(Displacement);
-                return;
             }
             else
+            {
                 fallAnimation.Update(elapsed);
-             */
+            }
         }
 
         public override bool Finished()
         {
-            if (ballWillFall)
+            if (fallAnimation == null)
             {
-                return fallAnimation.Finished();
+                return base.Finished();
             }
             else
             {
-                return base.Finished();
+                return fallAnimation.Finished();
             }
         }
 
