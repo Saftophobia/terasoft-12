1f7088b7c985c759ead3933218747817280d78eb
mohamedra2fat@gmail.com <mohamedra2fat@gmail.com@113cb9fd-de2b-231c-2074-2ec382a84f02>
2012-05-18 16:41:15 +0000

    Update issue 242 Status in progress Done with the angle calculation using new algorithm

--- trunk/ terasoft-12/Mechanect/Mechanect/Mechanect/Exp2/User2.cs
+++ trunk/ terasoft-12/Mechanect/Mechanect/Mechanect/Exp2/User2.cs
@@ -1,11 +1,14 @@
-﻿using System;
+﻿﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Microsoft.Kinect;
 using Microsoft.Xna.Framework;
+using Mechanect.Common;
+using Mechanect.Classes;
+using MKinect;
 
-namespace Mechanect.Exp2
+namespace Mechanect.Experiment2
 {
     /// <summary>
     /// This Class responsible for functionalities that User will do when testing either Velocity or Angle
@@ -13,7 +16,7 @@ namespace Mechanect.Exp2
     /// <para>Author: Mohamed Raafat</para>
     /// </remarks>
     /// </summary>
-    public class User2 : Mechanect.Common.User
+    class User2 : Mechanect.Common.User
     {
 
         /// <summary>
@@ -28,6 +31,20 @@ namespace Mechanect.Exp2
         private bool beforeHip;
         private double previousAngle;
         int counter = 0;
+        KinectSensor k;
+        private Skeleton[] skeletonFrames;
+        public Skeleton[] SkeletonFrames
+        {
+            get
+            {
+                return skeletonFrames;
+            }
+            set
+            {
+                skeletonFrames = value;
+
+            }
+        }
 
         private double measuredVelocity;
         private double measuredAngle;
@@ -72,6 +89,35 @@ namespace Mechanect.Exp2
             }
         }
 
+        public User2()
+        {
+            this.k.SkeletonFrameReady += new EventHandler<SkeletonFrameReadyEventArgs>(k_SkeletonFrameReady);
+            k.SkeletonStream.Enable();
+        }
+
+        void k_SkeletonFrameReady(object sender, SkeletonFrameReadyEventArgs e)
+        {
+            using (SkeletonFrame skeletonFrame = e.OpenSkeletonFrame())
+            {
+                if (skeletonFrame != null)
+                {
+                    //check to see if there is any data in the skeleton
+                    if (this.skeletonFrames == null)
+                    {
+                        //Allocate array of skeletons
+                        this.skeletonFrames = new Skeleton[skeletonFrame.SkeletonArrayLength];
+                    }
+
+                    //Copy skeletons from this frame
+                    skeletonFrame.CopySkeletonDataTo(this.skeletonFrames);
+
+                    //find first tracked skeleton, if any
+                    Skeleton skeleton = this.skeletonFrames.Where(s => s.TrackingState == SkeletonTrackingState.Tracked).FirstOrDefault();
+
+                }
+            }
+        }
+
         /// <summary>
         /// Getter and setter for the MeasuredAgnle Value
         /// </summary>
@@ -88,7 +134,7 @@ namespace Mechanect.Exp2
         /// <summary>
         /// Resets all instance variables to their intitial values
         /// </summary>
-     
+
         public void Reset()
         {
             shooting = false;
@@ -96,6 +142,8 @@ namespace Mechanect.Exp2
             measuredAngle = 0;
             previousAngle = 0;
         }
+
+
 
 
 
@@ -134,22 +182,45 @@ namespace Mechanect.Exp2
                     return;
                 counter = 0;
 
-                Vector2 hand = new Vector2(USER.Joints[JointType.HandLeft].Position.X - USER.Joints[JointType.ShoulderLeft].Position.X
-                    , USER.Joints[JointType.HandLeft].Position.Y - USER.Joints[JointType.ShoulderLeft].Position.Y);
+                Vector3 centerHipToLeftShoulder = new Vector3(USER.Joints[JointType.ShoulderLeft].Position.X - USER.Joints[JointType.HipCenter].Position.X,
+                    USER.Joints[JointType.ShoulderLeft].Position.Y - USER.Joints[JointType.HipCenter].Position.Y,
+                    USER.Joints[JointType.ShoulderLeft].Position.Z - USER.Joints[JointType.HipCenter].Position.Z);
 
-                double angle = (float)Math.Atan(hand.Y / hand.X);
+                Vector3 centerHiptoRightShoulder = new Vector3(USER.Joints[JointType.ShoulderRight].Position.X - USER.Joints[JointType.HipCenter].Position.X,
+                    USER.Joints[JointType.ShoulderRight].Position.Y - USER.Joints[JointType.HipCenter].Position.Y,
+                    USER.Joints[JointType.ShoulderRight].Position.Z - USER.Joints[JointType.HipCenter].Position.Z);
+
+                Vector3 leftShoulderToRightShoulder = new Vector3(USER.Joints[JointType.ShoulderLeft].Position.X - USER.Joints[JointType.ShoulderRight].Position.X,
+                    USER.Joints[JointType.ShoulderLeft].Position.Y - USER.Joints[JointType.ShoulderRight].Position.Y,
+                    USER.Joints[JointType.ShoulderLeft].Position.Z - USER.Joints[JointType.ShoulderRight].Position.Z);
+
+                Vector3 leftShoulderToLeftHand = new Vector3(USER.Joints[JointType.ShoulderLeft].Position.X - USER.Joints[JointType.ShoulderRight].Position.X,
+                    USER.Joints[JointType.ShoulderLeft].Position.Y - USER.Joints[JointType.ShoulderRight].Position.Y,
+                    USER.Joints[JointType.ShoulderLeft].Position.Z - USER.Joints[JointType.ShoulderRight].Position.Z);
+
+                Vector3 normalToShouldersHipPlane = Vector3.Cross(centerHipToLeftShoulder, centerHiptoRightShoulder);
+
+                Vector3 normalToShoulderHandPlane = Vector3.Cross(leftShoulderToLeftHand, leftShoulderToRightShoulder);
+
+                double angle = (double)Math.Acos(Vector3.Dot(normalToShoulderHandPlane, normalToShouldersHipPlane));
                 angle = angle * 180 / Math.PI;
-                angle += 90;
-                angle /= 2;
-
+                angle += 90; angle /= 2;
                 if (angle - previousAngle > 0.5)
                 {
                     previousAngle = angle;
                     return;
                 }
+
+
+
+
+
                 currentTime = (int)gametime.TotalGameTime.TotalMilliseconds - startTime;
+
                 measuredAngle = (int)(10 * angle) / 10f;
                 shooting = false;
+
+
             }
 
         }
@@ -174,8 +245,8 @@ namespace Mechanect.Exp2
             measuredVelocity = ((int)(500 * measuredAngle / currentTime)) / 10f;
         }
 
-       
-    
+
+
     }
 
 }
