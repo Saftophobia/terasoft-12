d341cce7c85616b9eaf43aa3732193654a08cc8c
ahmedhegazy835 <ahmedhegazy835@113cb9fd-de2b-231c-2074-2ec382a84f02>
2012-05-20 14:26:20 +0000

    update issue 8 Status: In Progress clearing the code after implementation review.

--- trunk/ terasoft-12/Mechanect/Mechanect/Mechanect/ButtonsAndSliders/Button.cs
+++ trunk/ terasoft-12/Mechanect/Mechanect/Mechanect/ButtonsAndSliders/Button.cs
@@ -1,13 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Audio;
-using Microsoft.Xna.Framework.Content;
-using Microsoft.Xna.Framework.GamerServices;
+﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
-using Microsoft.Xna.Framework.Input;
-using Microsoft.Xna.Framework.Media;
 using Microsoft.Kinect;
 using ButtonsAndSliders;
 using Mechanect.Common;
@@ -21,7 +13,6 @@ namespace ButtonsAndSliders
         private GifAnimation.GifAnimation texture, animation, stopped;
         private int screenW, ScreenH;
 
-        private Vector2 Position;
         private User user;
         private Texture2D hand;
         private Vector2 handPosition;
