3436aab95ed54cfb69093e3407248d2decde4328
3abdelazim@gmail.com <3abdelazim@gmail.com@113cb9fd-de2b-231c-2074-2ec382a84f02>
2012-05-20 13:36:32 +0000

    update issue 225 Status: In Progress Created Class "Simulation.cs" that will contain all my work in this story

---
+++  terasoft-12/Mechanect/Mechanect/Mechanect/Exp2/Simulation.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace Mechanect.Exp2
+{
+    class Simulation
+    {
+        public Simulation(Vector2 predatorPosition, Rectangle preyPosition, Rectangle aquariumPosition, float velocity, float angle)
+        {
+        }
+        void Draw(Rectangle position)
+        {
+        }
+        void Update(GameTime gameTime)
+        {
+        }
+    }
+}
