c6950ada89805df1703de8ef4f2cafda67f7bfe8
3abdelazim@gmail.com <3abdelazim@gmail.com@113cb9fd-de2b-231c-2074-2ec382a84f02>
2012-05-20 22:14:23 +0000

    update issue 225 Status: In Progress updated Class Simulation.cs added some instance variables, fields and updated the constructor

---  terasoft-12/Mechanect/Mechanect/Mechanect/Exp2/Simulation.cs
+++  terasoft-12/Mechanect/Mechanect/Mechanect/Exp2/Simulation.cs
@@ -8,13 +8,36 @@ namespace Mechanect.Exp2
 {
     class Simulation
     {
+        private Environment2 environment;
+        private float initialVelocity;
+        private float initialAngle;
+
+        private Vector2 InitialVelocity
+        {
+            get
+            {
+                return initialVelocity * new Vector2((float)Math.Cos(MathHelper.ToRadians(initialAngle)),
+                    (float)Math.Sin(MathHelper.ToRadians(initialAngle)));
+            }
+            set
+            {
+                initialVelocity = value.Length();
+                initialAngle = MathHelper.ToDegrees((float)Math.Tan(value.Y / value.X));
+            }
+        }
+
         public Simulation(Vector2 predatorPosition, Rectangle preyPosition, Rectangle aquariumPosition, float velocity, float angle)
         {
+            environment = new Environment2(predatorPosition, preyPosition, aquariumPosition);
+            initialVelocity = velocity;
+            initialAngle = angle;
         }
-        void Draw(Rectangle position)
+
+        void Update(GameTime gameTime)
         {
         }
-        void Update(GameTime gameTime)
+
+        void Draw(Rectangle position)
         {
         }
     }
