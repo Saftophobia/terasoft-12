35646d9241fd4d593abf4a3d06a435674880d3c0
ahmedhegazy835 <ahmedhegazy835@113cb9fd-de2b-231c-2074-2ec382a84f02>
2012-05-20 14:27:09 +0000

    update issue 6 Status: In Progress clearing the code after implementation review.

---  terasoft-12/Mechanect/Mechanect/Mechanect/ButtonsAndSliders/Slider.cs
+++  terasoft-12/Mechanect/Mechanect/Mechanect/ButtonsAndSliders/Slider.cs
@@ -1,16 +1,9 @@
-﻿using System;
-//using System.Collections.Generic;
-//using System.Linq;
-using Microsoft.Xna.Framework;
-//using Microsoft.Xna.Framework.Audio;
-using Microsoft.Xna.Framework.Content;
-//using Microsoft.Xna.Framework.GamerServices;
+﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
-using Mechanect.Common;
 using Microsoft.Kinect;
-//using Microsoft.Xna.Framework.Input;
-//using Microsoft.Xna.Framework.Media;
-
+using ButtonsAndSliders;
+using Mechanect.Common;
+using Microsoft.Xna.Framework.Content;
 
 namespace ButtonsAndSliders
 {
