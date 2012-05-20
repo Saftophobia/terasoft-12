5aa9e8af058180f45762f1cc32c751e32ded7a06
mohamedra2fat@gmail.com <mohamedra2fat@gmail.com@113cb9fd-de2b-231c-2074-2ec382a84f02>
2012-05-20 22:54:16 +0000

    Update Issue 242 Status: In-Progress Removed unused Fields in the class

---  terasoft-12/Mechanect/Mechanect/Mechanect/Exp2/User2.cs
+++  terasoft-12/Mechanect/Mechanect/Mechanect/Exp2/User2.cs
@@ -37,27 +37,7 @@ namespace Mechanect.Exp2
         private double measuredVelocity;
         private double measuredFinalAngle;
         private List<Vector2> angleAndTime = new List<Vector2>();
-
-        /// <summary>
-        /// Setter and Getter for Instance variable "angleBeingMeasured"
-        /// </summary>
-        /// <remarks>
-        /// <para>AUTHOR: Mohamed Raafat</para>
-        /// </remarks>
-        /// <returns>double, The value of the angleBeingMeasured</returns>
-
-        public double AngleBeingMeasured
-        {
-            get
-            {
-                return angleBeingMeasured;
-            }
-            set
-            {
-                angleBeingMeasured = value;
-            }
-        }
-
+      
         /// <summary>
         /// Setter and Getter for Instance variable "measuredFinalAngle"
         /// </summary>
@@ -98,165 +78,6 @@ namespace Mechanect.Exp2
             }
         }
 
-        /// <summary>
-        /// Setter and Getter for Instance variable "previousAngle"
-        /// </summary>
-        /// <remarks>
-        /// <para>AUTHOR: Mohamed Raafat</para>
-        /// </remarks>
-        /// <returns>double, The value of the previousAngle</returns>
-        /// 
-        public double PreviousAngle
-        {
-            get
-            {
-                return previousAngle;
-            }
-            set
-            {
-                previousAngle = value;
-            }
-        }
-
-        /// <summary>
-        /// Setter and Getter for Instance variable "shooting"
-        /// </summary>
-        /// <remarks>
-        /// <para>AUTHOR: Mohamed Raafat</para>
-        /// </remarks>
-        /// <returns>bool, The value of the shooting</returns>
-
-        public bool Shooting
-        {
-            get
-            {
-                return shooting;
-            }
-            set
-            {
-                shooting = value;
-            }
-
-        }
-
-        /// <summary>
-        /// Setter and Getter for Instance variable "beforeHip"
-        /// </summary>
-        /// <remarks>
-        /// <para>AUTHOR: Mohamed Raafat</para>
-        /// </remarks>
-        /// <returns>bool, The value of the beforeHip</returns>
-
-        public bool BeforeHip
-        {
-            get
-            {
-                return beforeHip;
-            }
-            set
-            {
-                beforeHip = value;
-            }
-        }
-
-        /// <summary>
-        /// Setter and Getter for Instance variable "currentTime"
-        /// </summary>
-        /// <remarks>
-        /// <para>AUTHOR: Mohamed Raafat</para>
-        /// </remarks>
-        /// <returns>int, The value of the currentTime</returns>
-
-        public int CurrentTime
-        {
-            get
-            {
-                return currentTime;
-            }
-            set
-            {
-                currentTime = value;
-            }
-        }
-
-        /// <summary>
-        /// Setter and Getter for Instance variable "startTime"
-        /// </summary>
-        /// <remarks>
-        /// <para>AUTHOR: Mohamed Raafat</para>
-        /// </remarks>
-        /// <returns>int, The value of the startTime</returns>
-
-        public int StartTime
-        {
-            get
-            {
-                return startTime;
-            }
-            set
-            {
-                startTime = value;
-            }
-        }
-
-        /// <summary>
-        /// Setter and Getter for Instance variable "counter"
-        /// </summary>
-        /// <remarks>
-        /// <para>AUTHOR: Mohamed Raafat</para>
-        /// </remarks>
-        /// <returns>int, The value of the counter</returns>
-
-        public int Counter
-        {
-            get
-            {
-                return counter;
-            }
-            set
-            {
-                counter = value;
-            }
-        }
-
-        /// <summary>
-        /// Setter and Getter for Instance variable "listCounter"
-        /// </summary>
-        /// <remarks>
-        /// <para>AUTHOR: Mohamed Raafat</para>
-        /// </remarks>
-        /// <returns> int,The value of the listCounter</returns>
-
-        public int ListCounter
-        {
-            get
-            {
-                return listCounter;
-            }
-            set
-            {
-                listCounter = value;
-            }
-        }
-        /// <summary>
-        /// Setter and Getter for Instance variable "angleAndTime"
-        /// </summary>
-        /// <remarks>
-        /// <para>AUTHOR: Mohamed Raafat</para>
-        /// </remarks>
-        /// <returns>List'Vector2',The value of the angleAndTime</returns>
-
-        public List<Vector2> AngleTime
-        {
-            get
-            {
-                return angleAndTime;
-            }
-            set
-            {
-                angleAndTime = value;
-            }
-        }
 
         /// <summary>
         /// Constructor for User2 class, that sets the value of the counter and listCounter
@@ -270,20 +91,7 @@ namespace Mechanect.Exp2
         }
 
 
-        /* 
-         /// <summary>
-         /// Getter and setter for the MeasuredAgnle Value
-         /// </summary>
-         /// <remarks>
-         /// <para>Author: Mohamed Raafat</para>
-         /// </remarks>
-
-         public void MeasureVelocityAndAngle(GameTime gameTime)
-         {
-             MeasureAngle(gameTime);
-             MeasureVelocity();
-         }
- */
+      
         /// <summary>
         /// Resets all instance variables to their intitial values
         /// </summary>
@@ -296,11 +104,6 @@ namespace Mechanect.Exp2
             measuredFinalAngle = 0;
             previousAngle = 0;
         }
-
-
-
-
-
 
         /// <summary>
         /// Calculate the angle between two planes, one is a vertical plane containing: left shoulder, right shoulder,
