f7b987df7c263df41b7bebc82e6f2352319799e0
3abdelazim@gmail.com <3abdelazim@gmail.com@113cb9fd-de2b-231c-2074-2ec382a84f02>
2012-05-20 23:56:45 +0000

    update issue 225 Status: In Progress updated Class Simulation.cs implemented Update, Draw, LoadContent Methods

--- trunk/ terasoft-12/Mechanect/Mechanect/Mechanect/Exp2/Simulation.cs
+++ trunk/ terasoft-12/Mechanect/Mechanect/Mechanect/Exp2/Simulation.cs
@@ -3,42 +3,67 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
 
 namespace Mechanect.Exp2
 {
     class Simulation
     {
         private Environment2 environment;
-        private float initialVelocity;
-        private float initialAngle;
-
-        private Vector2 InitialVelocity
-        {
-            get
-            {
-                return initialVelocity * new Vector2((float)Math.Cos(MathHelper.ToRadians(initialAngle)),
-                    (float)Math.Sin(MathHelper.ToRadians(initialAngle)));
-            }
-            set
-            {
-                initialVelocity = value.Length();
-                initialAngle = MathHelper.ToDegrees((float)Math.Tan(value.Y / value.X));
-            }
-        }
+        private Vector2 initialPredatorVelocity;
+        private Vector2 initialPredatorPosition;
+        private bool predatorInitialized;
+        private bool simulationRunning;
+        private float milliseconds;
+        private SpriteFont font;
 
         public Simulation(Vector2 predatorPosition, Rectangle preyPosition, Rectangle aquariumPosition, float velocity, float angle)
         {
             environment = new Environment2(predatorPosition, preyPosition, aquariumPosition);
-            initialVelocity = velocity;
-            initialAngle = angle;
+            initialPredatorVelocity = velocity * 
+                new Vector2((float)Math.Cos(MathHelper.ToRadians(angle)), 
+                    (float)Math.Sin(MathHelper.ToRadians(angle)));
+            initialPredatorPosition = predatorPosition;
+            simulationRunning = false;
+            predatorInitialized = false;
         }
 
-        void Update(GameTime gameTime)
+        public void LoadContent(ContentManager contentManager, Viewport viewport)
         {
+            font = contentManager.Load<SpriteFont>("Ariel");
+            //environment.LoadContent(contentManager, viewport);
         }
 
-        void Draw(Rectangle position)
+        public void Update(GameTime gameTime)
         {
+            if (!simulationRunning)
+            {
+                milliseconds += gameTime.ElapsedGameTime.Milliseconds;
+                if (milliseconds > 2000)
+                {
+                    milliseconds = 0;
+                    simulationRunning = true;
+                }
+                else if (milliseconds > 1000)
+                {
+                    environment.Predator.Location = initialPredatorPosition;
+                    environment.Predator.Velocity = initialPredatorVelocity;
+                    predatorInitialized = true;
+                }
+            }
+            else
+            {
+                //environment.Update(gameTime);
+            }
+        }
+
+        public void Draw(Rectangle rectangle, ContentManager contentManager, SpriteBatch spriteBatch, Viewport viewport)
+        {
+            //environment.Draw(new Rectangle(rectangle.X, rectangle.Y + 50, rectangle.Width, rectangle.Height), contentManager, spriteBatch, viewport);
+            string data = "Velocity = " + Math.Round(initialPredatorVelocity.Length(), 2) + ", Angle = " 
+                + Math.Round(MathHelper.ToDegrees((float)Math.Tan(initialPredatorVelocity.Y / initialPredatorVelocity.X)), 2);
+            spriteBatch.DrawString(font, data, new Vector2(rectangle.X, rectangle.Y), Color.Black);
         }
     }
 }
