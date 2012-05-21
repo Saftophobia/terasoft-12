b96faaa991c4cbf19053739c1a649f9e535a787a
vendettax6@gmail.com <vendettax6@gmail.com@113cb9fd-de2b-231c-2074-2ec382a84f02>
2012-05-21 16:26:49 +0000

    update issue 27 Status: In-Progress Changed Entire Animation. Works now :)

---  terasoft-12/Mechanect/Mechanect/Mechanect/Exp3/BallAnimation.cs
+++  terasoft-12/Mechanect/Mechanect/Mechanect/Exp3/BallAnimation.cs
@@ -12,9 +12,8 @@ namespace Mechanect.Exp3
     {
 
         private Ball ball;
-        private ModelFramedAnimation fallAnimation;
-        private Vector3 totalDisplacement;
-        private float holeRadius;
+        private bool willFall;
+        private int fallFactor;
 
         public BallAnimation(Ball ball, Vector3 velocity, float friction, Vector3 holePosition, float holeRadius)
             : base(ball, velocity, friction, LinearMotion.CalculateTime(velocity.Length(), 0, friction))
@@ -22,49 +21,34 @@ namespace Mechanect.Exp3
             this.ball = ball;
             Vector3 totalDisplacement = LinearMotion.CalculateDisplacement(velocity, friction, Duration);
             Vector3 stopPosition = StartPosition + totalDisplacement;
-            this.holeRadius = holeRadius;
 
-            //if (Vector3.Distance(stopPosition, holePosition) < (ball.Radius + holeRadius))
-            //{
-                //fallAnimation = new ModelFramedAnimation(ball);
-                /*if (stopPosition.X < holePosition.X)
-                    fallAnimation.AddFrame(new Vector3(holePosition.X + holeRadius, holePosition.Y, holePosition.Z), Vector3.Zero, TimeSpan.FromSeconds(0.5));
-                else
-                {
-                    if (stopPosition.X > holePosition.X)
-                        fallAnimation.AddFrame(new Vector3(holePosition.X - holeRadius, holePosition.Y, holePosition.Z), Vector3.Zero, TimeSpan.FromSeconds(0.5));
-                    else
-                        fallAnimation.AddFrame(new Vector3(holePosition.X, holePosition.Y, holePosition.Z + 10), Vector3.Zero, TimeSpan.FromSeconds(0.5));
-                }*/
-                //fallAnimation.AddFrame((totalDisplacement.Length() - holeRadius) * Vector3.Normalize(totalDisplacement), Vector3.Zero, TimeSpan.FromSeconds(0));
-                //fallAnimation.AddFrame(holePosition, Vector3.Zero, TimeSpan.FromSeconds(2));
-                //fallAnimation.AddFrame(new Vector3(holePosition.X, holePosition.Y - 20, holePosition.Z), Vector3.Zero, TimeSpan.FromSeconds(2));
-            //}
+            if (Vector3.Distance(stopPosition, holePosition) < (ball.Radius + holeRadius))
+            {
+                willFall = true;
+            }
 
         }
-
         public override void Update(TimeSpan elapsed)
         {
-            if (fallAnimation == null || Displacement.Length() < totalDisplacement.Length())
+            base.Update(elapsed);
+            ball.Rotate(Displacement);
+            if (!base.Finished())
             {
-                base.Update(elapsed);
-                ball.Rotate(Displacement);
-            }
-            else
-            {
-                fallAnimation.Update(elapsed);
-            }
-        }
-
-        public override bool Finished()
-        {
-            if (fallAnimation == null)
-            {
-                return base.Finished();
-            }
-            else
-            {
-                return fallAnimation.Finished();
+                if (willFall)
+                {
+                    if (ElapsedTime > Duration - TimeSpan.FromSeconds(3))
+                    {
+                        if (fallFactor < 120)
+                        {
+                            ball.SetHeight(ball.Position.Y - ball.Radius - (fallFactor * 0.15f));
+                            fallFactor++;
+                        }
+                        else
+                        {
+                            ball.SetHeight(ball.Position.Y - ball.Radius - (fallFactor * 0.15f));
+                        }
+                    }
+                }
             }
         }
 
