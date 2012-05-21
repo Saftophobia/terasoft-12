4a96e56693b32218ada43939cccecda546179d7e
khaled.osmaan@gmail.com <khaled.osmaan@gmail.com@113cb9fd-de2b-231c-2074-2ec382a84f02>
2012-05-21 19:54:03 +0000

    Update issue 237 Changed drawing the texture color pixel by pixel to drawing a texture2D for each color such that it only colors transparent part of the avatar and it looks way better... changed some class design issues and tested it, updated the class such that it works perfectly fine with 1 or two users, tracks and colors each of them separately\

---  terasoft-12/Mechanect/Mechanect/Mechanect/Screens/UserAvatarScreen.cs
+++  terasoft-12/Mechanect/Mechanect/Mechanect/Screens/UserAvatarScreen.cs
@@ -4,7 +4,6 @@ using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
 using Mechanect.Common;
 using Microsoft.Kinect;
-using Mechanect.Classes;
 
 namespace Mechanect.Screens
 {
@@ -19,42 +18,25 @@ namespace Mechanect.Screens
         Vector2[] avatarPosition;
         SpriteFont font;
         User[] users;
+        MKinect kinect;
         String[] command;
-        int minDepth;
-        int maxDepth;
+        const int minDepth = 120;
+        const int maxDepth = 350;
         int[] depth;
-        public UserAvatarScreen(User user, int minDepth, int maxDepth)
+        public UserAvatarScreen(User user, MKinect kinect)
         {
             this.users = new User[1];
-            this.command = new String[1];
-            avatar = new Texture2D[1];
-            avatarPosition = new Vector2[1];
+            this.kinect = kinect;
             this.users[0] = user;
-            this.minDepth = minDepth;
-            this.maxDepth = maxDepth;
-            this.depth= new int[1];
+            Initialize();
         }
-        public UserAvatarScreen(User user, User user2, int minDepth, int maxDepth)
+        public UserAvatarScreen(User user, User user2, MKinect kinect)
         {
             this.users = new User[2];
-            this.command = new String[2];
-            avatar = new Texture2D[2];
-            avatarPosition = new Vector2[2];
             this.users[0] = user;
             this.users[1] = user2;
-            this.minDepth = minDepth;
-            this.maxDepth = maxDepth;
-            this.depth = new int[2];
-        }
-        public UserAvatarScreen(User[] users, int minDepth, int maxDepth)
-        {
-            this.users = users;
-            this.command = new String[users.Length];
-            avatar = new Texture2D[users.Length];
-            avatarPosition = new Vector2[users.Length];
-            this.depth = new int[users.Length];
-            this.minDepth = minDepth;
-            this.maxDepth = maxDepth;
+            this.kinect = kinect;
+            Initialize();
         }
         /// <summary>
         /// LoadContent will be called only once before drawing and its the place to load
@@ -73,9 +55,17 @@ namespace Mechanect.Screens
             font = content.Load<SpriteFont>("spriteFont1");
             for (int i = 0; i < avatar.Length; i++)
             {
-                avatar[i] = content.Load<Texture2D>(@"Textures/user");
-                avatarPosition[i] = new Vector2(screenWidth / (2*(i+1)), screenHeight / (4*(i+1)));
+                avatar[i] = content.Load<Texture2D>(@"Textures/avatar-white");
+                avatarPosition[i] = new Vector2(screenWidth / (i + 1), screenHeight);
             }
+        }
+
+        public void Initialize()
+        {
+            this.depth = new int[users.Length];
+            this.command = new String[users.Length];
+            avatar = new Texture2D[users.Length];
+            avatarPosition = new Vector2[users.Length];
         }
 
         /// <summary>
@@ -92,16 +82,20 @@ namespace Mechanect.Screens
             depth[ID] = GenerateDepth(ID);
             if (depth[ID] == 0)
             {
-                command[ID] = "No player detected"+depth[ID];
-                return;
+                avatar[ID] = content.Load<Texture2D>(@"Textures/avatar-dead");
+                command[ID] = "No player detected" + depth[ID];
             }
             else
             {
-                command[ID] = ""+depth[ID];
-                return;
+                if (depth[ID] < minDepth)
+                    avatar[ID] = content.Load<Texture2D>(@"Textures/avatar-red");
+                else if (depth[ID] > maxDepth)
+                    avatar[ID] = content.Load<Texture2D>(@"Textures/avatar-white");
+                else if (depth[ID] < maxDepth)
+                    avatar[ID] = content.Load<Texture2D>(@"Textures/avatar-green");
             }
+            command[ID] = "You're standing at" + depth[ID];
         }
-
         /// <summary>
         /// Takes a 2D texture as a parameter and colors it according to user's distance from the kinect device.
         /// </summary>
@@ -113,25 +107,31 @@ namespace Mechanect.Screens
         /// </param>
         public void UpdateAvatar(Texture2D texture, User user)
         {
-            int userindex=0;
+            int userindex = getUserindex(user);
+            if (user.USER != null)
+            {
+                if (depth[userindex] < maxDepth / 4)
+                    ChangeTextureColor(texture, "Yellow");
+                else if (depth[userindex] < maxDepth / 2)
+                    ChangeTextureColor(texture, "Green");
+                else if (depth[userindex] < maxDepth)
+                    ChangeTextureColor(texture, "Blue");
+                else if (depth[userindex] > maxDepth)
+                    ChangeTextureColor(texture, "Red");
+            }
+            else ChangeTextureColor(texture, "Green");
+        }
+
+        public int getUserindex(User user)
+        {
+            int userindex = 0;
             for (int i = 0; i < users.Length; i++)
             {
                 if (users[i].Equals(user))
                     userindex = i;
             }
-            if (user.USER != null)
-                {
-                if (depth[userindex] < maxDepth / 4)
-                    ChangeTextureColor(texture, "Yellow");
-                else if (depth[userindex] < maxDepth / 2)
-                    ChangeTextureColor(texture,"Green");
-                else if (depth[userindex] < maxDepth)
-                    ChangeTextureColor(texture, "Blue");
-                else if (depth[userindex] > maxDepth)
-                    ChangeTextureColor(texture, "Red");
-                }
-            else ChangeTextureColor(texture, "Green");
-            }
+            return userindex;
+        }
 
         /// <summary>
         /// Takes the user's index in the array and gets his distance from the kinect device.
@@ -168,13 +168,23 @@ namespace Mechanect.Screens
         /// <param name="covered">Determines whether you want this screen to be covered by another screen or not.</param>
         public override void Update(GameTime gameTime, bool covered)
         {
+
+            if (users.Length == 1)
+            {
+                users[0].USER = kinect.requestSkeleton();
+
+            }
+            else if (users.Length == 2)
+            {
+                users[0].USER = kinect.requestSkeleton();
+                users[1].USER = kinect.request2ndSkeleton();
+            }
             for (int i = 0; i < users.Length; i++)
             {
                 UpdateUser(i);
-                UpdateAvatar(avatar[i], users[i]);
+                // UpdateAvatar(avatar[i], users[i]);
             }
         }
-
         /// <summary>
         /// This is called when the game screen should draw itself.
         /// </summary>
@@ -183,18 +193,20 @@ namespace Mechanect.Screens
         ///</remarks>
         /// <param name="gameTime">Provides a snapshot of timing values.</param>    
         public override void Draw(GameTime gameTime)
-        {          
-                graphics.Clear(Color.Transparent);
-                spriteBatch.Begin();
-                 for(int i=0; i<avatar.Length; i++)
-                spriteBatch.Draw(avatar[i], avatarPosition[i], null, Color.White, 0, 
-                    new Vector2(avatar[i].Width / 4, avatar[i].Height / 4), 1f, SpriteEffects.None, 0);
-                for (int i = 0; i < users.Length; i++)
-                {
-                    ScreenManager.SpriteBatch.DrawString(font, "Player " + i + " : " + command[i],
-                        new Vector2(100, 320 + 100 * i), Color.OrangeRed);
-                }
-                spriteBatch.End();
+        {
+            graphics.Clear(Color.Transparent);
+            spriteBatch.Begin();
+            for (int i = 0; i < avatar.Length; i++)
+            {
+                spriteBatch.Draw(avatar[i], avatarPosition[i], null, Color.White, 0,
+                    new Vector2(avatar[i].Width, avatar[i].Height), 1f, SpriteEffects.None, 0);
+            }
+            for (int i = 0; i < users.Length; i++)
+            {
+                ScreenManager.SpriteBatch.DrawString(font, "Player " + i + " : " + command[i],
+                    new Vector2(100, 320 + 100 * i), Color.OrangeRed);
+            }
+            spriteBatch.End();
         }
 
         /// <summary>
@@ -222,7 +234,7 @@ namespace Mechanect.Screens
                     for (int i = 0; i < data.Length; i++)
                     {
                         data[i].G = 255;
-                        data[i].B = 255;
+                        data[i].R = 255;
                     } break;
                 case "Green":
                     for (int i = 0; i < data.Length; i++)
@@ -233,7 +245,7 @@ namespace Mechanect.Screens
                 case "Transparent":
                     for (int i = 0; i < data.Length; i++)
                         data[i].A = 0; break;
-                default:break;
+                default: break;
             }
             texture.SetData(data);
         }
@@ -250,3 +262,4 @@ namespace Mechanect.Screens
         }
     }
 }
+s
