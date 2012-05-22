305650c92585643ed6a426575bb116250633b3dd
bishoybassem@gmail.com <bishoybassem@gmail.com@113cb9fd-de2b-231c-2074-2ec382a84f02>
2012-05-22 22:08:54 +0000

    update issue 40
    Status: In Progress
    
    added motion tests

---
+++  terasoft-12/Mechanect/TestsLib/Bisho/LinearMotionTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using NUnit.Framework;
+using Physics;
+using Microsoft.Xna.Framework;
+
+namespace Tests
+{
+    [TestFixture]
+    public class LinearMotionTests
+    {
+
+        [Test]
+        public void TestVectorDirection()
+        {
+            Assert.AreEqual(LinearMotion.GetVectorInDirectionOf(2, new Vector3(3, 0, 0)), new Vector3(2, 0, 0));
+        }
+
+        [Test]
+        public void TestTime()
+        {
+            Assert.AreEqual(LinearMotion.CalculateTime(0, 10, 1), TimeSpan.FromSeconds(10));
+            Assert.AreEqual(LinearMotion.CalculateTime(10, 0, -1), TimeSpan.FromSeconds(10));
+        }
+
+        [Test]
+        public void TestDisplacement()
+        {
+            Assert.AreEqual(LinearMotion.CalculateDisplacement(new Vector3(10, 0, 0), -1, TimeSpan.FromSeconds(3)), new Vector3(25.5f, 0, 0));
+        }
+
+        [Test]
+        public void TestVelocity()
+        {
+            Assert.AreEqual(LinearMotion.CalculateIntialVelocity(new Vector3(6, 0, 0), 4, 1), new Vector3(2f, 0, 0));
+        }
+    }
+}
