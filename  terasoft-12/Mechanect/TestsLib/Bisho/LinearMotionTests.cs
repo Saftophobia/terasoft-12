043776744b79719eaa7a1fbe0a40fbdc498ed108
bishoybassem@gmail.com <bishoybassem@gmail.com@113cb9fd-de2b-231c-2074-2ec382a84f02>
2012-05-22 23:05:13 +0000

    update issue 40
    Status: In Progress
    
    added more test cases

---  terasoft-12/Mechanect/TestsLib/Bisho/LinearMotionTests.cs
+++  terasoft-12/Mechanect/TestsLib/Bisho/LinearMotionTests.cs
@@ -13,6 +13,7 @@ namespace Tests
         public void TestVectorDirection()
         {
             Assert.AreEqual(LinearMotion.GetVectorInDirectionOf(2, new Vector3(3, 0, 0)), new Vector3(2, 0, 0));
+            Assert.AreEqual(LinearMotion.GetVectorInDirectionOf(-1, new Vector3(0, 4, 0)), new Vector3(0, -1, 0));
         }
 
         [Test]
@@ -26,12 +27,14 @@ namespace Tests
         public void TestDisplacement()
         {
             Assert.AreEqual(LinearMotion.CalculateDisplacement(new Vector3(10, 0, 0), -1, TimeSpan.FromSeconds(3)), new Vector3(25.5f, 0, 0));
+            Assert.AreEqual(LinearMotion.CalculateDisplacement(new Vector3(0, 10, 0), 1, TimeSpan.FromSeconds(3)), new Vector3(0, 34.5f, 0));
         }
 
         [Test]
         public void TestVelocity()
         {
-            Assert.AreEqual(LinearMotion.CalculateIntialVelocity(new Vector3(6, 0, 0), 4, 1), new Vector3(2f, 0, 0));
+            Assert.AreEqual(LinearMotion.CalculateIntialVelocity(new Vector3(6, 0, 0), 4, 1), new Vector3(2, 0, 0));
+            Assert.AreEqual(LinearMotion.CalculateIntialVelocity(new Vector3(0, 0, 5.5f), 5, -1), new Vector3(0, 0, 6));
         }
     }
 }
