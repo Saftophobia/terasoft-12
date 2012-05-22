54b0a3a5cc742a383153373cbd7951cb0f9cf854
mohamed.alzayat.eng@gmail.com <mohamed.alzayat.eng@gmail.com@113cb9fd-de2b-231c-2074-2ec382a84f02>
2012-05-22 21:01:25 +0000

    update issue 3
    Status: In-Progress
    
    A test for the Method PositionMapper.

---
+++  terasoft-12/Mechanect/TestsLib/Zayat/Environment2Test.cs
@@ -0,0 +1,103 @@
+﻿using Mechanect.Exp2;
+using NUnit.Framework;
+using System;
+using Microsoft.Xna.Framework;
+using System.Windows;
+using Microsoft.Xna.Framework.Graphics;
+
+
+
+namespace TestProjec
+{
+
+
+    /// <summary>
+    ///This is a test class for Environment2Test and is intended
+    ///to contain all Environment2Test Unit Tests
+    ///</summary>
+
+    public class Environment2Test
+    {
+
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        // 
+        //You can use the following additional attributes as you write your tests:
+        //
+        //Use ClassInitialize to run code before running the first test in the class
+        //[ClassInitialize()]
+        //public static void MyClassInitialize(TestContext testContext)
+        //{
+        //}
+        //
+        //Use ClassCleanup to run code after all tests in a class have run
+        //[ClassCleanup()]
+        //public static void MyClassCleanup()
+        //{
+        //}
+        //
+        //Use TestInitialize to run code before running each test
+        //[TestInitialize()]
+        //public void MyTestInitialize()
+        //{
+        //}
+        //
+        //Use TestCleanup to run code after each test has run
+        //[TestCleanup()]
+        //public void MyTestCleanup()
+        //{
+        //}
+        //
+        #endregion
+
+
+        /// <summary>
+        ///A test for PositionMapper
+        ///</summary>
+        ///<remarks>
+        /// <para>AUTHOR: Mohamed Alzayat </para>   
+        /// <para>DATE WRITTEN: May, 20 </para>
+        /// <para>DATE MODIFIED: May, 22 </para>
+        /// </remarks>
+        [Test]
+        public void PositionMapperTest()
+        {
+
+            Environment2 target = new Environment2(Vector2.Zero, new Rect(5, 5, 1, 1), new Rect(10, 3, 2, 2)); 
+            target.Draw(new Rectangle(10, 10, 100, 100),
+                new Microsoft.Xna.Framework.Graphics.SpriteBatch(new GraphicsDevice(GraphicsAdapter.DefaultAdapter,
+                new GraphicsProfile(), new PresentationParameters())));
+            Vector2[] unMapped = { new Vector2(-1, -1), new Vector2(0, 0), new Vector2(1, 1), new Vector2(-1, 1), 
+                                     new Vector2(10, 10) };
+            Vector2[] expected = { new Vector2(90.9f, 97.1f), new Vector2(84, 104), new Vector2(90.9f, 97.1f),
+                                     new Vector2(90.9f, 97.1f), new Vector2(153, 35) };
+            
+            Vector2 actual;
+            for (int i = 0; i < unMapped.Length; i++)
+            {
+
+                actual = target.PositionMapper(unMapped[i]);
+                Assert.AreEqual(expected[i], actual);
+            }
+            Assert.Inconclusive("Verify the correctness of this test method.");
+        }
+    }
+}
