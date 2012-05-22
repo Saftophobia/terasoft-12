3679b6408be4860d2ff296eb03cba98e0a4a0614
mohamedra2fat@gmail.com <mohamedra2fat@gmail.com@113cb9fd-de2b-231c-2074-2ec382a84f02>
2012-05-22 06:36:46 +0000

    Update Issue 236 Status In-Progress I wanted to generalize the Method of Wrap text, So it still exist in the Instruction class, just not to make compile errors, and I added a duplicate of it in the UI class, following the advice of 3abdelazim

--- trunk/ terasoft-12/Mechanect/UI/UILib.cs
+++ trunk/ terasoft-12/Mechanect/UI/UILib.cs
@@ -3,12 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Speech.Synthesis;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
 
 namespace UI
 {
     public static class UILib
     {
         private static SpeechSynthesizer speechSynthesizer;
+        private static SpriteFont spriteFont;
         /// <summary>
         /// This method takes a string of text and reads it to the user.
         /// </summary>
@@ -36,6 +40,35 @@ namespace UI
                 return false;
             }
 
+        }
+        public static void Write(string text, Rectangle position, SpriteBatch spriteBatch, ContentManager contentManager)
+        {
+            if (spriteFont == null)
+            {
+                spriteFont = contentManager.Load<SpriteFont>("Ariel");
+            }
+            spriteBatch.DrawString(spriteFont, WrapText(text, position), new Vector2(position.X, position.Y), Color.Black);
+
+        }
+        private static string WrapText(string text, Rectangle position)
+        {
+            string line = string.Empty;
+            string returnString = string.Empty;
+            string[] wordArray = text.Split(' ');
+
+            foreach (string word in wordArray)
+            {
+                if (spriteFont.MeasureString(line + word).Length() > position.Width)
+                {
+                    returnString += line + '\n';
+                    line = string.Empty;
+                }
+
+                line += word + ' ';
+            }
+
+            returnString += line;
+            return returnString;
         }
 
         
