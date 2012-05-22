a05c354d5e1d98f7a6846264076743c2faa92ccb
khaled.osmaan@gmail.com <khaled.osmaan@gmail.com@113cb9fd-de2b-231c-2074-2ec382a84f02>
2012-05-22 21:50:08 +0000

    Update issue 35 added Nunit test for hole position generating

---
+++  terasoft-12/Mechanect/TestsLib/5o5a/HoleTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using NUnit.Framework;
+using Mechanect;
+using Mechanect.Exp3;
+using Mechanect.Common;
+
+namespace Tests
+{
+    [TestFixture]
+    public class HoleTests
+    {
+        Game1 game1;
+        Hole hole;
+        ScreenManager screenManager;
+        Environment3 environment3;
+        [SetUp]
+        public void Init()
+        {
+            game1 = new Game1();
+            game1.Content.RootDirectory = @"F:\SE\ terasoft-12\Mechanect\TestsLib\bin\Debug\Content";
+            game1.Run();
+            environment3 = Constants3.environment3;
+            screenManager = new ScreenManager(game1);
+        }
+
+        [Test]
+        public void Test()
+        {
+            hole = new Hole(screenManager.Game.Content, screenManager.GraphicsDevice, environment3.terrainWidth, environment3.terrainHeight, 10, environment3.user.shootingPosition);
+            Assert.LessOrEqual(environment3.HoleProperty.Position.Z, (environment3.user.shootingPosition.Z - hole.Radius));
+            Assert.GreaterOrEqual(environment3.HoleProperty.Position.Z, -(environment3.terrainHeight - hole.Radius) / 2);
+            Assert.LessOrEqual(environment3.HoleProperty.Position.X, environment3.terrainWidth / 4);
+            Assert.GreaterOrEqual(environment3.HoleProperty.Position.X, -environment3.terrainWidth / 4);
+            Assert.AreEqual(environment3.HoleProperty.Position.Y, 0);
+        }
+    }
+}
