abc661e00aa912c14ffdb8f758ba80b4154912d2
mohamedra2fat@gmail.com <mohamedra2fat@gmail.com@113cb9fd-de2b-231c-2074-2ec382a84f02>
2012-05-23 07:05:55 +0000

    Update Issue 242 Status In Progress Initialized the Button Positions. Loaded the Simulation and Button Positions Done withe Instance Variables and Constructor

---  terasoft-12/Mechanect/Mechanect/Mechanect/Screens/StatisticScreen2.cs
+++  terasoft-12/Mechanect/Mechanect/Mechanect/Screens/StatisticScreen2.cs
@@ -1,72 +1,104 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
+using Mechanect.Common;
+using ButtonsAndSliders;
 using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Audio;
-using Microsoft.Xna.Framework.Content;
-using Microsoft.Xna.Framework.GamerServices;
 using Microsoft.Xna.Framework.Graphics;
-using Microsoft.Xna.Framework.Input;
-using Microsoft.Xna.Framework.Media;
-using Mechanect.Exp2;
+using Microsoft.Xna.Framework.Content;
+using Mechanect.Exp3;
+using System.Windows;
 
-
-namespace Mechanect.Screens
+namespace Mechanect.Exp2
 {
-
-    
-    /// <summary>
-    /// This is a game component that implements IUpdateable.
-    /// </summary>
-    public class StatisticScreen2 : Mechanect.Common.GameScreen
+    class StatisticsScreen : GameScreen
     {
+        User2 user;
+        private Simulation userSimulation;
+        private Simulation optimalSimulation;
+        Vector2 mainMenuButtonPosition;
+        Vector2 retryButtonPosition;
+        Vector2 newGameButtonPosition;
+        Vector2 seeResultsButtonPosition;
+        private bool correctAnswer;
+        private Button mainMenu;
+        private Button retry;
+        private Button solution;
+        private Button newGame;
+        private Texture2D hand;
 
-        Simulation s;
-        public StatisticScreen2()
+
+
+        public StatisticsScreen(Vector2 predatorPosition, Rect preyPosition, Rect aquariumPosition, float userVelocity,
+            float userAngle, User2 user)
         {
-            // TODO: Construct any child components here
+            this.userSimulation = new Simulation(predatorPosition, preyPosition, aquariumPosition, userVelocity, userAngle);
+            this.user = user;
+            correctAnswer = true;
         }
 
-        /// <summary>
-        /// Allows the game component to perform any initialization it needs to before starting
-        /// to run.  This is where it can query for any required services and load content.
-        /// </summary>
+        public StatisticsScreen(Vector2 predatorPosition, Rect preyPosition, Rect aquariumPosition, float userVelocity,
+            float userAngle, float optimalVelocity, float optimalAngle, User2 user)
+        {
+            this.userSimulation = new Simulation(predatorPosition, preyPosition, aquariumPosition, userVelocity, userAngle);
+            this.optimalSimulation = new Simulation(predatorPosition, preyPosition, aquariumPosition, optimalVelocity, optimalAngle);
+            this.user = user;
+            correctAnswer = false;
+        }
+
         public override void Initialize()
         {
-            // TODO: Add your initialization code here
-           
-        }
+            mainMenuButtonPosition = new Vector2((float)0.1665 * ScreenManager.GraphicsDevice.Viewport.Width,
+               (float)0.8325 * ScreenManager.GraphicsDevice.Viewport.Height);
 
+            retryButtonPosition = new Vector2((float)0.4995 * ScreenManager.GraphicsDevice.Viewport.Width,
+               (float)0.8325 * ScreenManager.GraphicsDevice.Viewport.Height);
+
+            newGameButtonPosition = new Vector2((float)0.8327 * ScreenManager.GraphicsDevice.Viewport.Width,
+               (float)0.8325 * ScreenManager.GraphicsDevice.Viewport.Height);
+
+            seeResultsButtonPosition = new Vector2((float)0.8327 * ScreenManager.GraphicsDevice.Viewport.Width,
+               (float)0.8325 * ScreenManager.GraphicsDevice.Viewport.Height);
+        }
+        //Still waiting for Hegazy to do the buttons for me.
         public override void LoadContent()
         {
-            //s = new Simulation(Vector2.Zero, new Rectangle(5, 5, 1, 1), new Rectangle(10, 3, 2, 2), 12, 60);
-            //s.LoadContent(ScreenManager.Game.Content, ScreenManager.GraphicsDevice.Viewport, ScreenManager.GraphicsDevice);
+            userSimulation.LoadContent(ScreenManager.Game.Content, ScreenManager.GraphicsDevice.Viewport, ScreenManager.GraphicsDevice);
+            if (!correctAnswer)
+                optimalSimulation.LoadContent(ScreenManager.Game.Content, ScreenManager.GraphicsDevice.Viewport, ScreenManager.GraphicsDevice);
 
-            
+            mainMenu = Tools3.MainMenuButton(ScreenManager.Game.Content, mainMenuButtonPosition, ScreenManager.GraphicsDevice.Viewport.Width,
+                ScreenManager.GraphicsDevice.Viewport.Width, user);
+
+            retry = Tools3.MainMenuButton(ScreenManager.Game.Content, mainMenuButtonPosition, ScreenManager.GraphicsDevice.Viewport.Width,
+                ScreenManager.GraphicsDevice.Viewport.Width, user);
+
+            newGame = Tools3.NewGameButton(ScreenManager.Game.Content, mainMenuButtonPosition, ScreenManager.GraphicsDevice.Viewport.Width,
+                ScreenManager.GraphicsDevice.Viewport.Width, user);
+
+            if (!correctAnswer)
+                solution = Tools3.MainMenuButton(ScreenManager.Game.Content, mainMenuButtonPosition, ScreenManager.GraphicsDevice.Viewport.Width,
+                ScreenManager.GraphicsDevice.Viewport.Width, user);
         }
 
-        /// <summary>
-        /// Allows the game component to update itself.
-        /// </summary>
-        /// <param name="gameTime">Provides a snapshot of timing values.</param>
-        public override void Update(GameTime gameTime)
-        {
-            // TODO: Add your update code here
-            s.Update(gameTime);
-            base.Update(gameTime);
-        }
-
-        /// <summary>
-        /// Allows the game component to update itself.
-        /// </summary>
-        /// <param name="gameTime">Provides a snapshot of timing values.</param>
         public override void Draw(GameTime gameTime)
         {
             ScreenManager.SpriteBatch.Begin();
-
-            // TODO: Add your update code here
-            //s.Draw(new Rectangle(100, 50, 500, 300),ScreenManager.Game.Content, ScreenManager.SpriteBatch, ScreenManager.GraphicsDevice.Viewport);
+            userSimulation.Draw(new Rectangle(5, 5, 250, 100), ScreenManager.Game.Content, ScreenManager.SpriteBatch,
+                ScreenManager.GraphicsDevice.Viewport);
+            mainMenu.Draw(ScreenManager.SpriteBatch);
+            mainMenu.DrawHand(ScreenManager.SpriteBatch);
             ScreenManager.SpriteBatch.End();
+
         }
+
+        public override void Update(GameTime gametime)
+        {
+            userSimulation.Update(gametime);
+        }
+
     }
+
+
 }
