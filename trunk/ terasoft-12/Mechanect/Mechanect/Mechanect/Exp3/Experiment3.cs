60c920b3405d36363e7e50ed3a49e0783e509875
vendettax6@gmail.com <vendettax6@gmail.com@113cb9fd-de2b-231c-2074-2ec382a84f02>
2012-05-23 16:07:07 +0000

    update issue 27 Status: In-Progress Fixed if wont fall.

--- trunk/ terasoft-12/Mechanect/Mechanect/Mechanect/Exp3/Experiment3.cs
+++ trunk/ terasoft-12/Mechanect/Mechanect/Mechanect/Exp3/Experiment3.cs
@@ -111,19 +111,19 @@ namespace Mechanect.Exp3
                 if (distance / totalDistance > 0.5 && !pauseScreenShowed)
                 {
                     pauseScreenShowed = true;
-                    FreezeScreen();
-                    ScreenManager.AddScreen(new PauseScreen(user,arriveVelocity,ball.Mass,user.assumedLegMass,environment.HoleProperty.Position));
+                    //FreezeScreen();
+                    //ScreenManager.AddScreen(new PauseScreen(user,arriveVelocity,ball.Mass,user.assumedLegMass,environment.HoleProperty.Position));
                    
                 }
                 bar.Update(new Vector2(ball.Position.X,ball.Position.Z));
 
-                /*if (distance / totalDistance > 1)
+                if (distance / totalDistance > 1)
                 {
                     firstAnimation = false;
                     shootVelocity = new Vector3(50, 0, -50);
                     animation = new BallAnimation(ball, environment, this.shootVelocity);
-                }*/
-                if (ball.hasBallEnteredShootRegion())
+                }
+                /*if (ball.hasBallEnteredShootRegion())
                 {
                     user.UpdateMeasuringVelocityAndAngle(gameTime);
                     Vector3 shootVelocity = user.velocity;
@@ -136,7 +136,7 @@ namespace Mechanect.Exp3
 
                     }
 
-                }
+                }*/
                 if (animation.Finished)
                 {
                     UpdateButtons(gameTime);
@@ -151,12 +151,6 @@ namespace Mechanect.Exp3
             {
                 UpdateButtons(gameTime);
                 simulation.Update(gameTime);
-                if (simulation.HasFinished())
-                {
-                    //FreezeScreen();
-                    Vector2 position = new Vector2(ScreenManager.GraphicsDevice.Viewport.Width / 2, ScreenManager.GraphicsDevice.Viewport.Height / 2);
-                    Tools3.DisplayIsWin(ScreenManager.SpriteBatch, ScreenManager.Game.Content, position, animation.willFall);
-                }
             }
             else
             {
@@ -199,6 +193,14 @@ namespace Mechanect.Exp3
                 simulation.Draw();
                 DrawButtons();
             }
+
+            if (simulation.HasFinished())
+            {
+                //FreezeScreen();
+                Vector2 position = new Vector2(ScreenManager.GraphicsDevice.Viewport.Width / 2, ScreenManager.GraphicsDevice.Viewport.Height / 2);
+                Tools3.DisplayIsWin(ScreenManager.SpriteBatch, ScreenManager.Game.Content, position, animation.willFall);
+            }
+
             base.Draw(gameTime);
         }
 
