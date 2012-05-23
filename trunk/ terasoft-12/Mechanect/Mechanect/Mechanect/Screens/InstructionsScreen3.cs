045194128fbf7e94f45653098086c7e2c84daf1d
khaled.osmaan@gmail.com <khaled.osmaan@gmail.com@113cb9fd-de2b-231c-2074-2ec382a84f02>
2012-05-23 22:25:19 +0000

    Update issue 36 editing the string, added 3 new sprite fonts for header's random text and titles, don't use class Instruction anymore

--- trunk/ terasoft-12/Mechanect/Mechanect/Mechanect/Screens/InstructionsScreen3.cs
+++ trunk/ terasoft-12/Mechanect/Mechanect/Mechanect/Screens/InstructionsScreen3.cs
@@ -7,17 +7,27 @@ namespace Mechanect.Screens
 {
     class InstructionsScreen3 : GameScreen
     {
-        private string instructions = "\n\n\n\n Welcome to Mechanect football game made by terasoft \n The point of this game is to shoot the ball such that it reaches the hole with zero velocity. \n In the beginning you're shown a screen where you can test shooting and see your angle and velocity. \n The avatar on the right represents your distance from the kinect device, below is the color code \n Green: you're standing in the correct position, White: you're almost too far, Red: Not all your skeleton is detected.";
+private string instructions = "";
+        string header= "\n\n       .Welcome To Mechanect Football Game."+ '\n'
+             + "                .By TeraSoft Team."  + '\n'
+               +"                    .GUC.";
+        private string title="\n Game Instructions :- \n\n\n\n";
         private Instruction instruction;
         private User3 user3;
         private Texture2D myTexture;
         private Rectangle rect;
         private Button button;
         private float scale;
+        SpriteFont font1;
+        SpriteFont font2;
+        SpriteFont font3;
+        Color colorCode;
+
         public InstructionsScreen3(User3 user3)
         {
             this.user3 = user3;
         }
+
         public InstructionsScreen3(string instructions, User3 user3): this(user3)
         {
             this.instructions = instructions;
@@ -34,10 +44,12 @@ namespace Mechanect.Screens
             myTexture = ScreenManager.Game.Content.Load<Texture2D>(@"Textures/Screens/instructions");
             scale = ((float)(ScreenManager.GraphicsDevice.Viewport.Width) / (float)myTexture.Width);
             rect = new Rectangle(0, 0, (int)(scale * myTexture.Width), (int)(scale * myTexture.Height));
-            instruction = new Instruction(instructions, ScreenManager.Game.Content, ScreenManager.SpriteBatch,
-                ScreenManager.GraphicsDevice, user3,rect);
-            instruction.SpriteFont = ScreenManager.Game.Content.Load<SpriteFont>("SpriteFont1");
-            instruction.LoadContent();
+            //instruction = new Instruction(instructions, ScreenManager.Game.Content, ScreenManager.SpriteBatch,
+            //    ScreenManager.GraphicsDevice, user3,rect);
+            font1 = ScreenManager.Game.Content.Load<SpriteFont>("SpriteFont4");
+            font2 = ScreenManager.Game.Content.Load<SpriteFont>("SpriteFont5");
+            font3 = ScreenManager.Game.Content.Load<SpriteFont>("SpriteFont6");
+           // instruction.LoadContent();
             button = Tools3.OKButton(ScreenManager.Game.Content,
             new Vector2(ScreenManager.GraphicsDevice.Viewport.Width - 496, ScreenManager.GraphicsDevice.Viewport.Height - 196), ScreenManager.GraphicsDevice.Viewport.Width,
             ScreenManager.GraphicsDevice.Viewport.Height, user3);
@@ -79,8 +91,9 @@ namespace Mechanect.Screens
             ScreenManager.SpriteBatch.Draw(myTexture, rect, Color.White);
             button.Draw(ScreenManager.SpriteBatch, scale);
             button.DrawHand(ScreenManager.SpriteBatch);
+            ScreenManager.SpriteBatch.DrawString(font1, header, Vector2.Zero, Color.Black, 0, Vector2.Zero, 1f, SpriteEffects.None, 0.0f);
             ScreenManager.SpriteBatch.End();
-            instruction.Draw(gameTime);
+            //instruction.Draw(gameTime);
             base.Draw(gameTime);
         }
         /// <summary>
@@ -92,7 +105,6 @@ namespace Mechanect.Screens
         public void Remove()
         {
             base.Remove();
-            
         }
 
     }
