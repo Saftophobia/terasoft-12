5f7dbfaca05b5e5c9dee10f3a4c0397e1c4500d5
mohamedra2fat@gmail.com <mohamedra2fat@gmail.com@113cb9fd-de2b-231c-2074-2ec382a84f02>
2012-05-23 20:25:52 +0000

    Update Issue 236 Status In Progress Adjusting Draw and Update method

--- trunk/ terasoft-12/Mechanect/Mechanect/Mechanect/Exp2/StatisticsScreen.cs
+++ trunk/ terasoft-12/Mechanect/Mechanect/Mechanect/Exp2/StatisticsScreen.cs
@@ -18,17 +18,30 @@ namespace Mechanect.Screens
         User2 user;
         private Simulation userSimulation;
         private Simulation optimalSimulation;
+
+        private Rectangle leftSimulationPosition;
+        private Rectangle rightSimulationPosition;
+        private Rectangle centerSimulationPosition;
+        private Rectangle currentUserSimulationPosition;
+
         Vector2 mainMenuButtonPosition;
         Vector2 retryButtonPosition;
         Vector2 newGameButtonPosition;
         Vector2 seeResultsButtonPosition;
+
         private bool correctAnswer;
+
+
+
         private Button mainMenu;
         private Button retry;
         private Button solution;
         private Button newGame;
 
-
+        private Viewport ViewPort
+        {
+            get { return ScreenManager.GraphicsDevice.Viewport; }
+        }
 
 
         public StatisticsScreen(Vector2 predatorPosition, Rect preyPosition, Rect aquariumPosition, float userVelocity,
@@ -51,17 +64,22 @@ namespace Mechanect.Screens
 
         public override void Initialize()
         {
-            mainMenuButtonPosition = new Vector2((float)0.01 * ScreenManager.GraphicsDevice.Viewport.Width,
-               (float)0.5 * ScreenManager.GraphicsDevice.Viewport.Height);
+            mainMenuButtonPosition = new Vector2((float)0.01*ScreenManager.GraphicsDevice.Viewport.Width,
+               (float)0.8*ScreenManager.GraphicsDevice.Viewport.Height);
 
-            retryButtonPosition = new Vector2((float)0.35 * ScreenManager.GraphicsDevice.Viewport.Width,
-               (float)0.5 * ScreenManager.GraphicsDevice.Viewport.Height);
+            retryButtonPosition = new Vector2((float)0.42 * ScreenManager.GraphicsDevice.Viewport.Width,
+               (float)0.8 * ScreenManager.GraphicsDevice.Viewport.Height);
 
-            newGameButtonPosition = new Vector2((float)0.7 * ScreenManager.GraphicsDevice.Viewport.Width,
-               (float)0.5 * ScreenManager.GraphicsDevice.Viewport.Height);
+            newGameButtonPosition = new Vector2((float)0.84 * ScreenManager.GraphicsDevice.Viewport.Width,
+               (float)0.8 * ScreenManager.GraphicsDevice.Viewport.Height);
 
-            seeResultsButtonPosition = new Vector2((float)0.7 * ScreenManager.GraphicsDevice.Viewport.Width,
-               (float)0.5 * ScreenManager.GraphicsDevice.Viewport.Height);
+            seeResultsButtonPosition = new Vector2((float)0.84 * ScreenManager.GraphicsDevice.Viewport.Width,
+               (float)0.8 * ScreenManager.GraphicsDevice.Viewport.Height);
+
+            rightSimulationPosition = new Rectangle(); ///
+            centerSimulationPosition = new Rectangle(); ///
+            leftSimulationPosition = new Rectangle(); ///
+            currentUserSimulationPosition = new Rectangle(); ///
         }
         //Still waiting for Hegazy to do the buttons for me.
         public override void LoadContent()
@@ -81,6 +99,7 @@ namespace Mechanect.Screens
             newGame = Mechanect.Exp3.Tools3.NewGameButton(ScreenManager.Game.Content, newGameButtonPosition,
                 ScreenManager.GraphicsDevice.Viewport.Width,
                 ScreenManager.GraphicsDevice.Viewport.Width, user);
+            
 
             if (!correctAnswer)
                 solution = Mechanect.Exp3.Tools3.MainMenuButton(ScreenManager.Game.Content, seeResultsButtonPosition,
@@ -90,18 +109,18 @@ namespace Mechanect.Screens
 
         public override void Draw(GameTime gameTime)
         {
+         
+             
             ScreenManager.SpriteBatch.Begin();
-            //userSimulation.Draw(new Rectangle((int)0.7*ScreenManager.GraphicsDevice.Viewport.Width, 
-            //  (int)0.5*ScreenManager.GraphicsDevice.Viewport.Height
-            //, (int) 0.6*ScreenManager.GraphicsDevice.Viewport.Width, 
-            //(int) 0.6*ScreenManager.GraphicsDevice.Viewport.Height),
-            //ScreenManager.SpriteBatch);
-            mainMenu.Draw(ScreenManager.SpriteBatch);
-            retry.Draw(ScreenManager.SpriteBatch);
-            newGame.Draw(ScreenManager.SpriteBatch);
-            if (!correctAnswer)
-                newGame.Draw(ScreenManager.SpriteBatch);
-            else
+            mainMenu.Draw(ScreenManager.SpriteBatch,0.5f);
+            retry.Draw(ScreenManager.SpriteBatch, 0.5f);
+            newGame.Draw(ScreenManager.SpriteBatch, 0.5f);
+            
+
+           // if (!correctAnswer)
+             //   newGame.Draw(ScreenManager.SpriteBatch);
+            //else
+
                 newGame.DrawHand(ScreenManager.SpriteBatch);
 
             ScreenManager.SpriteBatch.End();
