9e38680011a5b14b2f92617b85735bcb3435db03
ahmedhegazy835 <ahmedhegazy835@113cb9fd-de2b-231c-2074-2ec382a84f02>
2012-05-23 17:19:58 +0000

    update issue 6 Status: In Progress Editing the documentation of class Slider

--- trunk/ terasoft-12/Mechanect/Mechanect/Mechanect/ButtonsAndSliders/Slider.cs
+++ trunk/ terasoft-12/Mechanect/Mechanect/Mechanect/ButtonsAndSliders/Slider.cs
@@ -13,66 +13,72 @@ namespace ButtonsAndSliders
         private Vector2 positionBar;
         private Vector2 positionPointer;
         private Texture2D texture, onPic, offPic, barPic;
-        private int screenW, ScreenH;
+        private int screenW, screenH;
         public int Value { get; private set;}
 
         private User user;
         private Timer1 timer;
 
-        private ContentManager Content;
+        private ContentManager content;
 
-        ///<remarks>
-        ///<para>
-        ///Author: HegazY
-        ///</para>
-        ///</remarks>
         /// <summary>
-        /// The constructor used to initialize the slider
+        /// The constructor used to initialize the slider.
         /// </summary>
-        /// <param name="p">the position of the slider, where the center is the top left corner </param>
-        /// <param name="sw">screen width</param>
-        /// <param name="sh">screen height</param>
-        /// <param name="c">the content manager to draw the textures</param>
-        public Slider(Vector2 p, int sw, int sh, ContentManager c, User u)
+        /// <remarks>
+        /// <para>Author: HegazY</para>
+        /// </remarks>
+        /// <param name="position">The position of the slider, where the center is the top left corner </param>
+        /// <param name="screenW">The width of the screen.</param>
+        /// <param name="screenH">The height of the screen.</param>
+        /// <param name="content">The content manager to load the textures.</param>
+        /// <param name="user">The instance of the user.</param>
+        public Slider(Vector2 position, int screenW, int screenH, ContentManager content, User user)
         {
-            screenW = sw;
-            ScreenH = sh;
+            this.screenW = screenW;
+            this.screenH = screenH;
             Value = 1;
 
-            user = u;
-            Content = c;
+            this.user = user;
+            this.content = content;
             timer = new Timer1();
 
-            positionBar = p;
-            positionPointer.X = p.X;
+            positionBar = position;
+            positionPointer.X = position.X;
         }
 
         public void LoadContent()
         {
-            texture = Content.Load<Texture2D>("Textures/Slider/off");
-            onPic = Content.Load<Texture2D>("Textures/Slider/on");
-            offPic = Content.Load<Texture2D>("Textures/Slider/off");
-            barPic = Content.Load<Texture2D>("Textures/Slider/bar");
+            texture = content.Load<Texture2D>("Textures/Slider/off");
+            onPic = content.Load<Texture2D>("Textures/Slider/on");
+            offPic = content.Load<Texture2D>("Textures/Slider/off");
+            barPic = content.Load<Texture2D>("Textures/Slider/bar");
 
             positionPointer.Y = positionBar.Y - ((texture.Height - barPic.Height) / 2);
         }
 
         ///<remarks>
-        ///<para>
-        ///Author: HegazY
-        ///</para>
-        ///</remarks>
         /// <summary>
-        /// drawing the bar and the pointer
+        /// Draws the slider.
         /// </summary>
-        /// <param name="spriteBatch">used to draw the texture</param>
-        [System.Obsolete("add screen width, to scale the slider", false)]
+        /// <remarks>
+        /// <para>Author: HegazY</para>
+        /// </remarks>
+        /// <param name="spriteBatch">The spritebatch used to draw the textures.</param>
         public void Draw(SpriteBatch spriteBatch)
         {
             spriteBatch.Draw(barPic, positionBar, Color.White);
             spriteBatch.Draw(texture, positionPointer, Color.White);
         }
 
+
+        /// <summary>
+        /// Draws the slider.
+        /// </summary>
+        /// <remarks>
+        /// <para>Author: HegazY</para>
+        /// </remarks>
+        /// <param name="spriteBatch">The spritebatch used to draw the textures.</param>
+        /// <param name="scale">The ratio to scale the width and the height of the slider.</param>
         public void Draw(SpriteBatch spriteBatch, float scale)
         {
             Rectangle rectangleBar = new Rectangle((int)positionBar.X, (int)positionBar.Y,
@@ -85,6 +91,16 @@ namespace ButtonsAndSliders
             spriteBatch.Draw(texture, rectanglePointer, Color.White);
         }
 
+
+        /// <summary>
+        /// Draws the slider.
+        /// </summary>
+        /// <remarks>
+        /// <para>Author: HegazY</para>
+        /// </remarks>
+        /// <param name="spriteBatch">The spritebatch used to draw the textures.</param>
+        /// <param name="scaleW">The ratio to scale the width of the slider.</param>
+        /// <param name="scaleW">The ratio to scale the height of the slider.</param>
         public void Draw(SpriteBatch spriteBatch, float scaleW, float scaleH)
         {
             Rectangle rectangleBar = new Rectangle((int)positionBar.X, (int)positionBar.Y,
@@ -96,15 +112,15 @@ namespace ButtonsAndSliders
             spriteBatch.Draw(barPic, rectangleBar, Color.White);
             spriteBatch.Draw(texture, rectanglePointer, Color.White);
         }
-        ///<remarks>
-        ///<para>
-        ///Author: HegazY
-        ///</para>
-        ///</remarks>
+
+
         /// <summary>
-        /// checking if the hand of the user has been hovered on the pointer and starts
-        /// the timer and activating the pointer to be moved.
+        /// Allows the slider to run correctly.
         /// </summary>
+        /// <remarks>
+        /// <para>Author: HegazY</para>
+        /// </remarks>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
         public void Update(GameTime gameTime) 
         {
             if (CheckCollision())
@@ -129,21 +145,20 @@ namespace ButtonsAndSliders
             
         }
 
-        ///<remarks>
-        ///<para>
-        ///Author: HegazY
-        ///</para>
-        ///</remarks>
+
         /// <summary>
-        /// tracking the movement of the hand to move the pointer to the right or the left. Incrementing
+        /// Moves the pointer according to the movement of the hand. Incrementing
         /// or decrementing the value according to the movement.
         /// </summary>
+        /// <remarks>
+        /// <para>Author: HegazY</para>
+        /// </remarks>
         private void Move()
         {
             Skeleton skeleton = user.Kinect.requestSkeleton();
             if (skeleton != null)
             {
-                Point hand = user.Kinect.GetJointPoint(skeleton.Joints[JointType.HandRight], screenW, ScreenH);
+                Point hand = user.Kinect.GetJointPoint(skeleton.Joints[JointType.HandRight], screenW, screenH);
 
                 if ((hand.X - positionPointer.X) >= 30 && !(Value == 4))
                 {
@@ -158,48 +173,45 @@ namespace ButtonsAndSliders
             }
         }
 
-        ///<remarks>
-        ///<para>
-        ///Author: HegazY
-        ///</para>
-        ///</remarks>
+
         /// <summary>
-        /// changing the pointer to the activated pointer picture
+        /// Changes the pointer to the activated pointer picture.
         /// </summary>
+        /// <remarks>
+        /// <para>Author: HegazY</para>
+        /// </remarks>
         private void On()
         {
             texture = onPic;
         }
 
-        ///<remarks>
-        ///<para>
-        ///Author: HegazY
-        ///</para>
-        ///</remarks>
+
         /// <summary>
-        /// changing the pointer to the disactivated pointer picture
+        /// Changes the pointer to the disactivated pointer picture.
         /// </summary>
+        /// <remarks>
+        /// <para>Author: HegazY</para>
+        /// </remarks>
         private void Off()
         {
             texture = offPic;
         }
 
-        ///<remarks>
-        ///<para>
-        ///Author: HegazY
-        ///</para>
-        ///</remarks>
+
         /// <summary>
-        /// checks if the hand of the user is over the pointer or not
+        /// Checks if the hand of the user is over the pointer or not.
         /// </summary>
-        /// <returns>returns true if the user is hovering the pointer</returns>
+        /// <remarks>
+        /// <para>Author: HegazY</para>
+        /// </remarks>
+        /// <returns>Returns true if the user is hovering the pointer.</returns>
         private bool CheckCollision()
         {
 
             Skeleton skeleton = user.USER;
             if (skeleton != null)
             {
-                Point hand = user.Kinect.GetJointPoint(skeleton.Joints[JointType.HandRight], screenW, ScreenH);
+                Point hand = user.Kinect.GetJointPoint(skeleton.Joints[JointType.HandRight], screenW, screenH);
                 Rectangle r1 = new Rectangle(hand.X, hand.Y, 50, 50);
                 Rectangle r2 = new Rectangle((int)positionPointer.X, (int)positionPointer.Y, 
                     offPic.Width, offPic.Height);
@@ -207,22 +219,6 @@ namespace ButtonsAndSliders
                 return r1.Intersects(r2);
             }
             return false;
-        }
-
-
-        ///<remarks>
-        ///<para>
-        ///Author: HegazY
-        ///</para>
-        ///</remarks>
-        /// <summary>
-        /// used to get the value that the user selected
-        /// </summary>
-        /// <returns>the value that the user selected</returns>
-        [System.Obsolete("this method will be deleted, use button.value", false)]
-        public int GetValue()
-        {
-            return Value;
         }
 
     }
