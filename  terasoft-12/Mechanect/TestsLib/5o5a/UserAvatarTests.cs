27f2ccd54bca5e2ed450e198a699a5b92921008b
khaled.osmaan@gmail.com <khaled.osmaan@gmail.com@113cb9fd-de2b-231c-2074-2ec382a84f02>
2012-05-24 12:08:49 +0000

    Update issue 237 added a test for method GetUserIndex

---
+++  terasoft-12/Mechanect/TestsLib/5o5a/UserAvatarTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using NUnit.Framework;
+using Mechanect;
+using Mechanect.Exp3;
+using Mechanect.Common;
+
+namespace Tests
+{
+    [TestFixture]
+    public class UserAvatarTests
+    {
+        User user;
+        User[] users;
+         [SetUp]
+        public void Init()
+        {
+             user = new User(); ;
+            users= new User[4];
+            users[0] = user;
+        }
+
+        [Test]
+        public void TestGetUserIndex()
+        {
+            Assert.AreEqual(UserAvatar.getUserindex(user,users), user);
+        }
+    }
+}
