efaaf61df98c5cd5447da37df8663b846fd4b5e5
asamgdla@gmail.com <asamgdla@gmail.com@113cb9fd-de2b-231c-2074-2ec382a84f02>
2012-05-24 06:19:21 +0000

    update issue 235 Status: In-Progress Resolved the lines calling the Moving2DAvatar

--- trunk/ terasoft-12/Mechanect/Mechanect/Mechanect/Exp1/Environ1.cs
+++ trunk/ terasoft-12/Mechanect/Mechanect/Mechanect/Exp1/Environ1.cs
@@ -6,6 +6,7 @@ using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Audio;
 using Microsoft.Xna.Framework;
+using UI.Experiment1;
 
 namespace Mechanect.Exp1
 {
