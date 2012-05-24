1b9d6b684922efbf0c0ec0a7886babb4ec2d926b
3abdelazim@gmail.com <3abdelazim@gmail.com@113cb9fd-de2b-231c-2074-2ec382a84f02>
2012-05-24 20:05:15 +0000

    update issue 225 Status: Ready Documented Code

---  terasoft-12/Mechanect/Mechanect/Mechanect/Exp2/Simulation.cs
+++  terasoft-12/Mechanect/Mechanect/Mechanect/Exp2/Simulation.cs
@@ -1,41 +1,40 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Windows;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
-using System.Windows;
 
 namespace Mechanect.Exp2
 {
+
+    /// <summary>
+    /// Displays a Simulation of an environment with given locations and velocity
+    /// </summary>
+    /// <remarks>
+    /// <para>AUTHOR: Mohamed AbdelAzim</para>
+    /// </remarks>
     class Simulation
     {
         private Environment2 environment;
         private Vector2 initialPredatorVelocity;
         private Vector2 initialPredatorPosition;
-        private bool predatorInitialized;
         private bool simulationRunning;
         private float milliseconds;
         private SpriteFont font;
         private float velocity;
         private float angle;
 
-        [System.Obsolete("use Rect instead of Rectangle", true)]
-        public Simulation(Vector2 predatorPosition, Rectangle preyPosition, Rectangle aquariumPosition, float velocity, float angle)
-        {
-            environment = new Environment2(predatorPosition, preyPosition, aquariumPosition);
-            initialPredatorVelocity = velocity * 
-                new Vector2((float)Math.Cos(MathHelper.ToRadians(angle)), 
-                    (float)Math.Sin(MathHelper.ToRadians(angle)));
-            initialPredatorPosition = predatorPosition;
-            simulationRunning = false;
-            predatorInitialized = false;
-            this.velocity = velocity;
-            this.angle = angle;
-            
-        }
-
+        /// <summary>
+        /// Generates the Simulation that displays the environment during the projectile motion
+        /// </summary>
+        /// <remarks>
+        /// <para>AUTHOR: Mohamed AbdelAzim</para>
+        /// </remarks>
+        /// <param name ="predatorPosition">The initial position of the predator</param>
+        /// <param name ="preyPosition">The position of the prey</param>
+        /// <param name ="aquariumPosition">The position of the aquarium</param>
+        /// <param name ="velocity">The magnitude of velocity of the projectile</param>
+        /// <param name ="angle">The degree angle of the projectile</param>
         public Simulation(Vector2 predatorPosition, Rect preyPosition, Rect aquariumPosition, float velocity, float angle)
         {
             environment = new Environment2(predatorPosition, preyPosition, aquariumPosition);
@@ -43,25 +42,34 @@ namespace Mechanect.Exp2
                 new Vector2((float)Math.Cos(MathHelper.ToRadians(angle)),(float)Math.Sin(MathHelper.ToRadians(angle)));
             initialPredatorPosition = predatorPosition;
             simulationRunning = false;
-            predatorInitialized = false;
             this.velocity = velocity;
             this.angle = angle;
 
         }
 
+        /// <summary>
+        /// Loads the textures of the simulation
+        /// </summary>
+        /// <remarks>
+        /// <para>AUTHOR: Mohamed AbdelAzim</para>
+        /// </remarks>
+        /// <param name ="contentManager">The content manager of the screen manager</param>
+        /// <param name ="graphicsDevice">The graphics device of the screen manager</param>
         public void LoadContent(ContentManager contentManager, GraphicsDevice graphicsDevice)
         {
             font = contentManager.Load<SpriteFont>("Ariel");
             environment.LoadContent(contentManager, graphicsDevice,graphicsDevice.Viewport);
             
         }
-        [System.Obsolete("use LoadContent(ContentManager, GraphicsDevice)", true)]
-        public void LoadContent(ContentManager contentManager, Viewport viewport, GraphicsDevice graphicsDevice)
-        {
-            font = contentManager.Load<SpriteFont>("Ariel");
-            environment.LoadContent(contentManager, graphicsDevice, graphicsDevice.Viewport);
 
-        }
+
+        /// <summary>
+        /// Updates the simulation to generate different positions of predator with time
+        /// </summary>
+        /// <remarks>
+        /// <para>AUTHOR: Mohamed AbdelAzim</para>
+        /// </remarks>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
         public void Update(GameTime gameTime)
         {
             if (!simulationRunning)
@@ -78,7 +86,6 @@ namespace Mechanect.Exp2
                     environment.Predator.Velocity = initialPredatorVelocity;
                     environment.Predator.Movable = true;
                     environment.Prey.Eaten = false;
-                    predatorInitialized = true;
                 }
             }
             else
@@ -88,15 +95,15 @@ namespace Mechanect.Exp2
            
         }
 
+        /// <summary>
+        /// Draws the simulation in the specified Rectangle.
+        /// </summary>
+        /// <remarks>
+        /// <para>AUTHOR: Mohamed AbdelAzim</para>
+        /// </remarks>
+        /// <param name ="rectangle">The rectangle that the simulation will be drawn in.</param>
+        /// <param name ="spriteBatch">The sprite batch of the screen manager.</param>
         public void Draw(Rectangle rectangle, SpriteBatch spriteBatch)
-        {
-            environment.Draw(rectangle, spriteBatch);
-            string data = "Velocity = " + velocity + ", Angle = " + angle;
-            spriteBatch.DrawString(font, data, new Vector2(rectangle.X, rectangle.Y), Color.Black);
-        }
-
-        [System.Obsolete("use Draw(Rectangle, SpriteBatch)", true)]
-        public void Draw(Rectangle rectangle, ContentManager contentManager, SpriteBatch spriteBatch, Viewport viewport)
         {
             environment.Draw(rectangle, spriteBatch);
             string data = "Velocity = " + velocity + ", Angle = " + angle;
