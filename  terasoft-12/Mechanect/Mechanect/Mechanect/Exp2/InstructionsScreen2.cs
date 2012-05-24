8226472954d736444d4b074f8f5d1d6ddb15e3e5
hollaxyman@gmail.com <hollaxyman@gmail.com@113cb9fd-de2b-231c-2074-2ec382a84f02>
2012-05-24 03:13:35 +0000

    Updated InstructionScreen2 to have same design as InstructionScreen3

---  terasoft-12/Mechanect/Mechanect/Mechanect/Exp2/InstructionsScreen2.cs
+++  terasoft-12/Mechanect/Mechanect/Mechanect/Exp2/InstructionsScreen2.cs
@@ -1,7 +1,3 @@
- using Microsoft.Xna.Framework;
-using Mechanect.Common;
-using Microsoft.Xna.Framework.Graphics;
-using Mechanect.Classes;
 using Mechanect.Screens;
 using ButtonsAndSliders;
 using Mechanect.Exp3;
@@ -10,62 +6,89 @@ namespace Mechanect.Exp2
 {
     class InstructionsScreen2 : GameScreen
     {
-        string instructions;
-        User2 user;
-        Button button;
+       private string instructions = "";
+        string header= "\n\n\n                  .Welcome To Mechanect Projectile Game."+ '\n'
+             + "                           .By TeraSoft Team."  + '\n'
+               +"                                 .GUC.";
+        private string title1="\n\n\n\n\n\n      Game Instructions:-";
+        private string text1 = "\n\n\n\n\n\n\n\n\n\n\n          1-Goal: Use projectile equations to calculate  angle and velocity  " +
+            "\n          that the fish can be thrown by to eat the prey and fall in the aquarium.  \n "
+            +"\n          2-Givens: Predator Point,Prey Point,Aquarium point \n"+ 
+            "            \n"
+          +
+            "\n          3-Settings: The next screen will tell you how to adjust your position for the game.\n";
+        private string text2 = "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n          * The avatar on the top right \n            represents your distance from\n            the screen.\n";
+        private string green = "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n                                                                             Green: Good.";
+        private string white = "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n                                                                             White: Too Far.";
+        private string red = "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n                                                                             Red: Too Near.";
+        private string crossed = "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n                                                                             Crossed: Not Detected.";
+        private User2 user2;
+        private Texture2D myTexture;
+        private Rectangle rect;
+        private Button button;
+        private float scale;
+        SpriteFont font1;
+        SpriteFont font2;
+        SpriteFont font3;
 
         public InstructionsScreen2(User2 user2)
         {
-            instructions = " Hey there instructions : \n \nThe Goal of the game is to throw the fish in the aquarium passing by the prey. " + 
-                "You should move your arm from back to front " + 
-                "to a certain angle with some velocity to determine the angle and velocity of projectile.";
-            this.user = user2;
+            this.user2 = user2;
         }
-        public InstructionsScreen2(string instructions, User2 user)
+
+        public InstructionsScreen2(string instructions, User2 user2): this(user2)
         {
             this.instructions = instructions;
-            this.user = user;
         }
-
+        
         public override void LoadContent()
         {
-            
-            float screenWidth = ScreenManager.GraphicsDevice.Viewport.Width;
-            float screenHeight = ScreenManager.GraphicsDevice.Viewport.Height;
-            button = Tools3.OKButton(ScreenManager.Game.Content, new Vector2(screenWidth / 1.75f, 0.5f * screenHeight),
-                ScreenManager.GraphicsDevice.Viewport.Width, ScreenManager.GraphicsDevice.Viewport.Height, user);
+            myTexture = ScreenManager.Game.Content.Load<Texture2D>(@"Textures/Screens/instructions");
+            scale = ((float)(ScreenManager.GraphicsDevice.Viewport.Width) / (float)myTexture.Width);
+            rect = new Rectangle(0, 0, (int)(scale * myTexture.Width), (int)(scale * myTexture.Height));
+            font1 = ScreenManager.Game.Content.Load<SpriteFont>("SpriteFont4");
+            font2 = ScreenManager.Game.Content.Load<SpriteFont>("SpriteFont5");
+            font3 = ScreenManager.Game.Content.Load<SpriteFont>("SpriteFont6");
+            button = Tools3.OKButton(ScreenManager.Game.Content,
+            new Vector2(ScreenManager.GraphicsDevice.Viewport.Width - 496, ScreenManager.GraphicsDevice.Viewport.Height - 196), ScreenManager.GraphicsDevice.Viewport.Width,
+            ScreenManager.GraphicsDevice.Viewport.Height, user2);
             base.LoadContent();
         }
-
         public override void Update(GameTime gameTime)
         {
-            user.setSkeleton();
+            user2.setSkeleton();
             button.Update(gameTime);
             if (button.IsClicked())
             {
                 Remove();
-                ScreenManager.AddScreen(new AdjustPosition(user, 150, 350, -40, 40, 2));
+                ScreenManager.AddScreen(new AdjustPosition(user2, 150, 350, -40, 40, 2));
             }
             base.Update(gameTime);
-        }
 
+        }
         public override void Draw(GameTime gameTime)
         {
             ScreenManager.SpriteBatch.Begin();
-            int screenWidth = ScreenManager.GraphicsDevice.Viewport.Width;
-            int screenHeight = ScreenManager.GraphicsDevice.Viewport.Height;
-            UI.UILib.Write(instructions, new Rectangle(screenWidth / 5, screenHeight / 5, 3 * screenWidth / 5, 3 * screenHeight / 5), ScreenManager.SpriteBatch, ScreenManager.Game.Content);
-            button.Draw(ScreenManager.SpriteBatch,0.7f);
+            ScreenManager.SpriteBatch.Draw(myTexture, rect, Color.White);
+            button.Draw(ScreenManager.SpriteBatch, scale);
             button.DrawHand(ScreenManager.SpriteBatch);
+            ScreenManager.SpriteBatch.DrawString(font1, header, Vector2.Zero, Color.DarkViolet, 0, Vector2.Zero, 1f, SpriteEffects.None, 0.0f);
+            ScreenManager.SpriteBatch.DrawString(font2, title1, Vector2.Zero, Color.DarkRed, 0, Vector2.Zero, 1f, SpriteEffects.None, 0.0f);
+            ScreenManager.SpriteBatch.DrawString(font3, text1, Vector2.Zero, Color.Black, 0, Vector2.Zero, 1f, SpriteEffects.None, 0.0f);
+            ScreenManager.SpriteBatch.DrawString(font3, text2, Vector2.Zero, Color.Black, 0, Vector2.Zero, 1f, SpriteEffects.None, 0.0f);
+            ScreenManager.SpriteBatch.DrawString(font3, green, Vector2.Zero, Color.DarkGreen, 0, Vector2.Zero, 1f, SpriteEffects.None, 0.0f);
+            ScreenManager.SpriteBatch.DrawString(font3, white, Vector2.Zero, Color.White, 0, Vector2.Zero, 1f, SpriteEffects.None, 0.0f);
+            ScreenManager.SpriteBatch.DrawString(font3, red, Vector2.Zero, Color.DarkRed, 0, Vector2.Zero, 1f, SpriteEffects.None, 0.0f);
+            ScreenManager.SpriteBatch.DrawString(font3, crossed, Vector2.Zero, Color.DarkGray, 0, Vector2.Zero, 1f, SpriteEffects.None, 0.0f);
             ScreenManager.SpriteBatch.End();
             base.Draw(gameTime);
         }
-        public override void Remove()
+      
+        public void Remove()
         {
             base.Remove();
-
         }
-
 
     }
 }
+
