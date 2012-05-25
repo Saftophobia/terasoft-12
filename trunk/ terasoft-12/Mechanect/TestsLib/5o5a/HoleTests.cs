0d2b8fa337597fc5d58e5f0f5864366d94e05627
khaled.osmaan@gmail.com <khaled.osmaan@gmail.com@113cb9fd-de2b-231c-2074-2ec382a84f02>
2012-05-25 09:44:20 +0000

    Update issue 35 fixed errors in hole tests

--- trunk/ terasoft-12/Mechanect/TestsLib/5o5a/HoleTests.cs
+++ trunk/ terasoft-12/Mechanect/TestsLib/5o5a/HoleTests.cs
@@ -1,44 +1,37 @@
-﻿using System;
-using NUnit.Framework;
-using Mechanect;
+﻿using NUnit.Framework;
 using Mechanect.Exp3;
-using Mechanect.Common;
 
 namespace Tests
 {
     [TestFixture]
     public class HoleTests
     {
-        Game1 game1;
-        Hole hole;
-        ScreenManager screenManager;
+        Experiment3 experiment3;
         Environment3 environment3;
         int angleTolerance;
+
         [SetUp]
         public void Init()
         {
-            game1 = new Game1();
-            game1.Content.RootDirectory = @"F:\SE\ terasoft-12\Mechanect\TestsLib\bin\Debug\Content";
-            game1.Run();
-            environment3 = Constants3.environment3;
-            screenManager = new ScreenManager(game1);
+
+            experiment3 = new Experiment3(new User3());
+            environment3 = experiment3.EnvironmentProperty; 
             angleTolerance = 4;
         }
 
         [Test]
         public void Test()
         {
-            hole = new Hole(screenManager.Game.Content, screenManager.GraphicsDevice, environment3.terrainWidth, environment3.terrainHeight, 10, environment3.user.shootingPosition);
-            Assert.LessOrEqual(environment3.HoleProperty.Position.Z, environment3.terrainHeight);
-            Assert.GreaterOrEqual(environment3.HoleProperty.Position.Z, -(environment3.terrainHeight));
-            Assert.LessOrEqual(environment3.HoleProperty.Position.X, environment3.terrainWidth);
-            Assert.GreaterOrEqual(environment3.HoleProperty.Position.X, -environment3.terrainWidth);
+            Assert.LessOrEqual(environment3.HoleProperty.Position.Z, environment3.TerrainHeight);
+            Assert.GreaterOrEqual(environment3.HoleProperty.Position.Z, -(environment3.TerrainHeight));
+            Assert.LessOrEqual(environment3.HoleProperty.Position.X, environment3.TerrainWidth);
+            Assert.GreaterOrEqual(environment3.HoleProperty.Position.X, -environment3.TerrainWidth);
         }
         [Test]
         public void RadiusTest()
         {
-            Assert.LessOrEqual(environment3.GenerateRadius(angleTolerance), 40);
-            Assert.GreaterOrEqual(environment3.GenerateRadius(angleTolerance), 5);
+            Assert.LessOrEqual(Environment3.GenerateRadius(angleTolerance), 40);
+            Assert.GreaterOrEqual(Environment3.GenerateRadius(angleTolerance), 5);
         }
     }
 }
