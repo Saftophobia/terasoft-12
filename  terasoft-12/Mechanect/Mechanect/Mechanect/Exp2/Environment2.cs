013731032330e81e537192082c208a2a0c4f0be7
3abdelazim@gmail.com <3abdelazim@gmail.com@113cb9fd-de2b-231c-2074-2ec382a84f02>
2012-05-25 18:01:48 +0000

    update issue 1 Status: Ready Changed Boolean to bool

---  terasoft-12/Mechanect/Mechanect/Mechanect/Exp2/Environment2.cs
+++  terasoft-12/Mechanect/Mechanect/Mechanect/Exp2/Environment2.cs
@@ -288,7 +288,7 @@ namespace Mechanect.Exp2
         /// </para>
         /// </remarks>
         /// <returns>a boolean flag which is true if the prey is eating and false otherwise</returns>
-        private Boolean isPreyEaten()
+        private bool isPreyEaten()
         {
             if (Predator.Location.X >= Prey.Location.X - Prey.Width / 2
                 && Predator.Location.X <= Prey.Location.X + Prey.Width / 2
@@ -308,7 +308,7 @@ namespace Mechanect.Exp2
         /// </para>
         /// </remarks>
         /// <returns>returns true if the predator reached the aquarium</returns>
-        private Boolean isAquariumReached()
+        private bool isAquariumReached()
         {
             if (Predator.Location.X >= Aquarium.Location.X - Aquarium.Width / 2
                 && Predator.Location.X <= Aquarium.Location.X + Aquarium.Width / 2
