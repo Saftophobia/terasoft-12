b7cb8232d4b6399f8a37cbf270d644d6c6dd4b80
mohamedra2fat@gmail.com <mohamedra2fat@gmail.com@113cb9fd-de2b-231c-2074-2ec382a84f02>
2012-05-25 18:11:28 +0000

    Update issuer 242 Status Ready Modifying the dimension of the simulation

---  terasoft-12/Mechanect/Mechanect/Mechanect/Exp2/StatisticsScreen.cs
+++  terasoft-12/Mechanect/Mechanect/Mechanect/Exp2/StatisticsScreen.cs
@@ -128,8 +128,8 @@ namespace Mechanect.Exp2
 
             rightSimulationPosition = new Rectangle(7 * viewPort.Width / 12, viewPort.Height / 10, viewPort.Width / 3,
                 4 * viewPort.Height / 10); ///
-            centerSimulationPosition = new Rectangle(viewPort.Width / 3, viewPort.Height / 10, 2 * viewPort.Width / 3,
-                6 * viewPort.Height / 10);
+            centerSimulationPosition = new Rectangle(viewPort.Width / 3, viewPort.Height / 10, viewPort.Width / 3,
+                5 * viewPort.Height / 10);
             leftSimulationPosition = new Rectangle(viewPort.Width / 12, viewPort.Height / 10, viewPort.Width / 3,
                 4 * viewPort.Height / 10); ///
             currentUserSimulationPosition = centerSimulationPosition; 
