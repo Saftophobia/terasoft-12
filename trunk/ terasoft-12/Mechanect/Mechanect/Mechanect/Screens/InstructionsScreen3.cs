f54b80342993b378e944d9c8eccbfd386c747cd4
mk.cena92@gmail.com <mk.cena92@gmail.com@113cb9fd-de2b-231c-2074-2ec382a84f02>
2012-05-25 13:57:24 +0000

    update issue 243
    changed the hand to be drawn at the end

--- trunk/ terasoft-12/Mechanect/Mechanect/Mechanect/Screens/InstructionsScreen3.cs
+++ trunk/ terasoft-12/Mechanect/Mechanect/Mechanect/Screens/InstructionsScreen3.cs
@@ -112,7 +112,6 @@ namespace Mechanect.Screens
             spriteBatch.Begin();
             spriteBatch.Draw(myTexture, rect, Color.White);
             button.Draw(spriteBatch, scale);
-            button.DrawHand(spriteBatch);
             spriteBatch.DrawString(font1, header, Vector2.Zero, Color.DarkViolet, 0, Vector2.Zero, 1f, SpriteEffects.None, 0.0f);
                 UILib.Write(title1, new Rectangle(screenWidth/10-40,screenHeight/5,screenWidth,screenHeight), spriteBatch, font2 , Color.DarkRed);
                 UILib.Write(text1, new Rectangle(screenWidth / 10 -22, screenHeight / 4 + 18, screenWidth-200, screenHeight), spriteBatch,  font3, Color.Black);
@@ -122,6 +121,7 @@ namespace Mechanect.Screens
                 UILib.Write(white, new Rectangle(screenWidth / 2 + 130, screenHeight / 2 + 170, screenWidth, screenHeight), spriteBatch,  font3, Color.White);
                 UILib.Write(red, new Rectangle(screenWidth / 2 + 130, screenHeight / 2 + 185, screenWidth, screenHeight), spriteBatch,  font3, Color.DarkRed);
                 UILib.Write(crossed, new Rectangle(screenWidth / 2 + 130, screenHeight / 2 + 200, screenWidth, screenHeight), spriteBatch, font3, Color.DarkGray);
+            button.DrawHand(spriteBatch);
             spriteBatch.End();
             base.Draw(gameTime);
         }
